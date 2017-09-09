40e3eb080c9acef84b07ae484cd413ecadd4df00
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-09 12:26:52 -0300

    Links para seleção de idioma.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,17 @@
   <div class="row">
     <div class="col-md-6 col-md-offset-3">
       <div class="panel panel-login">
+      <div class="row col-md-offset-2">
+        <div class="col-xs-3">
+           <a href="{{ route('lang.portuguese') }}">Português</a>
+        </div>
+        <div class="col-xs-3">
+           <a href="{{ route('lang.english') }}">English</a>
+        </div>
+        <div class="col-xs-3">
+           <a href="{{ route('lang.spanish') }}">Español</a>
+        </div>
+      </div>
         <div class="panel-heading">
           <div class="row">
             <div class="col-xs-6">
