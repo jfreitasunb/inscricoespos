d36d83d57ad5ea6973663a43d802c2bc1650f4aa
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-14 11:48:05 -0300

    Função para impedir que se configure duas inscrições com interseção da datas.

--- app/Models/ConfiguraInscricaoPos.php
+++ app/Models/ConfiguraInscricaoPos.php
@@ -87,6 +87,21 @@ class ConfiguraInscricaoPos extends Model
         }
     }
 
+    public function autoriza_configuracao_inscricao($nova_inscricao_inicio)
+    {
+        $inicio = Carbon::createFromFormat('Y-m-d', $this->retorna_inscricao_ativa()->inicio_inscricao);
+        $fim = Carbon::createFromFormat('Y-m-d', $this->retorna_inscricao_ativa()->fim_inscricao);
+
+        $data_inicio = $inicio->format('Y-m-d');
+        $data_fim = $fim->format('Y-m-d');
+
+        if ($nova_inscricao_inicio > $data_fim) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public function ira_ano_semestre()
     {    
         $date = new Carbon();
