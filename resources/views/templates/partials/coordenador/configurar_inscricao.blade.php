4a11a4f672c8270cbc33700570ff9d0903a15dd2
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-14 16:22:04 -0300

    Informar ano e número do edital.

--- resources/views/templates/partials/coordenador/configurar_inscricao.blade.php
+++ resources/views/templates/partials/coordenador/configurar_inscricao.blade.php
@@ -1,14 +1,15 @@
 @extends('templates.default')
 
 @section('stylesheets')
-  <link href="{{ asset('css/parsley.css') }}" rel="stylesheet">
+  {!! Html::style( asset('css/parsley.css') ) !!}
+  {!! Html::style( asset('bower_components/moment/locale/fr.js') ) !!}
 @endsection
 
 @section('configura_inscricao')
   
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
-      {!! Form::open(array('route' => 'configura.inscricao','data-parsley-validate' => '','enctype' => 'multipart/form-data')) !!}
+      {!! Form::open(array('route' => 'configura.inscricao','data-parsley-validate' => '' ,'enctype' => 'multipart/form-data')) !!}
         <legend>Configurar período da abertura da inscrição</legend>
         <div class="col-xs-4">
           <div class="form-group form-inline">
@@ -53,9 +54,15 @@
           @endforeach
         
         <legend>Edital</legend>
-
-
-
+        <div class="col-xs-6">
+          
+          {!! Form::label('edital_ano', 'Ano', ['class' => 'form-group form-inline']) !!}
+          {!! Form::text('edital_ano', null, ['class' => 'form-group', 'required' => '', 'data-parsley-type' => 'integer']) !!}
+        </div>
+        <div class="col-xs-6">
+          {!! Form::label('edital_numero', 'Número', ['class' => 'form-group form-inline']) !!}
+          {!! Form::text('edital_numero', null, ['class' => 'form-group', 'required' => '', 'data-parsley-type' => 'integer']) !!}
+        </div>
 
         {!! Form::submit('Teste', array('class' => 'register-submit btn btn-primary btn-lg', 'id' => 'register-submit', 'tabindex' => '4')) !!}
       {!! Form::close() !!}
@@ -65,12 +72,10 @@
 @endsection
 
 @section('scripts')
-  <script type="text/javascript" src="{{ asset('bower_components/jquery/jquery.min.js') }}"></script>
-  <script type="text/javascript" src="{{ asset('bower_components/moment/min/moment.min.js')}}"></script>
-  <script type="text/javascript" src="{{ asset('bower_components/moment/locale/pt-br.js')}}"></script>
-  <script type="text/javascript" src="{{ asset('bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js')}}"></script>
-  <link rel="stylesheet" href="{{ asset('bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css')}}" />
-  <script src="{{ asset('bower_components/moment/locale/fr.js')}}"></script>
-  <script type="text/javascript" src="{{ asset('js/datepicker.js') }}"></script>
-  <script type="text/javascript" src="{{ asset('js/parsely.min.js') }}"></script>
+  {!! Html::script( asset('bower_components/moment/min/moment.min.js') ) !!}
+  {!! Html::script( asset('bower_components/moment/locale/pt-br.js') ) !!}
+  {!! Html::script( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') ) !!}
+  {!! Html::script( asset('bower_components/moment/locale/fr.js') ) !!}
+  {!! Html::script( asset('js/datepicker.js') ) !!}
+  {!! Html::script( asset('js/parsley.min.js') ) !!}
 @endsection
