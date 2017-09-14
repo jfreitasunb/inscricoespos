e733860c62bdaddddf3338a12ffdd7109fa035f9
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-14 15:35:24 -0300

    Identação.

--- resources/views/templates/partials/coordenador/configurar_inscricao.blade.php
+++ resources/views/templates/partials/coordenador/configurar_inscricao.blade.php
@@ -9,40 +9,40 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       {!! Form::open(array('route' => 'configura.inscricao','data-parsley-validate' => '','enctype' => 'multipart/form-data')) !!}
-      <legend>Configurar período da abertura da inscrição</legend>
-      <div class="col-xs-4">
-        <div class="form-group form-inline">
-          {!! Form::label('inicio_inscricao', 'Início da Inscrição:') !!}
-          <div class='input-group' id='inicio_inscricao'>
-            {!! Form::text('inicio_inscricao', null, ['class' => 'form-control', 'required' => '']) !!}
-            <span class="input-group-addon">
-              <span class="glyphicon glyphicon-calendar"></span>
-            </span>
+        <legend>Configurar período da abertura da inscrição</legend>
+        <div class="col-xs-4">
+          <div class="form-group form-inline">
+            {!! Form::label('inicio_inscricao', 'Início da Inscrição:') !!}
+            <div class='input-group' id='inicio_inscricao'>
+              {!! Form::text('inicio_inscricao', null, ['class' => 'form-control', 'required' => '']) !!}
+              <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="col-xs-4">
-        <div class="form-group form-inline">
-        {!! Form::label('fim_inscricao', 'Final da Inscrição:') !!}
-        <div class='input-group' id='fim_inscricao'>
-          {!! Form::text('fim_inscricao', null, ['class' => 'form-control', 'required' => '']) !!}
-          <span class="input-group-addon">
-            <span class="glyphicon glyphicon-calendar"></span>
-          </span>
-        </div>
-      </div>
-      </div>
-      <div class="col-xs-4">
-        <div class="form-group form-inline">
-          {!! Form::label('prazo_carta', 'Prazo para envio da carta:') !!}
-          <div class='input-group' id='prazo_carta'>
-            {!! Form::text('prazo_carta', null, ['class' => 'form-control', 'required' => '']) !!}
-            <span class="input-group-addon">
-              <span class="glyphicon glyphicon-calendar"></span>
-            </span>
+        <div class="col-xs-4">
+          <div class="form-group form-inline">
+            {!! Form::label('fim_inscricao', 'Final da Inscrição:') !!}
+            <div class='input-group' id='fim_inscricao'>
+              {!! Form::text('fim_inscricao', null, ['class' => 'form-control', 'required' => '']) !!}
+              <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+        <div class="col-xs-4">
+          <div class="form-group form-inline">
+            {!! Form::label('prazo_carta', 'Prazo para envio da carta:') !!}
+            <div class='input-group' id='prazo_carta'>
+              {!! Form::text('prazo_carta', null, ['class' => 'form-control', 'required' => '']) !!}
+              <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+              </span>
+            </div>
+          </div>
+        </div>
         {!! Form::submit('Teste', array('class' => 'register-submit btn btn-primary btn-lg', 'id' => 'register-submit', 'tabindex' => '4')) !!}
       {!! Form::close() !!}
       
