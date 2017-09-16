4f5dc59a068b6fe774d81e9e9f1a4097cb9df53a
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-16 12:22:20 -0300

    Traduções para os termos na tela de login.

---
+++ resources/lang/en/tela_login.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+*Tradução dos textos para Inglês
+ */
+
+return [
+    'menu_email' => 'E-mail',
+    'menu_senha' => 'Password',
+    'menu_entrar' => 'Log in',
+    'menu_esqueceu_senha' => 'Forgott your Password?',
+    'menu_registro_sucesso_parte_inicial' => 'Account succesfully created. It was send an e-mail to: ',
+    'menu_registro_sucesso_parte_final' => 'with a link to activate your account.',
+
+];
---
+++ resources/lang/es/tela_login.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+*Tradução dos textos para Espanhol
+ */
+
+return [
+    'menu_email' => 'E-mail',
+    'menu_senha' => 'Contraseña',
+    'menu_entrar' => 'Entrar',
+    'menu_esqueceu_senha' => '¿Esqueceu Contraseña?',
+    'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
+    'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
+
+];
