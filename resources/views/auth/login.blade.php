ad715af16b258e9517577800075aeb6ecb743dcd
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-18 18:59:17 -0300

    Usando o gerador de formulário do Laravel na tela de login.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,28 +15,23 @@
           <div class="panel-body">
             <div class="row">
               <div class="col-lg-12">
-                <form id="login-form" action="{{ route('auth.login') }}" method="post" role="form">
-                  <div class="form-group{{ $errors->has('login') ? ' has-error' : '' }}">
-                    <input type="text" name="email" id="email" tabindex="1" class="form-control" placeholder="{{trans('tela_login.menu_email')}}" value="">
-                      @if ($errors->has('email'))
-                        <span class="help-block">{{ $errors->first('email') }}</span>
-                      @endif
+                {!! Form::open(array('route' => 'auth.login','data-parsley-validate' => '' )) !!}
+                <div class="form-group">
+                  {!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => trans('tela_login.menu_email')]) !!}
+                </div>
+                <div class="form-group">
+                  {!! Form::password('password', ['class' => 'form-control', 'tabindex' => '2', 'placeholder' => trans('tela_login.menu_senha')]) !!}
+                </div>
+                <div class="form-group">
+                  <div class="row">
+                    <div class="col-sm-6 col-sm-offset-3">
+                      {!! Form::submit(trans('tela_login.menu_entrar'), ['class' => 'form-control btn btn-login']) !!}
                     </div>
-                  <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="{{trans('tela_login.menu_senha')}}">
-                    @if ($errors->has('password'))
-                      <span class="help-block">{{ $errors->first('password') }}</span>
-                    @endif
                   </div>
-                    <div class="col-xs-12" style="height:20px;"></div>
-                    <div class="form-group">
-                      <div class="row">
-                        <div class="col-sm-6 col-sm-offset-3">
-                          <input type="submit" name = "login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="{{trans('tela_login.menu_entrar')}}">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="form-group">
+                </div>  
+                {!! Form::close() !!}
+              </div>
+              <div class="form-group">
                       <div class="row">
                         <div class="col-lg-12">
                           <div class="text-center">
@@ -45,10 +40,6 @@
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <input type="hidden" name="_token" value="{{ Session::token() }}">
-                </form>
-              </div>
             </div>
           </div>
         </div>
