9cf667e31be6cd8575846e19bed8c38aaf6645be
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-19 18:04:13 -0300

    Tradução da tela de dados pessoais.

---
+++ resources/lang/en/dados_pessoais.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+*Tradução dos textos para Inglês
+ */
+
+return [
+    'nome' => 'Full Name',
+    'rg' => 'RG',
+    'data_nascimento' => 'Birthday',
+    'endereco' => 'Address',
+    'cidade' => 'City',
+    'estado' => 'Estate',
+    'cep' => 'ZIP Code',
+    'pais' => 'Country',
+    'celular' => 'Celphone',
+    'menu_enviar' => 'Send',
+    'menu_registro_sucesso_parte_inicial' => 'Account succesfully created. It was send an e-mail to: ',
+    'menu_registro_sucesso_parte_final' => 'with a link to activate your account.',
+
+];
---
+++ resources/lang/pt-br/dados_pessoais.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+*Tradução dos textos para Português
+ */
+
+return [
+    'nome' => 'Nome',
+    'rg' => 'RG',
+    'data_nascimento' => 'Data de Nascimento',
+    'endereco' => 'Endereço',
+    'cidade' => 'Cidade',
+    'estado' => 'Estado',
+    'cep' => 'CEP',
+    'pais' => 'País',
+    'celular' => 'Celular',
+    'menu_enviar' => 'Enviar',
+    'menu_registro_sucesso_parte_inicial' => 'Account succesfully created. It was send an e-mail to: ',
+    'menu_registro_sucesso_parte_final' => 'with a link to activate your account.',
+
+];
