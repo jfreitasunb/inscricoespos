9849a6ddf6ca36ba3b3d77fb847485897338a0ee
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-20 12:30:46 -0300

    Remoção de seeds antigos.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,37 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {   
-        // Estados
-        $this->call('EstadosTableSeeder');
-
-        // Cidades
-        $this->call('CidadesAcreSeeder');
-        $this->call('CidadesAlagoasSeeder');
-        $this->call('CidadesAmapaSeeder');
-        $this->call('CidadesAmazonasSeeder');
-        $this->call('CidadesBahiaSeeder');
-        $this->call('CidadesCearaSeeder');
-        $this->call('CidadesDistritoFederalSeeder');
-        $this->call('CidadesEspiritoSantoSeeder');
-        $this->call('CidadesGoiasSeeder');
-        $this->call('CidadesMaranhaoSeeder');
-        $this->call('CidadesMatoGrossoSeeder');
-        $this->call('CidadesMatoGrossoDoSulSeeder');
-        $this->call('CidadesMinasGeraisSeeder');
-        $this->call('CidadesParaSeeder');
-        $this->call('CidadesParaibaSeeder');
-        $this->call('CidadesParanaSeeder');
-        $this->call('CidadesPernambucoSeeder');
-        $this->call('CidadesPiauiSeeder');
-        $this->call('CidadesRioDeJaneiroSeeder');
-        $this->call('CidadesRioGrandeDoNorteSeeder');
-        $this->call('CidadesRioGrandeDoSulSeeder');
-        $this->call('CidadesRondoniaSeeder');
-        $this->call('CidadesRoraimaSeeder');
-        $this->call('CidadesSantaCatarinaSeeder');
-        $this->call('CidadesSaoPauloSeeder');
-        $this->call('CidadesSergipeSeeder');
-        $this->call('CidadesTocantinsSeeder');
         
         $user = ['email' => 'jfreitas@mat.unb.br', 'password' => bcrypt('1'), 'user_type' => 'admin' , 'ativo' => '1', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
         $db_user = DB::table('users')->insert($user);
@@ -53,13 +22,10 @@ class DatabaseSeeder extends Seeder
             'id_user' => '1',
             'nome' => 'Jota',
             'numerorg' => '1',
-            'emissorrg' => '1', 
-            'cpf' => '1',
             'endereco' => '1',
             'cidade' => '1',
             'cep' => '1',
             'estado' => '1',
-            'telefone' => '1',
             'celular' => '1',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
@@ -69,14 +35,11 @@ class DatabaseSeeder extends Seeder
         $dados_coord = [
             'id_user' => '2',
             'nome' => 'Coordenação de Pós-Graduação',
-            'numerorg' => '2',
-            'emissorrg' => '2', 
-            'cpf' => '2',
+            'numerorg' => '2', 
             'endereco' => '2',
             'cidade' => '2',
             'cep' => '2',
             'estado' => '2',
-            'telefone' => '2',
             'celular' => '2',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
@@ -93,7 +56,7 @@ class DatabaseSeeder extends Seeder
             
             $disciplina = ['id_area_pos' => $i, 'nome' => $lista_areas_pos[$i], 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
             $db_disciplina = DB::table('area_pos_mat')->insert($disciplina);
-        }
+    }
 
 
         $programa_pos1 = ['id_programa_pos' => 1, 'tipo_programa_pos' => 'Mestrado', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
@@ -103,6 +66,10 @@ class DatabaseSeeder extends Seeder
         $programa_pos2 = ['id_programa_pos' => 2, 'tipo_programa_pos' => 'Doutorado', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")];
 
         $db_programa_pos2 = DB::table('programa_pos_mat')->insert($programa_pos2);
+
+        $this->call(CountriesTableSeeder::class);
+        $this->call(StatesTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
 
     }
 }
