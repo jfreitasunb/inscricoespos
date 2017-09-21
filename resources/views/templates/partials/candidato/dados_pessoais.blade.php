879b4fe8192d7cc52cd6824723653c12183c8ecf
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-21 13:05:48 -0300

    CSS.

--- resources/views/templates/partials/candidato/dados_pessoais.blade.php
+++ resources/views/templates/partials/candidato/dados_pessoais.blade.php
@@ -2,7 +2,7 @@
 
 @section('stylesheets')
   {!! Html::style( asset('css/parsley.css') ) !!}
-  {!! Html::style( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') ) !!}  
+  {!! Html::style( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') ) !!}
   {!! Html::style( asset('bower_components/moment/locale/fr.js') ) !!}
 @endsection
 
