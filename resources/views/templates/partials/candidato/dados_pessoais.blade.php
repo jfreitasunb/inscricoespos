9105a87bdb6d5ae814061832182b1ec58fa2bd7f
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-21 13:03:39 -0300

    Exibir seletor de data de nascimento.

--- resources/views/templates/partials/candidato/dados_pessoais.blade.php
+++ resources/views/templates/partials/candidato/dados_pessoais.blade.php
@@ -1,7 +1,9 @@
 @extends('templates.default')
 
 @section('stylesheets')
-  <link href="{{ asset('css/parsley.css') }}" rel="stylesheet">
+  {!! Html::style( asset('css/parsley.css') ) !!}
+  {!! Html::style( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') ) !!}  
+  {!! Html::style( asset('bower_components/moment/locale/fr.js') ) !!}
 @endsection
 
 @section('dados_pessoais')
@@ -141,4 +143,10 @@
         
    });
 </script>
+  {!! Html::script( asset('bower_components/moment/min/moment.min.js') ) !!}
+  {!! Html::script( asset('bower_components/moment/locale/pt-br.js') ) !!}
+  {!! Html::script( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') ) !!}
+  {!! Html::script( asset('bower_components/moment/locale/fr.js') ) !!}
+  {!! Html::script( asset('js/datepicker.js') ) !!}
+  {!! Html::script( asset('js/parsley.min.js') ) !!}
 @endsection
