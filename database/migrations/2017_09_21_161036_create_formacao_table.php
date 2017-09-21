24f2001e6d3ce261ecd36696a115709f8e121d95
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-21 16:18:20 -0300

    Migration para a tabela de formação.

---
+++ database/migrations/2017_09_21_161036_create_formacao_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormacaoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formacao', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tipo',50);
+            $table->string('nivel',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formacao');
+    }
+}
