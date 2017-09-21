c5f93f1105cfec4a661bbcea5e1414c182ec7623
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-09-21 18:25:46 -0300

    Iniciando o template de escolha.

--- resources/views/templates/partials/candidato/escolha_candidato.blade.php
+++ resources/views/templates/partials/candidato/escolha_candidato.blade.php
@@ -5,7 +5,21 @@
 @endsection
 
 @section('escolha_monitoria')
+  <fieldset class="scheduler-border">
+    <legend class="scheduler-border">Monitorias disponíveis</legend>
+      {!! Form::open(array('route' => 'dados.escolhas', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!}
+
+
       
+      <div class="form-group">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3 text-center">
+            {!! Form::submit(trans('tela_dados_pessoais.menu_enviar'), ['class' => 'btn btn-primary btn-lg register-submit']) !!}
+          </div>
+        </div>
+      </div>
+      {!! Form::close() !!}
+  </fieldset>
 @endsection
 
 @section('scripts')
