575aad63202dfed05dbd6e6c8767e26ed0762780
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-06 17:34:05 -0300

    Seção para informar se tem interesse em bolsa.

--- resources/views/templates/partials/candidato/escolha_candidato.blade.php
+++ resources/views/templates/partials/candidato/escolha_candidato.blade.php
@@ -12,25 +12,38 @@
       <div class="row">
         @foreach($programa_para_inscricao as $programa => $key)
           <div class="col-md-4">
-            <label class="radio-inline">{!! Form::radio('programa_pretendido', $programa, ['class' => 'col-md-4 control-label']) !!} {{$key}}</label>
+            <label class="radio-inline">{!! Form::radio('programa_pretendido', $programa, false , ['class' => 'col-md-4 control-label']) !!} {!! $key !!}</label>
           </div>
         @endforeach
         @if(isset($areas_pos))
-          <div class="col-md-4">
+          <div class="col-md-3">
             <label class="radio">{!! Form::select('areas_pos', $areas_pos, ['class' => 'col-md-4 control-label']) !!}</label>
           </div>    
         @endif
       </div>
+  </fieldset>
+
+  <fieldset class="scheduler-border">
+    <legend class="scheduler-border">{{trans('tela_escolha_candidato.interesse_bolsa')}}</legend>
+      <div class="row">
+        <div class="col-md-4">
+          <label class="radio-inline">{!! Form::radio('interesse_bolsa', 0, false, ['class' => 'col-md-4 control-label']) !!}Não</label>
+        </div>
+        <div class="col-md-4">
+          <label class="radio-inline">{!! Form::radio('interesse_bolsa', 1, false, ['class' => 'col-md-4 control-label']) !!}Sim</label>
+        </div>
+      </div>
+  </fieldset>
       
       <div class="form-group">
         <div class="row">
           <div class="col-md-6 col-md-offset-3 text-center">
-            {!! Form::submit(trans('tela_dados_pessoais.menu_enviar'), ['class' => 'btn btn-primary btn-lg register-submit']) !!}
+            {!! Form::submit(trans('tela_escolha_candidato.menu_enviar'), ['class' => 'btn btn-primary btn-lg register-submit']) !!}
           </div>
         </div>
       </div>
       {!! Form::close() !!}
-  </fieldset>
+  
 @endsection
 
 @section('scripts')
