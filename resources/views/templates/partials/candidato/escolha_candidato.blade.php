bd688adf3e63315dfebec7b542f5ad061c19bdab
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-06 18:22:48 -0300

    Informando e validando os dados dos recomendantes.

--- resources/views/templates/partials/candidato/escolha_candidato.blade.php
+++ resources/views/templates/partials/candidato/escolha_candidato.blade.php
@@ -5,9 +5,10 @@
 @endsection
 
 @section('escolha_monitoria')
+{!! Form::open(array('route' => 'dados.escolhas', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!}
   <fieldset class="scheduler-border">
     <legend class="scheduler-border">{{trans('tela_escolha_candidato.programa_disponivel')}}</legend>
-      {!! Form::open(array('route' => 'dados.escolhas', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!}
+      
       
       <div class="row">
         @foreach($programa_para_inscricao as $programa => $key)
@@ -40,43 +41,43 @@
       <div class="row">
         <div class="col-md-4">
           {!! Form::label('recomendante_1', '1) '.trans('tela_escolha_candidato.nome'), ['class' => 'control-label emailrecomendante']) !!}
-          {!! Form::text('nome_recomendante[]',null , ['class' => 'control-label emailrecomendante']) !!}
+          {!! Form::text('nome_recomendante[]',null , ['class' => 'control-label emailrecomendante', 'required' => '']) !!}
         </div>
         <div class="col-md-4">
           {!! Form::label('email_1', 'E-mail: ', ['class' => 'control-label']) !!}
-          {!! Form::text('email_recomendante[]',null , ['class' => 'control-label emailrecomendante']) !!}
+          {!! Form::text('email_recomendante_1',null , ['id' => 'email_recomendante_1', 'class' => 'control-label emailrecomendante', 'required' => '', 'data-parsley-type' => 'email']) !!}
         </div>
         <div class="col-md-4">
           {!! Form::label('email_1', trans('tela_escolha_candidato.confirmar_email'), ['class' => 'control-label']) !!}
-          {!! Form::text('email_recomendante[]',null , ['class' => 'control-label emailrecomendante']) !!}
+          {!! Form::text('confirmar_email_recomendante_1',null , ['class' => 'control-label emailrecomendante', 'required' => '', 'data-parsley-type' => 'email', 'data-parsley-equalto' => '#email_recomendante_1']) !!}
         </div>
       </div>
       <div class="row">
         <div class="col-md-4">
           {!! Form::label('recomendante_2', '2) '.trans('tela_escolha_candidato.nome'), ['class' => 'control-label emailrecomendante']) !!}
-          {!! Form::text('nome_recomendante[]',null , ['class' => 'control-label emailrecomendante']) !!}
+          {!! Form::text('nome_recomendante[]',null , ['class' => 'control-label emailrecomendante', 'required' => '']) !!}
         </div>
         <div class="col-md-4">
           {!! Form::label('email_2', 'E-mail: ', ['class' => 'control-label']) !!}
-          {!! Form::text('email_recomendante[]',null , ['class' => 'control-label emailrecomendante']) !!}
+          {!! Form::text('email_recomendante_2',null , ['id' => 'email_recomendante_2', 'class' => 'control-label emailrecomendante', 'required' => '', 'data-parsley-type' => 'email']) !!}
         </div>
         <div class="col-md-4">
           {!! Form::label('email_2', trans('tela_escolha_candidato.confirmar_email'), ['class' => 'control-label']) !!}
-          {!! Form::text('email_recomendante[]',null , ['class' => 'control-label emailrecomendante']) !!}
+          {!! Form::text('confirmar_email_recomendante_2',null , ['class' => 'control-label emailrecomendante', 'required' => '', 'data-parsley-type' => 'email', 'data-parsley-equalto' => '#email_recomendante_2']) !!}
         </div>
       </div>
       <div class="row">
         <div class="col-md-4">
           {!! Form::label('recomendante_3', '3) '.trans('tela_escolha_candidato.nome'), ['class' => 'control-label emailrecomendante']) !!}
-          {!! Form::text('nome_recomendante[]',null , ['class' => 'control-label emailrecomendante']) !!}
+          {!! Form::text('nome_recomendante[]',null , ['class' => 'control-label emailrecomendante', 'required' => '']) !!}
         </div>
         <div class="col-md-4">
           {!! Form::label('email_3', 'E-mail: ', ['class' => 'control-label']) !!}
-          {!! Form::text('email_recomendante[]',null , ['class' => 'control-label emailrecomendante']) !!}
+          {!! Form::text('email_recomendante_3',null , ['id' => 'email_recomendante_3', 'class' => 'control-label emailrecomendante', 'required' => '', 'data-parsley-type' => 'email']) !!}
         </div>
         <div class="col-md-4">
           {!! Form::label('email_3', trans('tela_escolha_candidato.confirmar_email'), ['class' => 'control-label']) !!}
-          {!! Form::text('email_recomendante[]',null , ['class' => 'control-label emailrecomendante']) !!}
+          {!! Form::text('confirmar_email_recomendante_3',null , ['class' => 'control-label emailrecomendante', 'required' => '', 'data-parsley-type' => 'email', 'data-parsley-equalto' => '#email_recomendante_3']) !!}
         </div>
       </div>
   </fieldset>
