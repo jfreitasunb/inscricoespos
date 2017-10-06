12f376366bb4e6c949ef613bcb941e8fb97f0e61
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-06 17:34:17 -0300

    Termos da tela de escolha de monitoria.

--- resources/lang/pt-br/tela_escolha_candidato.php
+++ resources/lang/pt-br/tela_escolha_candidato.php
@@ -8,12 +8,8 @@
 return [
 	'tela_escolhas' => 'Escolha',
 	'programa_disponivel' => 'Programa Pretendido',
-    'menu_registro' => 'Registrar',
-    'menu_nome' => 'Nome Completo',
-    'menu_email' => 'E-mail',
-    'menu_confirma_email' => 'Confirmar E-mail',
-    'menu_senha' => 'Senha',
-    'menu_confira_senha' => 'Confirmar Senha',
+    'interesse_bolsa' => 'Tem interesse em bolsa?',
+    'menu_enviar' => 'Enviar',
     'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
     'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
 
