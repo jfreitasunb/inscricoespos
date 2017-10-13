4bf7e9dcfee8b51e0d6b185660b5ab04bce3af12
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-13 16:54:59 -0300

    Campos que podem ser preenchidos.

--- app/Models/ContatoRecomendante.php
+++ app/Models/ContatoRecomendante.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContatoRecomendante extends Model
 {
-    //
+    protected $primaryKey = 'id';
+
+    protected $table = 'escolhas_candidato';
+
+    protected $fillable = [
+        'id_recomendante',
+        'id_inscricao_pos',
+    ];
 }
