2941d5737d2df4a6329217fd37ebf1a58e4da798
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-13 18:55:20 -0300

    Texto da parte de concordar com as regras.

--- resources/lang/pt-br/tela_escolha_candidato.php
+++ resources/lang/pt-br/tela_escolha_candidato.php
@@ -13,10 +13,11 @@ return [
     'nome' => 'Nome: ',
     'confirmar_email' => 'Confirmar e-mail:',
     'motivacao' => 'Escreva no espaço abaixo a sua motivação e expectativa em relação ao programa pretendido:',
+    'declaracao_concordancia' => 'Declaração de conhecimento das regras da Monitoria da UnB',
     'concordancia_1' => 'O solicitante declara formalmente que está de acordo com as ',
     'concordancia_2' => ' e que está ciente que as decisões da Comissão de Seleção da Matemática serão irrecorríveis, admitindo-se pedido de reconsideração 
-para a própria Comissão nas hipóteses de erros procedimentais ou materiais. 
-(declaração feita em observância aos artigos 297-299 do Código Penal Brasileiro)',
+para a própria Comissão nas hipóteses de erros procedimentais ou materiais. (declaração feita em observância aos artigos 297-299 do Código Penal Brasileiro)',
+	'texto_link_edital' => 'normas estabelecidas no Edital do Programa de Matemática da UnB',
 	'concordancia_3' => 'Li e estou de acordo com a declaração acima.',
     'menu_enviar' => 'Enviar',
     'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
