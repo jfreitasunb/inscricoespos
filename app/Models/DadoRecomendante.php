8605663bb648f427f3d5e96601e65862bebff7a6
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-13 18:16:08 -0300

    Campos preenchíveis na tabela de dados dos recomendantes.

--- app/Models/DadoRecomendante.php
+++ app/Models/DadoRecomendante.php
@@ -4,7 +4,19 @@ namespace Posmat\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class DadosRecomendante extends Model
+class DadoRecomendante extends Model
 {
-    //
+    protected $primaryKey = 'id';
+
+    protected $table = 'dados_recomendantes';
+
+    protected $fillable = [
+        'nome_recomendante',
+        'instituicao_recomendante',
+        'titulacao_recomendante',
+        'area_recomendante',
+        'ano_titulacao',
+        'inst_obtencao_titulo',
+        'endereco_recomendante',
+    ];
 }
