aeccae19602cc4fc257fa7fd9995af0be9b17ca9
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-17 15:12:53 -0200

    Remoção de termos não utilizados.

--- resources/lang/pt-br/tela_dados_academicos.php
+++ resources/lang/pt-br/tela_dados_academicos.php
@@ -10,23 +10,10 @@ return [
     'curso_graduacao' => 'Graduação',
 	'curso' => 'Curso',
     'tipo_curso' => 'Tipo',
-    'instituicao_graduacao' => 'Instituição',
+    'instituicao' => 'Instituição',
     'ano_conclusao' => 'Ano de conclusão (ou previsão)',
-    'interesse_bolsa' => 'Tem interesse em bolsa?',
-    'vinculo_empregaticio' => 'Possui vínculo empregatício?',
-    'recomendante' => 'Nome completo e e-mail de três professores que, por sua solicitação, enviarão cartas de recomendação. ',
-    'nome' => 'Nome: ',
-    'confirmar_email' => 'Confirmar e-mail:',
-    'motivacao' => 'Escreva no espaço abaixo a sua motivação e expectativa em relação ao programa pretendido:',
-    'declaracao_concordancia' => 'Declaração de conhecimento das regras da Monitoria da UnB',
-    'concordancia_1' => 'O solicitante declara formalmente que está de acordo com as ',
-    'concordancia_2' => ' e que está ciente que as decisões da Comissão de Seleção da Matemática serão irrecorríveis, admitindo-se pedido de reconsideração 
-para a própria Comissão nas hipóteses de erros procedimentais ou materiais. (declaração feita em observância aos artigos 297-299 do Código Penal Brasileiro)',
-	'texto_link_edital' => 'normas estabelecidas no Edital do Programa de Matemática da UnB',
-	'concordancia_3' => 'Li e estou de acordo com a declaração acima.',
+    'curso_pos' => 'Pós-Graduação',
     'menu_enviar' => 'Enviar',
     'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
     'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
-
-
 ];
