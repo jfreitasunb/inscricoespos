1a580adcc4005f2943bf04631e3edc077af4088e
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-17 18:47:10 -0200

    Remoção de termos não utilizados e acréscimo de termos para os arquivos.

--- resources/lang/pt-br/tela_finalizar_inscricao.php
+++ resources/lang/pt-br/tela_finalizar_inscricao.php
@@ -7,14 +7,9 @@
 
 return [
 	'tela_finaliza' => 'Finalizar Inscrição',
-	'programa_disponivel' => 'Programa Pretendido',
-    'interesse_bolsa' => 'Tem interesse em bolsa?',
-    'vinculo_empregaticio' => 'Possui vínculo empregatício?',
-    'recomendante' => 'Nome completo e e-mail de três professores que, por sua solicitação, enviarão cartas de recomendação. ',
-    'nome' => 'Nome: ',
-    'confirmar_email' => 'Confirmar e-mail:',
     'motivacao' => 'Escreva no espaço abaixo a sua motivação e expectativa em relação ao programa pretendido:',
-    'declaracao_concordancia' => 'Declaração de conhecimento das regras da Monitoria da UnB',
+    'documentos_pessoais' => 'RG e foto em um mesmo arquivo',
+    'historico' => 'Histórico da maior titulação',
     'concordancia_1' => 'O solicitante declara formalmente que está de acordo com as ',
     'concordancia_2' => ' e que está ciente que as decisões da Comissão de Seleção da Matemática serão irrecorríveis, admitindo-se pedido de reconsideração 
 para a própria Comissão nas hipóteses de erros procedimentais ou materiais. (declaração feita em observância aos artigos 297-299 do Código Penal Brasileiro)',
