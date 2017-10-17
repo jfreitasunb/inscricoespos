b4a5f3db33dcfa3dbfa7888a5b1402b8d3a481cc
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-17 17:53:59 -0200

    Mudança de nomes dos menus.

--- resources/views/templates/partials/candidato/menu_candidato.blade.php
+++ resources/views/templates/partials/candidato/menu_candidato.blade.php
@@ -11,9 +11,9 @@
     <div class="collapse navbar-collapse" id="bar1">
       <ul class="nav navbar-nav">
         <li class="{{ Route::currentRouteNamed('dados.pessoais') ? 'active' : '' }}"><a href="{{ route('dados.pessoais') }}">{{trans('tela_dados_pessoais.tela_dados_pessoais')}}</a></li>
-        <li class="{{ Route::currentRouteNamed('dados.bancarios') ? 'active' : '' }}"><a href="{{ route('dados.bancarios') }}">Dados Bancários</a></li>
-        <li class="{{ Route::currentRouteNamed('dados.academicos') ? 'active' : '' }}"><a href="{{ route('dados.academicos') }}">Dados Acadêmicos</a></li>
-        <li class="{{ Route::currentRouteNamed('dados.escolhas') ? 'active' : '' }}"><a href="{{ route('dados.escolhas') }}">{{trans('tela_escolha_candidato.tela_escolhas')}}</a></li>
+        <li class="{{ Route::currentRouteNamed('dados.academicos') ? 'active' : '' }}"><a href="{{ route('dados.academicos') }}">{{ trans('tela_dados_academicos.tela_dados_academicos') }}</a></li>
+        <li class="{{ Route::currentRouteNamed('dados.escolhas') ? 'active' : '' }}"><a href="{{ route('dados.escolhas') }}">{{ trans('tela_escolha_candidato.tela_escolhas') }}</a></li>
+        <li class="{{ Route::currentRouteNamed('finalizar.inscricao') ? 'active' : '' }}"><a href="{{ route('finalizar.inscricao') }}">{{ trans('tela_finalizar_inscricao.tela_finaliza') }}</a></li>
         <li class="{{ Route::currentRouteNamed('auth.logout') ? 'active' : '' }}"><a href="{{ route('auth.logout') }}">Sair</a></li>
       </ul>
     </div>
