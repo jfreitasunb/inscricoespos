a57a8b894d5d5fbe9a478d07680bf1b6513dc510
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-18 17:42:09 -0200

    Textos da tela de finalização.

--- resources/lang/pt-br/tela_finalizar_inscricao.php
+++ resources/lang/pt-br/tela_finalizar_inscricao.php
@@ -7,15 +7,9 @@
 
 return [
 	'tela_finaliza' => 'Finalizar Inscrição',
-    'motivacao' => 'Escreva no espaço abaixo a sua motivação e expectativa em relação ao programa pretendido:',
-    'documentos_pessoais' => 'RG e foto em um mesmo arquivo',
-    'historico' => 'Histórico da maior titulação',
-    'concordancia_1' => 'O solicitante declara formalmente que está de acordo com as ',
-    'concordancia_2' => ' e que está ciente que as decisões da Comissão de Seleção da Matemática serão irrecorríveis, admitindo-se pedido de reconsideração 
-para a própria Comissão nas hipóteses de erros procedimentais ou materiais. (declaração feita em observância aos artigos 297-299 do Código Penal Brasileiro)',
-	'texto_link_edital' => 'normas estabelecidas no Edital do Programa de Matemática da UnB',
-	'concordancia_3' => 'Li e estou de acordo com a declaração acima.',
-    'menu_enviar' => 'Enviar',
+    'texto_finaliza' => 'No link abaixo você encontrará um arquivo PDF com os dados da sua inscrição. Confira todos os dados e caso encontre algum erro retorne à tela correspondente e efetue a correção. Após o envio definitivo da inscrição não será possível efetuar nenhuma mudanção.',
+    'ficha_inscricao' => 'Ficha de Inscrição',
+    'menu_envio_definitivo' => 'Envio Definitivo',
     'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
     'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
 
