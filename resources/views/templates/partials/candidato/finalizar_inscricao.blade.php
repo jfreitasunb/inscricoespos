4735462a5f9db406e442c3e3bf402662ed445a1d
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-18 18:30:14 -0200

    Opção do candidato cancelar o envio definitivo.

--- resources/views/templates/partials/candidato/finalizar_inscricao.blade.php
+++ resources/views/templates/partials/candidato/finalizar_inscricao.blade.php
@@ -24,7 +24,7 @@
       <div class="form-group">
         <div class="row">
           <div class="col-md-6 col-md-offset-3 text-center">
-            {!! Form::button(trans('tela_finalizar_inscricao.menu_envio_definitivo'), ['class' => 'btn btn-warning btn-lg register-submit']) !!}
+            {!! Form::button(trans('tela_finalizar_inscricao.menu_envio_definitivo'), ['class' => 'btn btn-warning btn-lg register-submit', 'onclick' => 'return archiveFunction(event)', 'id' => 'envio_definitivo']) !!}
           </div>
         </div>
       </div>
@@ -32,7 +32,32 @@
   
 @endsection
 
-@section('scripts')
+@section('post-script')
+<script>
+function archiveFunction(event) {
+event.preventDefault(); // prevent form submit
+var form = event.target.form; // storing the form
+        swal({
+  title: "Are you sure?",
+  text: "But you will still be able to retrieve this file.",
+  type: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#DD6B55",
+  confirmButtonText: "Yes, archive it!",
+  cancelButtonText: "No, cancel please!",
+}).then(function () {
+  form.submit();          // submitting the form when user press yes
+},function(dismiss){
+  if (dismiss === 'cancel') {
+    swal(
+      'Cancelled',
+      'Your imaginary file is safe :)',
+      'error'
+    )
+  } 
+});
+}
+</script>
   {!! Html::script( asset('js/parsley.min.js') ) !!}
   {!! Html::script( asset('i18n/pt-br.js') ) !!}
 @endsection
