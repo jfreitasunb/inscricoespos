602cc725125701364f3d996582140e80cebbd5b4
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-19 15:08:43 -0200

    Função para retornar a carta de motivação.

--- app/Models/CartaMotivacao.php
+++ app/Models/CartaMotivacao.php
@@ -13,4 +13,12 @@ class CartaMotivacao extends Model
     protected $fillable = [
         'motivacao',
     ];
+
+    public function retorna_carta_motivacao($id_user,$id_inscricao_pos)
+    {
+        $carta_motivacao = $this->where("id_user", $id_user)->where("id_inscricao_pos", $id_inscricao_pos)->get();
+
+        return $carta_motivacao;
+
+    }
 }
