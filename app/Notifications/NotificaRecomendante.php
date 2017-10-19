52efe372f33e489dbfdbe7530f907a4ce741797f
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-19 17:44:52 -0200

    Notificação de carta de recomendação.

---
+++ app/Notifications/NotificaRecomendante.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Posmat\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class NotificaRecomendante extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject(trans('mensagens_gerais.carta_recomendacao'))
+            ->line('Você está recebendo essa mensagem porquê você acabou de criar uma conta em nosso sistema.')
+            ->line('A ativação é necessária para poder efetuar login no sistema.')
+            ->action('Clique no link', url('register/verify', $this->token), 'para que sua conta seja ativada.')
+            ->line('Se você não criou nenhuma conta, por favor ignore essa mensagem.');
+    }
+}
