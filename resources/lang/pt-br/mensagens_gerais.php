75b9f830562599e434d2461c63c519275c685509
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-19 17:44:39 -0200

    Texto do e-mail.

--- resources/lang/pt-br/mensagens_gerais.php
+++ resources/lang/pt-br/mensagens_gerais.php
@@ -12,4 +12,6 @@ return [
     'informe_area' => 'Você deve informar a área do Doutorado.',
     'selecionar' => 'Por favor selecione',
     'mensagem_sucesso' => 'Dados salvos com sucesso',
+    'carta_recomendacao' => 'Carta de Recomendação ao MAT/UnB',
+    'motivo_mensagem' => 'Você está recebendo essa mensagem porquê você acabou de criar uma conta em nosso sistema.',
 ];
