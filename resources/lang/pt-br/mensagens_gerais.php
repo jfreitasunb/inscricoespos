c6efee27400b529d005e44aaf82062cab90877a3
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-19 17:56:07 -0200

    Mensagens.

--- resources/lang/pt-br/mensagens_gerais.php
+++ resources/lang/pt-br/mensagens_gerais.php
@@ -8,10 +8,11 @@
 return [
 
     'inscricao_inativa' => 'O período de inscrição já está encerrado ou ainda não começou.',
-    'inscricao_finalizada' => 'Você já finalizou sua inscrição. Não é possível fazer novas escolhas de disciplinas para candidatura à Monitoria do MAT.',
+    'inscricao_finalizada' => 'Você já finalizou sua inscrição. Não é possível alterar esses dados. Somente dados pessoais.',
     'informe_area' => 'Você deve informar a área do Doutorado.',
     'selecionar' => 'Por favor selecione',
     'mensagem_sucesso' => 'Dados salvos com sucesso',
+    'envio_final' => 'Sua inscrição foi envidada corretamente para nosso sistema.',
     'carta_recomendacao' => 'Carta de Recomendação ao MAT/UnB',
     'motivo_mensagem' => 'Você está recebendo essa mensagem porquê você acabou de criar uma conta em nosso sistema.',
 ];
