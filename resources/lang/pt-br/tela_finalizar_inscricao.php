81f725b0ca26612041ab75b05a00f736af8290f3
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-19 15:09:08 -0200

    Mais textos.

--- resources/lang/pt-br/tela_finalizar_inscricao.php
+++ resources/lang/pt-br/tela_finalizar_inscricao.php
@@ -9,6 +9,7 @@ return [
 	'tela_finaliza' => 'Finalizar Inscrição',
     'texto_finaliza' => 'No link abaixo você encontrará um arquivo PDF com os dados da sua inscrição. Confira todos os dados e caso encontre algum erro retorne à tela correspondente e efetue a correção. Após o envio definitivo da inscrição não será possível efetuar nenhuma mudanção.',
     'ficha_inscricao' => 'Ficha de Inscrição',
+    'falta_dados_academicos' => 'Você deve informar seus dados acadêmicos antes de finalizar a inscrição.',
     'menu_envio_definitivo' => 'Envio Definitivo',
     'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
     'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
