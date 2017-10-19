ade3bec87576107edc2f05fac4f2dae3131d429e
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-19 19:44:37 -0200

    Preparando mensagem para recomendante.

--- app/Notifications/NotificaRecomendante.php
+++ app/Notifications/NotificaRecomendante.php
@@ -16,11 +16,11 @@ class NotificaRecomendante extends Notification
      *
      * @return void
      */
-    public $token;
+    public $dados_email;
 
-    public function __construct($token)
+    public function __construct($dados_email)
     {
-        $this->token = $token;
+        $this->dados_email = $dados_email;
     }
 
     public function via($notifiable)
@@ -32,9 +32,12 @@ class NotificaRecomendante extends Notification
     {
         return (new MailMessage)
             ->subject(trans('mensagens_gerais.carta_recomendacao'))
-            ->line(trans('mensagens_gerais.motivo_mensagem'))
-            ->line('A ativação é necessária para poder efetuar login no sistema.')
-            ->action('Clique no link',  'para que sua conta seja ativada.')
-            ->line('Se você não criou nenhuma conta, por favor ignore essa mensagem.');
+            ->line(trans('mensagens_gerais.motivo_mensagem_1').$dados_email['nome_professor'].',')
+            ->line(trans('mensagens_gerais.motivo_mensagem_2').$dados_email['programa'].trans('mensagens_gerais.motivo_mensagem_3').$dados_email['nome_candidato'].'.')
+            ->line(trans('mensagens_gerais.motivo_mensagem_4').url('/'))
+            ->line(trans('mensagens_gerais.motivo_mensagem_5').$dados_email['email_recomendante'])
+            ->line(trans('mensagens_gerais.prazo_envio').$dados_email['prazo_envio'])
+            ->action(trans('mensagens_gerais.recupera_senha'),url('esqueci/senha'))
+            ->line(trans('mensagens_gerais.saudacoes_finais'));
     }
 }
