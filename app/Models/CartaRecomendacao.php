bbba30d5d29afe2f0f99cdff0dae7dd1e2e61ae9
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-20 11:59:35 -0200

    Model da carta de recomendação.

--- app/Models/CartaRecomendacao.php
+++ app/Models/CartaRecomendacao.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class CartaRecomendacao extends Model
 {
-    //
+    protected $primaryKey = 'id';
+
+    protected $table = 'cartas_recomendacoes';
+
+    protected $fillable = [
+        'tempo_conhece_candidato',
+        'circunstancia_1',
+        'circunstancia_2',
+        'circunstancia_3',
+        'circunstancia_4',
+        'circunstancia_outra',
+        'desempenho_academico',
+        'capacidade_aprender',
+        'capacidade_trabalhar',
+        'criatividade',
+        'curiosidade',
+        'esforco',
+        'expressao_escrita',
+        'expressao_oral',
+        'relacionamento',
+        'antecedentes_academicos',
+        'possivel_aproveitamento',
+        'informacoes_relevantes',
+        'como_aluno',
+        'como_orientando',
+        'outra_situacao',
+    ];
 }
