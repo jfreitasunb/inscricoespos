9ee8cbaaba84da0db3b62f0dae95040ee251b0f7
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-20 14:33:06 -0200

    Removendo variáveis desnecessárias.

--- app/Models/EscolhaCandidato.php
+++ app/Models/EscolhaCandidato.php
@@ -23,9 +23,7 @@ class EscolhaCandidato extends Model
 
 public function retorna_escolha_candidato($id_user,$id_inscricao_pos)
     {
-        $escolheu_monitoria = $this->where("id_user", $id_user)->where("id_inscricao_pos", $id_inscricao_pos)->get();
-
-        return $escolheu_monitoria;
+       return $this->where("id_user", $id_user)->where("id_inscricao_pos", $id_inscricao_pos)->get();
 
     }
 
