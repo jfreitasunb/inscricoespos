be7b780f2a490bd4b027b6cb1e4c54eb54451bae
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-20 09:02:56 -0200

    Passando array com os dados para o e-mail.

--- app/Notifications/NotificaRecomendante.php
+++ app/Notifications/NotificaRecomendante.php
@@ -16,11 +16,13 @@ class NotificaRecomendante extends Notification
      *
      * @return void
      */
-    public $dados_email;
+    public $dados_email = [];
 
-    public function __construct($dados_email)
+    public function __construct(array $dados_email)
     {
         $this->dados_email = $dados_email;
+
+
     }
 
     public function via($notifiable)
@@ -32,11 +34,11 @@ class NotificaRecomendante extends Notification
     {
         return (new MailMessage)
             ->subject(trans('mensagens_gerais.carta_recomendacao'))
-            ->line(trans('mensagens_gerais.motivo_mensagem_1').$dados_email['nome_professor'].',')
-            ->line(trans('mensagens_gerais.motivo_mensagem_2').$dados_email['programa'].trans('mensagens_gerais.motivo_mensagem_3').$dados_email['nome_candidato'].'.')
+            ->line(trans('mensagens_gerais.motivo_mensagem_1').$this->dados_email['nome_professor'].',')
+            ->line(trans('mensagens_gerais.motivo_mensagem_2').$this->dados_email['programa'].trans('mensagens_gerais.motivo_mensagem_3').$this->dados_email['nome_candidato'].'.')
             ->line(trans('mensagens_gerais.motivo_mensagem_4').url('/'))
-            ->line(trans('mensagens_gerais.motivo_mensagem_5').$dados_email['email_recomendante'])
-            ->line(trans('mensagens_gerais.prazo_envio').$dados_email['prazo_envio'])
+            ->line(trans('mensagens_gerais.motivo_mensagem_5').$this->dados_email['email_recomendante'])
+            ->line(trans('mensagens_gerais.prazo_envio').$this->dados_email['prazo_envio'])
             ->action(trans('mensagens_gerais.recupera_senha'),url('esqueci/senha'))
             ->line(trans('mensagens_gerais.saudacoes_finais'));
     }
