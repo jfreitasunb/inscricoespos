39b54cea2dc54734473ea90a3c2bdeac4778c163
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-20 18:59:37 -0200

    Retornando carta de motivação.

--- app/Models/CartaMotivacao.php
+++ app/Models/CartaMotivacao.php
@@ -16,7 +16,7 @@ class CartaMotivacao extends Model
 
     public function retorna_carta_motivacao($id_user,$id_inscricao_pos)
     {
-        $carta_motivacao = $this->where("id_user", $id_user)->where("id_inscricao_pos", $id_inscricao_pos)->get();
+        $carta_motivacao = $this->where("id_user", $id_user)->where("id_inscricao_pos", $id_inscricao_pos)->get()->first();
 
         return $carta_motivacao;
 
