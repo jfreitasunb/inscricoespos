e977d6ba51b63da95fd065a62a9ef4ff51b4596f
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-24 17:42:37 -0200

    Mais tradução.

--- resources/lang/pt-br/tela_cartas_pendentes.php
+++ resources/lang/pt-br/tela_cartas_pendentes.php
@@ -7,14 +7,14 @@
 
 return [
     'tela_pendentes' => 'Cartas Pendentes',
+    'selecionar' => 'Selecione',
 	'prazo_carta' => 'Prezado(a) recomendante, o prazo para envio das cartas está encerrado.',
-    'curso_graduacao' => 'Graduação',
-	'curso' => 'Curso',
-    'tipo_curso' => 'Tipo',
-    'instituicao' => 'Instituição',
-    'ano_conclusao' => 'Ano de conclusão (ou previsão)',
-    'curso_pos' => 'Pós-Graduação',
-    'menu_enviar' => 'Enviar',
+    'situacao_carta' => 'Status da carta',
+    'status_enviada' => 'Carta enviada',
+	'status_nao_enviada' => 'Carta não enviada',
+    'nome_candidato' => 'Nome do candidato',
+    'mensagem_status_cartas' => 'Selecione a carta que deseja preencher',
+    'menu_enviar' => 'Preencher carta',
     'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
     'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
 ];
