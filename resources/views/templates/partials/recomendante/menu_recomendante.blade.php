72de128b9fd79e78340992fd1ed531dcf27b6631
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-23 22:00:31 -0200

    Menu do recomendante.

--- resources/views/templates/partials/recomendante/menu_recomendante.blade.php
+++ resources/views/templates/partials/recomendante/menu_recomendante.blade.php
@@ -10,12 +10,11 @@
     </div>
     <div class="collapse navbar-collapse" id="bar1">
       <ul class="nav navbar-nav">
-        <li class="{{ Route::currentRouteNamed('dados.pessoais') ? 'active' : '' }}"><a href="{{ route('dados.pessoais') }}">{{trans('tela_dados_pessoais.tela_dados_pessoais')}}</a></li>
-        @liberamenu(Auth()->user())
-        <li class="{{ Route::currentRouteNamed('dados.academicos') ? 'active' : '' }}"><a href="{{ route('dados.academicos') }}">{{ trans('tela_dados_academicos.tela_dados_academicos') }}</a></li>
-        <li class="{{ Route::currentRouteNamed('dados.escolhas') ? 'active' : '' }}"><a href="{{ route('dados.escolhas') }}">{{ trans('tela_escolha_candidato.tela_escolhas') }}</a></li>
+        <li class="{{ Route::currentRouteNamed('dados.pessoais') ? 'active' : '' }}"><a href="{{ route('dados.pessoais') }}">{{trans('tela_recomendante_dados_pessoais.tela_dados_pessoais')}}</a></li>
+        @liberacarta(Auth()->user())
+        <li class="{{ Route::currentRouteNamed('cartas.pendentes') ? 'active' : '' }}"><a href="{{ route('cartas.pendentes') }}">{{ trans('tela_escolha_candidato.tela_escolhas') }}</a></li>
         <li class="{{ Route::currentRouteNamed('motivacao.documentos') ? 'active' : '' }}"><a href="{{ route('motivacao.documentos') }}">{{ trans('tela_motivacao_documentos.tela_motivacao_documentos') }}</a></li>
-        @endliberamenu
+        @endliberacarta
         @statuscarta
           <li class="{{ Route::currentRouteNamed('status.cartas') ? 'active' : '' }}"><a href="{{ route('status.cartas') }}">{{ trans('tela_status_cartas.status_cartas') }}</a></li>
         @endstatuscarta
