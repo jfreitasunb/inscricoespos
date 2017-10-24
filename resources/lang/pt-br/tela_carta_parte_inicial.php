a0574c49ff4b756a95d07e980243b667845a6874
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-24 18:30:16 -0200

    Tradução da parte inicial da carta.

---
+++ resources/lang/pt-br/tela_carta_parte_inicial.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+*Tradução dos textos para Português
+ */
+
+
+return [
+    'tela_pendentes' => 'Preencher Carta',
+    'tempo_conhece_candidato' => 'Conhece-o há quanto tempo (meses, anos)',
+    'circunstancia' => 'Conhece-o sob que circunstâncias',
+    'circunstancia_1' => 'Aulas',
+    'circunstancia_2' => 'Orientação',
+    'circunstancia_3' => 'Seminários',
+    'circunstancia_4' => 'Outra (Informe no campo abaixo)',
+    'tabela_avaliacao' => 'Procure avaliá-lo sob os itens abaixo',
+	'prazo_carta' => 'Prezado(a) recomendante, o prazo para envio das cartas está encerrado.',
+    'situacao_carta' => 'Status da carta',
+    'status_enviada' => 'Carta enviada',
+	'status_nao_enviada' => 'Carta não enviada',
+    'nome_candidato' => 'Nome do candidato',
+    'mensagem_status_cartas' => 'Selecione a carta que deseja preencher',
+    'menu_enviar' => 'Salvar',
+    'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
+    'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
+];
