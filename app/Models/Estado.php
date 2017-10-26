909e93ae525353dbae6cce1e7ce385449ef731f8
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-26 18:06:23 -0200

    Retornando o nome do estado pelo id.

--- app/Models/Estado.php
+++ app/Models/Estado.php
@@ -16,4 +16,9 @@ class Estado extends Model
         return $this->hasMany('Posmat\Models\Cidade');
     }
 
+    public function retorna_nome_estados_por_id($id_pais, $id_estado)
+    {
+    	return $this->select('name')->where('id_state',$id_estado)->where('country_id', $id_pais)->get()->first()->name;
+    }
+
 }
