995dfe7479e44385820e439dc656b4db2a1bd135
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-10-26 17:59:14 -0200

    Mudança no nome de tabelas.

---
+++ database/migrations/2017_09_20_163722_create_cidade_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('cidade', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('state_id')->unsigned()->default(0);
+            $table->foreign('state_id')->references('id_state')->on('states');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cidade');
+    }
+}
