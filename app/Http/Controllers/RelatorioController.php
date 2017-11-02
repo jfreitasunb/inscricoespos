dde5ee4198276fca231831b85e65c6dd1061ae50
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-02 11:19:03 -0200

    Formatando o nome do arquivo e removendo caracteres especiais.

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -233,8 +233,26 @@ public function geraRelatorio($id_inscricao_pos)
 
     File::isDirectory($local_relatorios) or File::makeDirectory($local_relatorios,077,true,true);
 
+    $string = 'Ë À Ì Â Í Ã Î Ä Ï Ç Ò È Ó É Ô Ê Õ Ö ê Ù ë Ú î Û ï Ü ô Ý õ â û ã ÿ ç';
 
-    $arquivo_relatorio_candidato = $local_relatorios.'Relatorio_'.$dados_candidato_para_relatorio['id_aluno'].'.pdf';
+    $normalizeChars = array(
+      'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
+      'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
+      'Ï'=>'I', 'Ñ'=>'N', 'Ń'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
+      'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a',
+      'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i',
+      'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ń'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
+      'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ƒ'=>'f',
+      'ă'=>'a', 'î'=>'i', 'â'=>'a', 'ș'=>'s', 'ț'=>'t', 'Ă'=>'A', 'Î'=>'I', 'Â'=>'A', 'Ș'=>'S', 'Ț'=>'T',
+    );
+
+
+    if (is_null($dados_candidato_para_relatorio['area_pos'])) {
+      $arquivo_relatorio_candidato = $local_relatorios.'Inscricao_'.$dados_candidato_para_relatorio['programa_pretendido'].'_'.strtr($dados_candidato_para_relatorio['nome'], $normalizeChars).'_'.$dados_candidato_para_relatorio['id_aluno'].'.pdf';
+    }else{
+      $arquivo_relatorio_candidato = $local_relatorios.'Inscricao_'.$dados_candidato_para_relatorio['programa_pretendido'].'_'.strtr($dados_candidato_para_relatorio['area_pos'], $normalizeChars).'_'.strtr($dados_candidato_para_relatorio['nome'], $normalizeChars).'_'.$dados_candidato_para_relatorio['id_aluno'].'.pdf';
+    }
+    
     $pdf = PDF::loadView('templates.partials.coordenador.pdf_relatorio', compact('dados_candidato_para_relatorio','recomendantes_candidato'));
     $pdf->save($arquivo_relatorio_candidato);
 
