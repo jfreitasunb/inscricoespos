28f5da1c5831f3e7039c0588623e5955b3dea8b3
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-05 15:48:17 -0200

    Configuração dos editais que tiveram inscrições online.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -53,6 +53,72 @@ class DatabaseSeeder extends Seeder
 
         $db_configura_inscricao_pos_1 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_1);
 
+        $configura_inscricao_pos_2 = ['inicio_inscricao' => '2013-02-25', 'fim_inscricao' => '2013-03-09', 'prazo_carta' => '2013-03-12','edital' => '2013-1', 'programa' => '1', 'id_coordenador' => '2','created_at' => '2013-02-20 14:00:00', 'updated_at' => '2013-02-20 14:00:00'];
+
+        $db_configura_inscricao_pos_2 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_2);
+
+        $configura_inscricao_pos_3 = ['inicio_inscricao' => '2013-05-27', 'fim_inscricao' => '2013-06-07', 'prazo_carta' => '2013-03-08','edital' => '2013-2', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2013-05-20 14:00:00', 'updated_at' => '2013-05-20 14:00:00'];
+
+        $db_configura_inscricao_pos_3 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_3);
+
+        $configura_inscricao_pos_4 = ['inicio_inscricao' => '2013-09-30', 'fim_inscricao' => '2013-11-11', 'prazo_carta' => '2013-11-15','edital' => '2013-3', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2013-09-20 14:00:00', 'updated_at' => '2013-09-20 14:00:00'];
+
+        $db_configura_inscricao_pos_4 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_4);
+
+        $configura_inscricao_pos_5 = ['inicio_inscricao' => '2014-02-17', 'fim_inscricao' => '2014-02-27', 'prazo_carta' => '2014-02-28','edital' => '2014-1', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2014-02-10 14:00:00', 'updated_at' => '2014-02-10 14:00:00'];
+
+        $db_configura_inscricao_pos_5 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_5);
+
+
+        $configura_inscricao_pos_6 = ['inicio_inscricao' => '2014-05-02', 'fim_inscricao' => '2014-05-30', 'prazo_carta' => '2014-06-01','edital' => '2014-2', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2014-04-25 14:00:00', 'updated_at' => '2014-04-25 14:00:00'];
+
+        $db_configura_inscricao_pos_6 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_6);
+
+        $configura_inscricao_pos_7 = ['inicio_inscricao' => '2014-10-01', 'fim_inscricao' => '2014-11-02', 'prazo_carta' => '2014-11-10','edital' => '2014-3', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2014-09-25 14:00:00', 'updated_at' => '2014-09-25 14:00:00'];
+
+        $db_configura_inscricao_pos_7 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_7);
+
+        $configura_inscricao_pos_10 = ['inicio_inscricao' => '2015-02-09', 'fim_inscricao' => '2015-02-20', 'prazo_carta' => '2015-02-22','edital' => '2015-1', 'programa' => '1', 'id_coordenador' => '2','created_at' => '2015-02-02 14:00:00', 'updated_at' => '2015-02-02 14:00:00'];
+
+        $db_configura_inscricao_pos_8 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_8);
+
+        $configura_inscricao_pos_9 = ['inicio_inscricao' => '2015-05-04', 'fim_inscricao' => '2015-05-28', 'prazo_carta' => '2015-06-01','edital' => '2015-2', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2015-04-20 14:00:00', 'updated_at' => '2015-04-20 14:00:00'];
+
+        $db_configura_inscricao_pos_9 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_9);
+
+        $configura_inscricao_pos_10 = ['inicio_inscricao' => '2015-10-05', 'fim_inscricao' => '2015-11-06', 'prazo_carta' => '2015-11-10','edital' => '2015-3', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2015-09-20 14:00:00', 'updated_at' => '2015-09-20 14:00:00'];
+
+        $db_configura_inscricao_pos_10 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_10);
+
+        $configura_inscricao_pos_11 = ['inicio_inscricao' => '2016-01-25', 'fim_inscricao' => '2015-02-03', 'prazo_carta' => '2016-02-06','edital' => '2016-1', 'programa' => '1', 'id_coordenador' => '2','created_at' => '2016-01-20 14:00:00', 'updated_at' => '2016-01-20 14:00:00'];
+
+        $db_configura_inscricao_pos_11 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_11);
+
+
+        $configura_inscricao_pos_12 = ['inicio_inscricao' => '2016-05-09', 'fim_inscricao' => '2015-05-30', 'prazo_carta' => '2016-06-04','edital' => '2016-2', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2016-04-20 14:00:00', 'updated_at' => '2016-04-20 14:00:00'];
+
+        $db_configura_inscricao_pos_12 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_12);
+
+
+        $configura_inscricao_pos_13 = ['inicio_inscricao' => '2016-09-26', 'fim_inscricao' => '2015-10-20', 'prazo_carta' => '2016-10-28','edital' => '2016-3', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2016-09-20 14:00:00', 'updated_at' => '2016-09-20 14:00:00'];
+
+        $db_configura_inscricao_pos_13 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_13);
+
+        $configura_inscricao_pos_14 = ['inicio_inscricao' => '2017-02-01', 'fim_inscricao' => '2017-02-10', 'prazo_carta' => '2017-02-15','edital' => '2017-1', 'programa' => '1', 'id_coordenador' => '2','created_at' => '2017-01-20 14:00:00', 'updated_at' => '2017-01-20 14:00:00'];
+
+
+        $db_configura_inscricao_pos_14 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_14);
+
+        $configura_inscricao_pos_15 = ['inicio_inscricao' => '2017-04-10', 'fim_inscricao' => '2017-05-08', 'prazo_carta' => '2017-05-16','edital' => '2017-2', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2017-03-20 14:00:00', 'updated_at' => '2017-03-20 14:00:00'];
+
+        $db_configura_inscricao_pos_15 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_15);
+
+        $configura_inscricao_pos_16 = ['inicio_inscricao' => '2017-09-11', 'fim_inscricao' => '2017-10-13', 'prazo_carta' => '2017-10-21','edital' => '2017-3', 'programa' => '1_2', 'id_coordenador' => '2','created_at' => '2017-08-20 14:00:00', 'updated_at' => '2017-08-20 14:00:00'];
+
+        $db_configura_inscricao_pos_16 = DB::table('configura_inscricao_pos')->insert($configura_inscricao_pos_16);
+
+
+
 
         $lista_areas_pos = [1 => 'Álgebra', 2 => 'Análise', 3 => 'Análise Numérica', 4 => 'Geometria', 5 => 'Matemática Aplicada', 6 => 'Probabilidade', 7 => 'Sistemas Dinâmicos', 8 => 'Teoria da Computação', 9 => 'Teoria dos Números'];
 
