e7f7a88d8b242fae1611ed47afe0c0033871e0b5
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-05 19:55:24 -0200

    Migrando os usuários para o sistema novo.

--- app/Http/Controllers/MigracaoController.php
+++ app/Http/Controllers/MigracaoController.php
@@ -8,6 +8,7 @@ use Mail;
 use Session;
 use File;
 use ZipArchive;
+use Purifier;
 use PDF;
 use Posmat\Http\Controllers\FPDFController;
 use Carbon\Carbon;
@@ -45,6 +46,23 @@ class MigracaoController extends BaseController
 {
   public function getMigracao()
   {
+    $users = DB::connection('pos2')->table('inscricao_pos_login')->get();
 
+    foreach ($users as $user) {
+      if ($user->coduser !=197) {
+        $novo_usuario = new User();
+        if ($user->coduser == 348 or $user->coduser == 567 or $user->coduser == 565 or $user->coduser == 566 or $user->coduser == 563 or $user->coduser == 557 or $user->coduser == 685 or $user->coduser == 632 or $user->coduser == 530 or $user->coduser == 989 or $user->coduser == 1339 or $user->coduser == 415 or $user->coduser == 470 or $user->coduser == 472 or $user->coduser == 2124 or $user->coduser == 2125 or $user->coduser == 667 or $user->coduser == 225 or $user->coduser == 350 or $user->coduser == 593 or $user->coduser == 1881) {
+          $novo_usuario->email = Purifier::clean(strtolower(trim($user->login)).'.duplicado');
+        }else{
+          $novo_usuario->email = Purifier::clean(strtolower(trim($user->login)));
+        }
+        $novo_usuario->password = bcrypt(trim($user->senha));
+        $novo_usuario->validation_code = null;
+        $novo_usuario->user_type = $user->status;
+        $novo_usuario->ativo = true;
+        $novo_usuario->save(); 
+      }
+    }
+    dd($users);
   }
 }
