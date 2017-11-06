f0d922ae8e8bd700e39d0e6fd571fc8f82c881be
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-06 12:01:29 -0200

    Começo da migração dos dados pessoais.

--- app/Http/Controllers/MigracaoController.php
+++ app/Http/Controllers/MigracaoController.php
@@ -46,23 +46,38 @@ class MigracaoController extends BaseController
 {
   public function getMigracao()
   {
-    $users = DB::connection('pos2')->table('inscricao_pos_login')->get();
+    // $users = DB::connection('pos2')->table('inscricao_pos_login')->get();
 
-    foreach ($users as $user) {
-      if ($user->coduser !=197) {
-        $novo_usuario = new User();
-        if ($user->coduser == 348 or $user->coduser == 567 or $user->coduser == 565 or $user->coduser == 566 or $user->coduser == 563 or $user->coduser == 557 or $user->coduser == 685 or $user->coduser == 632 or $user->coduser == 530 or $user->coduser == 989 or $user->coduser == 1339 or $user->coduser == 415 or $user->coduser == 470 or $user->coduser == 472 or $user->coduser == 2124 or $user->coduser == 2125 or $user->coduser == 667 or $user->coduser == 225 or $user->coduser == 350 or $user->coduser == 593 or $user->coduser == 1881) {
-          $novo_usuario->email = Purifier::clean(strtolower(trim($user->login)).'.duplicado');
-        }else{
-          $novo_usuario->email = Purifier::clean(strtolower(trim($user->login)));
-        }
-        $novo_usuario->password = bcrypt(trim($user->senha));
-        $novo_usuario->validation_code = null;
-        $novo_usuario->user_type = $user->status;
-        $novo_usuario->ativo = true;
-        $novo_usuario->save(); 
-      }
+    // foreach ($users as $user) {
+    //   if ($user->coduser !=197) {
+    //     $novo_usuario = new User();
+    //     if ($user->coduser == 348 or $user->coduser == 567 or $user->coduser == 565 or $user->coduser == 566 or $user->coduser == 563 or $user->coduser == 557 or $user->coduser == 685 or $user->coduser == 632 or $user->coduser == 530 or $user->coduser == 989 or $user->coduser == 1339 or $user->coduser == 415 or $user->coduser == 470 or $user->coduser == 472 or $user->coduser == 2124 or $user->coduser == 2125 or $user->coduser == 667 or $user->coduser == 225 or $user->coduser == 350 or $user->coduser == 593 or $user->coduser == 1881) {
+    //       $novo_usuario->email = Purifier::clean(strtolower(trim($user->login)).'.duplicado');
+    //     }else{
+    //       $novo_usuario->email = Purifier::clean(strtolower(trim($user->login)));
+    //     }
+    //     $novo_usuario->password = bcrypt(trim($user->senha));
+    //     $novo_usuario->validation_code = null;
+    //     $novo_usuario->user_type = $user->status;
+    //     $novo_usuario->ativo = true;
+    //     $novo_usuario->save(); 
+    //   }
+    // }
+    
+    $users_candidato = DB::connection('pos2')->table('inscricao_pos_login')->where('status', 'candidato')->get();
+
+    foreach ($users_candidato as $candidato) {
+
+      if ($candidato->coduser == 348 or $candidato->coduser == 567 or $candidato->coduser == 565 or $candidato->coduser == 566 or $candidato->coduser == 563 or $candidato->coduser == 557 or $candidato->coduser == 685 or $candidato->coduser == 632 or $candidato->coduser == 530 or $candidato->coduser == 989 or $candidato->coduser == 1339 or $candidato->coduser == 415 or $candidato->coduser == 470 or $candidato->coduser == 472 or $candidato->coduser == 2124 or $candidato->coduser == 2125 or $candidato->coduser == 667 or $candidato->coduser == 225 or $candidato->coduser == 350 or $candidato->coduser == 593 or $candidato->coduser == 1881) {
+           $candidato->email = Purifier::clean(strtolower(trim($candidato->login)).'.duplicado');
+         }else{
+           $candidato->email = Purifier::clean(strtolower(trim($candidato->login)));
+         }
+
+         $dados_antigos_usuario = DB::connection('pos2')->table('inscricao_pos_dados_candidato')->where('id_aluno', $candidato->coduser)->get();
+
+         dd($dados_antigos_usuario);
     }
-    dd($users);
+    
   }
 }
