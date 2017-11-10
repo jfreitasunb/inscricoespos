6635c92497b287d4c6591d6be1654f72790a65d7
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-10 18:38:34 -0200

    Gerando o PDF com a carta enviada.

--- app/Http/Controllers/RecomendanteController.php
+++ app/Http/Controllers/RecomendanteController.php
@@ -6,6 +6,8 @@ use Auth;
 use DB;
 use Mail;
 use Session;
+use PDF;
+use File;
 use Validator;
 use Purifier;
 use Notification;
@@ -363,19 +365,44 @@ class RecomendanteController extends BaseController
 	{
 		$user = Auth::user();
 		
-		// $id_prof = $user->id_user;
-		
-		$id_prof = 25;
+		$id_prof = $user->id_user;
 
 		$indicacoes = new CartaRecomendacao;
 
-		$indicacoes_anteriores = $indicacoes->retorna_cartas_por_recomendante($id_prof);
+		$indicacoes_anteriores = $indicacoes->retorna_cartas_por_recomendante($id_prof)->paginate(2);
 
 		return view('templates.partials.recomendante.cartas_anteriores', compact('indicacoes_anteriores'));
 	}
 
-	public function postCartasAnteriores(Request $request)
+	public function GeraCartasAnteriores()
 	{
+		
+		$user = Auth::user();
+		
+		$id_prof = $user->id_user;
+
+		// $locais_arquivos_carta_enviadas = public_path("/relatorios/cartas_enviadas/");
+		
+		// File::isDirectory($locais_arquivos_carta_enviadas) or File::makeDirectory($locais_arquivos_carta_enviadas,077,true,true);
+
+		$id_inscricao_pos = (int) $_GET['id_inscricao_pos'];
+		
+		$id_aluno = (int) $_GET['id_aluno'];
+
+		$carta = new RelatorioController;
+
+		$dados_para_carta_enviada['nome_candidato'] = $carta->ConsolidaDadosPessoais($id_aluno)['nome'];
+
+		$dados_para_carta_enviada['programa_pretendido'] = $carta->ConsolidaEscolhaCandidato($id_aluno,$id_inscricao_pos)['programa_pretendido'];
+
+		$carta_enviada = $carta->ConsolidaCartaPorRecomendante($id_prof,$id_aluno,$id_inscricao_pos);
+
+		// $nome_arquivos_carta_envidada = $locais_arquivos_carta_enviadas.bin2hex(openssl_random_pseudo_bytes(15)).'.pdf';
+
+		
+		$pdf = PDF::loadView('templates.partials.recomendante.pdf_carta_enviada', compact('dados_para_carta_enviada','carta_enviada'));
+      	
+      	return $pdf->stream($dados_para_carta_enviada['nome_candidato'].'.pdf',array('Attachment'=> 1));
 
 	}
 }
