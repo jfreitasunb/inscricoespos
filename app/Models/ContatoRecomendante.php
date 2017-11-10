8911e5023fde1a425412bb02efa1b0439b93975f
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-10 14:59:49 -0200

    Voltanto ao nome antigo da coluna.

--- app/Models/ContatoRecomendante.php
+++ app/Models/ContatoRecomendante.php
@@ -19,8 +19,7 @@ class ContatoRecomendante extends Model
 
     public function retorna_candidatos_por_recomendante($id_prof)
     {
-        return $this->where('id_recomendante', $id_prof)->join('dados_pessoais', 'id_aluno','id_user')->join('escolhas_candidato', 'escolhas_candidato.id_user', 'contatos_recomendantes.id_aluno')->join('programa_pos_mat', 'id_programa_pos', 'escolhas_candidato.programa_pretendido')->get();
-        // ->join('escolhas_candidato', 'escolhas_candidato.id_user', 'contatos_recomendantes.id_aluno')->join('programa_pos_mat', 'id_programa_pos', 'escolhas_candidato.programa_pretendido')
+        return $this->where('id_recomendante', $id_prof)->join('dados_pessoais', 'dados_pessoais.id_user','contatos_recomendantes.id_user')->join('escolhas_candidato', 'escolhas_candidato.id_user', 'contatos_recomendantes.id_user')->join('programa_pos_mat', 'id_programa_pos', 'escolhas_candidato.programa_pretendido')->select('contatos_recomendantes.id_user')->get();
     }
 
     public function retorna_recomendante_candidato($id_user,$id_inscricao_pos)
