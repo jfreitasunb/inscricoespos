4442b92559fed6744cef803c0742ee3704e24589
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-13 18:18:35 -0200

    Retornando Laravel query builder.

--- app/Http/Controllers/DataTable/UserController.php
+++ app/Http/Controllers/DataTable/UserController.php
@@ -2,10 +2,15 @@
 
 namespace Posmat\Http\Controllers\DataTable;
 
+use Posmat\User;
+
 use Illuminate\Http\Request;
 use Posmat\Http\Controllers\Controller;
 
 class UserController extends DataTableController
 {
-    //
+    public function builder()
+    {
+    	return User::query();
+    }
 }
