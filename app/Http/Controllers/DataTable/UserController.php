02bfe2916cda268bfb6489bc99e230e0ee80d6e6
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-13 18:15:51 -0200

    UserController extende DataTableController.

--- app/Http/Controllers/DataTable/UserController.php
+++ app/Http/Controllers/DataTable/UserController.php
@@ -5,7 +5,7 @@ namespace Posmat\Http\Controllers\DataTable;
 use Illuminate\Http\Request;
 use Posmat\Http\Controllers\Controller;
 
-class UserController extends Controller
+class UserController extends DataTableController
 {
     //
 }
