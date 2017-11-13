46ebebc2ab633c8f5f3605413bc9abb386e8b94c
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-13 18:11:30 -0200

    Controller para user datatable.

---
+++ app/Http/Controllers/DataTable/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Posmat\Http\Controllers\DataTable;
+
+use Illuminate\Http\Request;
+use Posmat\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    //
+}
