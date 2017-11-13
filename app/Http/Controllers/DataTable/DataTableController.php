d39348dcbb2626555b46b14fb71e8bdf9227ef0e
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-13 18:26:03 -0200

    Construct.

--- app/Http/Controllers/DataTable/DataTableController.php
+++ app/Http/Controllers/DataTable/DataTableController.php
@@ -2,10 +2,31 @@
 
 namespace Posmat\Http\Controllers\DataTable;
 
+use Exception;
+
 use Illuminate\Http\Request;
 use Posmat\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Builder;
 
 abstract class DataTableController extends Controller
 {
+	protected $builder;
+
+	public function __construct()
+	{
+		$builder = $this->builder();
+
+		if (!$builder instanceof Builder) {
+			
+			throw new Exception("Entity builder not instance of Builder");
+
+		}
+	}
+
     abstract public function builder();
+
+    public function index()
+    {
+
+    }
 }
