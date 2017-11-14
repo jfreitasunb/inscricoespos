317f0bc269a8e9c861fd4d04e2e242a30e78b2a9
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-14 18:13:18 -0200

    Inicianda a datatable.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,7 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('data-table', require('./components/DataTable.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/views/templates/partials/admin/index.blade.php
@@ -0,0 +1,11 @@
+@extends('templates.default')
+
+@section('content')
+	<div class="containter">
+		<div class="row">
+			<div class="col-md-12">
+				<data-table></data-table>	
+			</div>
+		</div>
+	</div>
+@endsection
