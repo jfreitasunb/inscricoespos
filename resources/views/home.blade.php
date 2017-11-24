22b8d5c72a718c1fd43a74cf88e367484bdb1733
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-24 17:04:34 -0200

    Alterando a tela inicial.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,18 @@
 @section('inicio')
   <div class="row">
     <div class="col-md-6 col-md-offset-3">
-      <div class="panel panel-login">
-      <div class="row col-md-offset-2">
-        <div class="col-xs-3">
-           <a href="{{ route('lang.portuguese') }}">Português</a>
-        </div>
-        <div class="col-xs-3">
-           <a href="{{ route('lang.english') }}">English</a>
-        </div>
-        <div class="col-xs-3">
-           <a href="{{ route('lang.spanish') }}">Español</a>
-        </div>
+      <div class="panel-login btn-toolbar">
+        <div class="btn-group btn-group-justified">
+          <a href="{{ route('lang.portuguese') }}" class="btn btn-primary">Português</a>
+          <a href="{{ route('lang.english') }}" class="btn btn-primary">English</a>
+          <a href="{{ route('lang.spanish') }}" class="btn btn-primary">Spañol</a>
+        </div>  
       </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <div class="panel panel-login">
         <div class="panel-heading">
           <div class="row">
             <div class="col-xs-6">
@@ -25,9 +25,18 @@
             </div>
           </div>
         </div>
+        <div class="form-group">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="text-center">
+                <a href="{{ route('password.request') }}" tabindex="5" class="forgot-password">{{trans('tela_login.menu_esqueceu_senha')}}</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  
   <p class="bottom-three">
    
   </p>
