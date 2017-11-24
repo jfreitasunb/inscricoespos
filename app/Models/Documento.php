d3fdb691966f1c8f8cd31697dd371013cb4468b5
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-11-24 11:54:01 -0200

    Retorna os arquivos mais recentes enviados pelo candidato.

--- app/Models/Documento.php
+++ app/Models/Documento.php
@@ -23,13 +23,13 @@ class Documento extends Model
     	return $this->select('nome_arquivo')->where('id_user',$id_user)->orderBy('created_at','desc')->first();
     }
 
-    public function retorna_historico($id_user,$id_inscricao_pos)
+    public function retorna_historico($id_user)
     {
-        return $this->select('nome_arquivo')->where('id_user',$id_user)->where('tipo_arquivo','Histórico')->where('id_inscricao_pos',$id_inscricao_pos)->orderBy('created_at','desc')->first();
+        return $this->select('nome_arquivo')->where('id_user',$id_user)->where('tipo_arquivo','Histórico')->orderBy('created_at','desc')->first();
     }
 
-    public function retorna_documento($id_user,$id_inscricao_pos)
+    public function retorna_documento($id_user)
     {
-        return $this->select('nome_arquivo')->where('id_user',$id_user)->where('tipo_arquivo','Documentos')->where('id_inscricao_pos',$id_inscricao_pos)->orderBy('created_at','desc')->first();
+        return $this->select('nome_arquivo')->where('id_user',$id_user)->where('tipo_arquivo','Documentos')->orderBy('created_at','desc')->first();
     }
 }
