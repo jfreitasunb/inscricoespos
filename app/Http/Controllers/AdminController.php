3ccd11c9fe7e2be81ccc599a4c8b29aba2597185
jfreitasmat <jfreitas@mat.unb.br>
2017-11-29 19:34:44 -0200

    Reativando inscrição.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,11 +7,7 @@ use DB;
 use Mail;
 use Session;
 use Carbon\Carbon;
-use Posmat\Models\User;
-use Posmat\Models\ConfiguraInscricaoPos;
-use Posmat\Models\AreaPosMat;
-use Posmat\Models\ProgramaPosMat;
-use Posmat\Models\RelatorioController;
+use Posmat\Models\{User, ConfiguraInscricaoPos, AreaPosMat, ProgramaPosMat, RelatorioController, FinalizaInscricao};
 use Illuminate\Http\Request;
 use Posmat\Mail\EmailVerification;
 use Posmat\Http\Controllers\Controller;
@@ -244,5 +240,89 @@ class AdminController extends CoordenadorController
 
 		return redirect()->route('editar.inscricao');
 	}
+
+	public function getReativarInscricaoCandidato()
+	{
+
+		$modo_pesquisa = true;
+
+		$email_candidato = '';
+
+		return view('templates.partials.admin.reativar_inscricao_candidato')->with(compact('modo_pesquisa', 'email_candidato'));
+	}
+
+	public function getPesquisaCandidato($email_candidato)
+	{
+	
+		$user = new User;
+
+		return $user->retorna_user_por_email($email_candidato)->id_user;
+	}
+
+	public function getInscricaoParaReativar(Request $request)
+	{
+		$this->validate($request, [
+			'email_candidato' => 'required|email',
+		]);
+
+		$email_candidato = trim($request->email_candidato);
+
+		$id_user = $this->getPesquisaCandidato($email_candidato);
+
+		$edital = new ConfiguraInscricaoPos;
+
+		$edital_vigente = $edital->retorna_edital_vigente();
+
+		$finaliza_inscricao = new FinalizaInscricao;
+
+		$finalizou = $finaliza_inscricao->retorna_usuario_inscricao_finalizada($id_user, $id_user);
+
+		if (!is_null($finalizou)) {
+			
+
+			$modo_pesquisa = false;
+
+			return view('templates.partials.admin.reativar_inscricao_candidato')->with(compact('modo_pesquisa','finalizou','email_candidato'));
+
+		}else{
+			
+			notify()->flash('O candidato com e-mail: '.$email_candidato.' ainda não finalizou a inscrição!','error');
+
+			$modo_pesquisa = true;
+
+			return view('templates.partials.admin.reativar_inscricao_candidato')->with(compact('modo_pesquisa'));
+		}
+	}
+
+	public function postReativarInscricaoCandidato(Request $request)
+	{
+		$this->validate($request, [
+			'finalizada' => 'required',
+		]);
+
+		$id = (int)$request->id;
+		$id_inscricao_pos = (int)$request->id_inscricao_pos;
+		$id_user = (int)$request->id_user;
+		$email_candidato = $request->email_candidato;
+
+
+		$finalizada = (int)$request->finalizada;
+
+		if (!$finalizada) {
+			$inscricao_finalizada = new FinalizaInscricao;
+
+			DB::table('finaliza_inscricao')->where('id', $id)->where('id_user', $id_user)->where('id_inscricao_pos', $id_inscricao_pos)->update(['finalizada' => 'false', 'updated_at' => date('Y-m-d H:i:s') ]);
+
+			notify()->flash('A inscrição do candidato com e-mail: '.$email_candidato.' foi reativada com sucesso!','success');
+
+			return redirect()->route('reativar.candidato');
+		}else{
+
+			notify()->flash('Nenhuma alteração feita na inscrição do candidato: '.$email_candidato,'info');
+
+			return redirect()->route('reativar.candidato');
+		}
+	}
+
 
 }
