0bd5a199cf836b32ba8f240e7dbabe332b18296a
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-12-05 19:01:12 -0200

    Controller para logar como um dado usuário.

---
+++ app/Http/Controllers/Admin/ImpersonateController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Posmat\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Posmat\Http\Controllers\Controller;
+
+class ImpersonateController extends Controller
+{
+    //
+}
