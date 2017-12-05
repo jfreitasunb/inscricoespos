fd9d5b08af962ef950e2026058520808d8f659af
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-12-05 19:42:49 -0200

    Middleware para o impersonate.

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,7 @@ Route::prefix('recomendante')->middleware('user.role:recomendante','define.local
 *Área do Admin
  */
 
-Route::prefix('admin')->middleware('user.role:admin')->group(function () {
+Route::prefix('admin')->middleware('user.role:admin', 'impersonate.user')->group(function () {
 
 	Route::get('/', '\Posmat\Http\Controllers\AdminController@getMenu')->name('menu.admin');
 
