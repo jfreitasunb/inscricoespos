7d0bba83b5bd6f03457c937969684e6880380836
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-12-06 17:45:08 -0200

    Traduação das telas iniciais para espanhol.

--- resources/lang/es/tela_registro.php
+++ resources/lang/es/tela_registro.php
@@ -8,13 +8,11 @@
 return [
 
     'menu_registro' => 'Registrar',
-    'menu_nome' => 'Nome Completo',
+    'menu_nome' => 'Nombre Completo',
     'menu_email' => 'E-mail',
     'menu_confirma_email' => 'Confirmar E-mail',
-    'menu_senha' => 'Senha',
-    'menu_confira_senha' => 'Confirmar Senha',
-    'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
-    'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
-
-
+    'menu_senha' => 'Contraseña',
+    'menu_confira_senha' => 'Confirmar contraseña',
+    'menu_registro_sucesso_parte_inicial' => 'Cuenta creada con éxito. Se envió al correto electrónico: ',
+    'menu_registro_sucesso_parte_final' => 'un vínculo de activación de su cuenta.',
 ];
