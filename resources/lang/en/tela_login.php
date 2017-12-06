8d75ada327cd9424332a1b2b038c8980bbb60b4a
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-12-06 17:44:50 -0200

    Tradução das telas iniciais para o inglês.

--- resources/lang/en/tela_login.php
+++ resources/lang/en/tela_login.php
@@ -7,9 +7,7 @@
 
 return [
     'menu_email' => 'E-mail',
-    'menu_senha' => 'Senha',
-    'menu_entrar' => 'Entrar',
-    'menu_esqueceu_senha' => 'Esqueceu senha?',
-    'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
-    'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
+    'menu_senha' => 'Password',
+    'menu_entrar' => 'Enter',
+    'menu_esqueceu_senha' => 'Forgot password?',
 ];
