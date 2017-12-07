aaf5af7fbab1d26d4147e5cd767872f832504f34
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2017-12-07 13:22:43 -0200

    Forma de exibir o ano.

--- resources/views/templates/partials/rodape.blade.php
+++ resources/views/templates/partials/rodape.blade.php
@@ -1,11 +1,11 @@
 <footer id="footer">
-  <div class="footer-main">
-  	<div class="footer-foot">
-  		<div class="container-fluid">
-  			<div class="row-fluid">
-				<p>MAT/UnB - <?php echo date("Y");?></p>
+	<div class="footer-main">
+		<div class="footer-foot">
+			<div class="container-fluid">
+				<div class="row-fluid">
+					<p>MAT/UnB - {{ date("Y") }}</p>
+				</div>
 			</div>
-  		</div>
-  	</div>
-  </div>
+		</div>
+	</div>
 </footer>
