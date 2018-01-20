7fb3eb9e6677e6cfcadd7f00b62f719f35ba8893
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-20 18:27:22 -0200

    Mais traduções para o espanhol.

--- resources/lang/es/tela_carta_parte_final.php
+++ resources/lang/es/tela_carta_parte_final.php
@@ -6,19 +6,17 @@
 
 
 return [
-    'tela_pendentes' => 'Preencher Carta',
-    'opiniao_antecedentes_academicos' => 'Dê-nos sua opinião sobre os antecedentes acadêmicos, profissionais e/ou técnicos do candidato ',
-    'opiniao_desempenho_candidato' => 'Dê-nos sua opinião sobre o possível desempenho do candidato, se aceito no programa',
-    'outras_informacoes_relevantes' => 'Outras informações relevante',
-    'classifica_candidato' => 'Entre os estudantes que já conheceu, de que maneira você classificaria o candidato?',
+    'tela_pendentes' => 'Llenar la Carta',
+    'opiniao_antecedentes_academicos' => 'Dé su opinión sobre los antecedentes académicos, profesionales y/o técnicos del candidato',
+    'opiniao_desempenho_candidato' => 'Dé su opinión sobre el posible desempeño del candidato, si fuese aceptado en el programa',
+    'outras_informacoes_relevantes' => 'Otra información relevante',
+    'classifica_candidato' => 'Entre los estudiantes que ha conocido, de qué manera  clasificaría al candidato:',
     'top_5' => '5% melhores',
     'top_10' => '10% melhores',
     'top_25' => '25% melhores',
     'top_50' => '50% melhores',
     'nao_sabe' => 'Não sabe',
-    'como_aluno' => 'Como aluno, em aulas',
-    'como_orientando' => 'Como orientando',
-    'menu_enviar' => 'Envio definitivo',
-    'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
-    'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
+    'como_aluno' => 'Como alumno, en aulas',
+    'como_orientando' => 'Como dirigido',
+    'menu_enviar' => 'Envío definitivo',
 ];
