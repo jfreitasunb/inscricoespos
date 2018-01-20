86afe603cc2ceb90b5619c1d0dd1567623b11b23
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-20 17:31:30 -0200

    Tradução da tela de escolhas para o espanhol.

--- resources/lang/es/tela_escolha_candidato.php
+++ resources/lang/es/tela_escolha_candidato.php
@@ -7,14 +7,12 @@
 
 return [
 	'tela_escolhas' => 'Escolha',
-	'programa_disponivel' => 'Programa Pretendido',
-    'interesse_bolsa' => 'Tem interesse em bolsa?',
-    'vinculo_empregaticio' => 'Possui vínculo empregatício?',
-    'recomendante' => 'Nome completo e e-mail de três professores que, por sua solicitação, enviarão cartas de recomendação. ',
-    'nome' => 'Nome: ',
+	'programa_disponivel' => 'Programa al que aspira',
+    'interesse_bolsa' => '¿Tiene interés en solicitar beca?',
+    'vinculo_empregaticio' => '¿Es usted empleado?',
+    'recomendante' => 'Nombre completo y e-mail de tres profesores que, por solicitud suya, enviarán las cartas de recomendación.',
+    'nome' => 'Nombre: ',
     'confirmar_email' => 'Confirmar e-mail:',
-    'motivacao' => 'Escreva no espaço abaixo a sua motivação e expectativa em relação ao programa pretendido:',
+    'motivacao' => 'Escriba en el espacio de  abajo su motivación y expectativa con respecto al programa que aspira:',
     'menu_enviar' => 'Enviar',
-    'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
-    'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
 ];
