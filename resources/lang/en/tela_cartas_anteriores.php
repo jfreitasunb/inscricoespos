f5fca15de6bffde75a1f6bbda4d33287568b52d6
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-29 18:35:14 -0200

    Tradução da tela de cartas anteriores para o inglês.

--- resources/lang/en/tela_cartas_anteriores.php
+++ resources/lang/en/tela_cartas_anteriores.php
@@ -6,15 +6,7 @@
 
 
 return [
-    'tela_cartas_anteriores' => 'Previous Letters',
-	'atualizar_dados' => 'Dear professor, before sending your recommendation, please update your data.',
-    'curso_graduacao' => 'Graduação',
-	'curso' => 'Curso',
-    'tipo_curso' => 'Tipo',
-    'instituicao' => 'Instituição',
-    'ano_conclusao' => 'Ano de conclusão (ou previsão)',
-    'curso_pos' => 'Pós-Graduação',
-    'menu_enviar' => 'Enviar',
-    'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
-    'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
+    'tela_cartas_anteriores' => 'View Previous Letters',
+	'cartas_antigas' => 'Recommendation Letters Sent',
+    'mensagem_carta_anteriores' => 'Here you can access your previous recomendation sent',
 ];
