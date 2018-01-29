29fc2bbb6899746d3045118ab5722ea3d1fceb7e
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-29 18:36:16 -0200

    Tradução da tela de cartas anteriores para o espanhol.

--- resources/lang/es/tela_cartas_anteriores.php
+++ resources/lang/es/tela_cartas_anteriores.php
@@ -7,12 +7,6 @@
 
 return [
     'tela_cartas_anteriores' => 'Cartas Anteriores',
-	'atualizar_dados' => 'Respetado(a) recomendante, antes de enviar su recomendación, por favor actualice sus datos.',
-    'curso_graduacao' => 'Graduación',
-	'curso' => 'Curso',
-    'tipo_curso' => 'Especie',
-    'instituicao' => 'Institución',
-    'ano_conclusao' => 'Año de conclusión (o previsión)',
-    'curso_pos' => 'Posgraduación',
-    'menu_enviar' => 'Enviar',
+	'cartas_antigas' => 'Cartas de recomendación enviadas',
+    'mensagem_carta_anteriores' => 'quí puede acceder a su recomendación anterior enviada',
 ];
