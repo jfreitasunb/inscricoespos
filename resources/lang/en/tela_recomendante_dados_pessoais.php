cd23bd442f16c59c59767f838c5bae6f6b3855ab
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-29 18:35:57 -0200

    Correção da tradução.

--- resources/lang/en/tela_recomendante_dados_pessoais.php
+++ resources/lang/en/tela_recomendante_dados_pessoais.php
@@ -11,7 +11,7 @@ return [
 	'atualizar_dados' => 'Dear professor, before sending your recommendation, please update your data.',
     'instituicao_recomendante' => 'Institution',
 	'titulacao_recomendante' => 'Highest academic degree achieved',
-    'area_recomendante' => 'Área',
+    'area_recomendante' => 'Area',
     'ano_titulacao' => 'Year of the aforementioned achievement',
     'inst_obtencao_titulo' => 'Institution where the aforementioned academic degree was achieved',
     'endereco_recomendante' => 'Institutional address',
