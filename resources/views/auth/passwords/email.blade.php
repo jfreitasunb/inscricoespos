75ead8ccbbe677b047ca45c02e5ea19a316b2e83
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-29 21:39:34 -0200

    Traduzindo a tela de recuperação de senha.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,13 +3,13 @@
 @section('stylesheets')
   <link href="{{ asset('css/parsley.css') }}" rel="stylesheet">
 @endsection
-
+{{-- {{ dd(Session::get('locale')) }} --}}
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Esqueci Senha</div>
+                <div class="panel-heading">{{ trans('tela_esqueceu_senha.menu_esqueceu_senha') }}</div>
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
@@ -21,7 +21,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+                            <label for="email" class="col-md-4 control-label">{{ trans('tela_esqueceu_senha.email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required="" data-parsley-type="email">
@@ -37,7 +37,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Enviar
+                                    {{ trans('tela_esqueceu_senha.menu_enviar') }}
                                 </button>
                             </div>
                         </div>
