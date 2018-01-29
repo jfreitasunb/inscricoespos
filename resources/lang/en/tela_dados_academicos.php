fcd6373eabf034e7c1c85061189c7308a11b92aa
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-29 17:49:57 -0200

    Tradução da tela de dados acadêmicos para o inglês.

--- resources/lang/en/tela_dados_academicos.php
+++ resources/lang/en/tela_dados_academicos.php
@@ -6,15 +6,13 @@
 
 
 return [
-	'tela_dados_academicos' => 'Dados Acadêmicos',
-    'curso_graduacao' => 'Graduação',
-	'curso' => 'Curso',
-    'tipo_curso' => 'Tipo',
-    'instituicao' => 'Instituição',
-    'ano_conclusao' => 'Ano de conclusão (ou previsão)',
-    'curso_pos' => 'Pós-Graduação',
-    'nivel' => 'Nivel',
-    'menu_enviar' => 'Enviar',
-    'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
-    'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
+	'tela_dados_academicos' => 'Academic Data',
+    'curso_graduacao' => 'University Graduate',
+	'curso' => 'Course',
+    'tipo_curso' => 'Type',
+    'instituicao' => 'Institution',
+    'ano_conclusao' => 'Year of conclusion (or forecast)',
+    'curso_pos' => 'Postgraduate Studies',
+    'nivel' => 'Level',
+    'menu_enviar' => 'Submit',
 ];
