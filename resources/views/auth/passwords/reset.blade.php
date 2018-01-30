0dc4ec7a2273d1f494d6dbb60f85c7a1d4483ff5
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-30 14:51:57 -0200

    Tradução do template de acordo com o idioma preferido.

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -9,7 +9,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Mudar Senha</div>
+                <div class="panel-heading">{{ trans('tela_mudar_senha.menu_mudar') }}</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -23,7 +23,7 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+                            <label for="email" class="col-md-4 control-label">{{ trans('tela_mudar_senha.email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required="" data-parsley-type="email">
@@ -37,7 +37,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Senha</label>
+                            <label for="password" class="col-md-4 control-label">{{ trans('tela_mudar_senha.senha') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required="" data-parsley-minlength="1">
@@ -51,7 +51,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirmar Senha</label>
+                            <label for="password-confirm" class="col-md-4 control-label">{{ trans('tela_mudar_senha.confirmar_senha') }}</label>
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required="" data-parsley-minlength="1" data-parsley-equalto="#password">
 
@@ -66,7 +66,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Trocar Senha
+                                    {{ trans('tela_mudar_senha.botao_trocar') }}
                                 </button>
                             </div>
                         </div>
