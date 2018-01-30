c624ada419200ca812f8a8fba26d4527a2a547d3
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-30 14:51:26 -0200

    Tradução para espanhol da tela de mudança de senha.

--- resources/lang/es/tela_mudar_senha.php
+++ resources/lang/es/tela_mudar_senha.php
@@ -11,5 +11,10 @@ return [
     'motivo_mensagem' => 'Estás recibiendo este correo porque hiciste una solicitud de recuperación de contraseña para tu cuenta.',
     'mudar_senha' => 'Recuperar contraseña',
     'nao_solicitou' => 'Si no realizaste esta solicitud, no se requiere realizar ninguna otra acción.',
-    'menu_enviar' => 'Enviar',
+    'email' => 'E-mail',
+    'menu_mudar' => 'Cambiar Contraseña',
+    'senha' => 'Contraseña',
+    'confirmar_senha' => 'Confirmar contraseña',
+    'botao_trocar' => 'Cambiar contraseña',
+    'menu_enviar' => 'Obtener nueva contraseña',
 ];
