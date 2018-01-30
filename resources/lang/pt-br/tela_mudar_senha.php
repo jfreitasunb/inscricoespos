a3b266f0538312f3b46f21706e1a47a840e4e562
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-30 14:34:09 -0200

    Mensagem de mudança de senha em Português.

--- resources/lang/pt-br/tela_mudar_senha.php
+++ resources/lang/pt-br/tela_mudar_senha.php
@@ -8,5 +8,8 @@
 return [
     'assunto_email' => 'Link para mudança de senha',
     'email' => 'E-mail',
+    'motivo_mensagem' => 'Você está recebendo essa mensagem porquê solicitou a mudança da sua senha.',
+    'mudar_senha' => 'Mudar senha',
+    'nao_solicitou' => 'Se você não solicitou a mudança da sua senha, por favor ignore essa mensagem.',
     'menu_enviar' => 'Enviar',
 ];
