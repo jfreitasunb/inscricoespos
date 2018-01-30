8808b39c97296cc12d4f030bfc1120842f7770df
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-30 14:51:38 -0200

    Tradução para português da tela de mudança de senha.

--- resources/lang/pt-br/tela_mudar_senha.php
+++ resources/lang/pt-br/tela_mudar_senha.php
@@ -11,5 +11,10 @@ return [
     'motivo_mensagem' => 'Você está recebendo essa mensagem porquê solicitou a mudança da sua senha.',
     'mudar_senha' => 'Mudar senha',
     'nao_solicitou' => 'Se você não solicitou a mudança da sua senha, por favor ignore essa mensagem.',
+    'email' => 'E-mail',
+    'menu_mudar' => 'Mudar Senha',
+    'senha' => 'Senha',
+    'confirmar_senha' => 'Confirmar Senha',
+    'botao_trocar' => 'Mudar Senha',
     'menu_enviar' => 'Enviar',
 ];
