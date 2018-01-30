324958f83b995075fff0d2014d6b0fe3041f6bf9
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-30 14:30:40 -0200

    E-mail de mudança de senha em Inglês.

--- resources/lang/en/tela_mudar_senha.php
+++ resources/lang/en/tela_mudar_senha.php
@@ -8,5 +8,8 @@
 return [
     'assunto_email' => 'Password reset link',
     'email' => 'E-mail',
-    'menu_enviar' => 'Enviar',
+    'motivo_mensagem' => 'You are receiving this email because we received a password reset request for your account. Click the button below to reset your password:',
+    'mudar_senha' => 'Reset Password',
+    'nao_solicitou' => 'If you did not request a password reset, no further action is required.',
+    'menu_enviar' => 'Submit',
 ];
