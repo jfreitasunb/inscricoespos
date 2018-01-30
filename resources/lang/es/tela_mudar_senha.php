a387985dc90872f1a3476950e0baad8844681a26
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-01-30 14:33:53 -0200

    Mensagem de mudança de senha em Espanhol.

--- resources/lang/es/tela_mudar_senha.php
+++ resources/lang/es/tela_mudar_senha.php
@@ -6,7 +6,10 @@
 
 
 return [
-    'assunto_email' => 'Recordatorio de contraseña',
+    'assunto_email' => 'Recuperar contraseña',
     'email' => 'E-mail',
+    'motivo_mensagem' => 'Estás recibiendo este correo porque hiciste una solicitud de recuperación de contraseña para tu cuenta.',
+    'mudar_senha' => 'Recuperar contraseña',
+    'nao_solicitou' => 'Si no realizaste esta solicitud, no se requiere realizar ninguna otra acción.',
     'menu_enviar' => 'Enviar',
 ];
