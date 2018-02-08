edc81feeeeac79538010a339de5b3dec1efd01f0
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-02-08 17:30:12 -0200

    Exibição.

--- resources/views/templates/partials/admin/link_muda_senha.blade.php
+++ resources/views/templates/partials/admin/link_muda_senha.blade.php
@@ -7,28 +7,45 @@
 @endsection
 
 @section('link_muda_senha')
+@if ($modo_pesquisa)
   {!! Form::open(array('route' => 'pesquisa.email.muda.senha', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!}
+@else
+  {!! Form::open(array('route' => 'pesquisa.email.muda.senha', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!} 
+@endif
 
 <div class="form-group">
   {!! Form::label('email', 'Pesquisar Usuário', ['class' => 'col-md-4 control-label']) !!}
   <div class="col-md-4">
     {!! Form::text('email', '' , ['class' => 'form-control input-md']) !!}
   </div>
-</div>  {!! Form::hidden('id_user', $user->id_user, []) !!}
+</div>
+@if (!$modo_pesquisa)
+  {!! Form::hidden('id_user', $user->id_user, []) !!}
    <div class="form-group">
-    {!! Form::label('link', 'Link para mudança de senha', ['class' => 'col-md-4 control-label']) !!}
+    {!! Form::label('link', 'Link de mudança de senha', ['class' => 'col-md-4 control-label']) !!}
     <div class="col-md-4">
-      {!! Form::text('link', $user->link, ['class' => 'form-control input-md', 'readonly' => 'readonly']) !!}
+      {!! Form::text('link', $url_mudar_senha, ['class' => 'form-control input-md', 'readonly' => 'readonly']) !!}
     </div>
   </div>
+@endif
 
+@if ($modo_pesquisa)
   <div class="form-group">
     <div class="row">
       <div class="col-md-6 col-md-offset-3 text-center">
-        {!! Form::submit('Gerar link', ['class' => 'btn btn-primary btn-lg register-submit']) !!}
+        {!! Form::submit('Gerar link', ['class' => 'btn btn-warning btn-lg register-submit']) !!}
+      </div>
+    </div>
+  </div> 
+@else
+  <div class="form-group">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3 text-center">
+        {!! Form::submit('Limpar', ['class' => 'btn btn-primary btn-lg register-submit', 'name' =>'cancelar',]) !!}
       </div>
     </div>
   </div>
+@endif
 
 {!! Form::close() !!}
 @endsection
