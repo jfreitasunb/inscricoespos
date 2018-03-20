566336991f5f35d9b66125660d2332106904974f
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-03-20 15:34:57 -0300

    Exibindo os dados na view.

--- resources/views/templates/partials/coordenador/editar_area_pos.blade.php
+++ resources/views/templates/partials/coordenador/editar_area_pos.blade.php
@@ -11,6 +11,36 @@
 <fieldset class="scheduler-border">
   <legend class="scheduler-border">Áreas da Pós-Graduação</legend>
 
+  @foreach ($areas_pos_mat as $area)
+  {!! Form::open(array('route' => 'editar.area.pos', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!}
+
+  {!! Form::hidden('id_area_pos', $area->id_area_pos, []) !!}
+  
+    <div class="form-group">
+      <div class="col-sm-1">
+        {!! Form::label('nome_ptbr', 'Nome', ['class' => 'control-label']) !!}
+      </div>
+          <div class="col-sm-4">
+            {!! Form::text('nome_ptbr', $area->nome_ptbr, ['class' => 'form-control']) !!}
+          </div>
+          <div class="col-sm-1">
+        {!! Form::label('nome_en', 'Nome Inglês', ['class' => 'control-label']) !!}
+      </div>
+          <div class="col-sm-4">
+            {!! Form::text('nome_en', $area->nome_en, ['class' => 'form-control']) !!}
+          </div>
+          <div class="col-sm-1">
+        {!! Form::label('nome_es', 'Nome Espanhol', ['class' => 'control-label']) !!}
+      </div>
+          <div class="col-sm-4">
+            {!! Form::text('nome_es', $area->nome_es, ['class' => 'form-control']) !!}
+          </div>
+          <div class="col-sm-2">
+            {!! Form::submit('Alterar', ['class' => 'btn btn-danger pull-righ']) !!}
+          </div>
+      </div>
+      {!! Form::close() !!}
+  @endforeach
   
 </fieldset>
 
