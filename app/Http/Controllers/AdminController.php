e23b096bfaed5f762863701527b37d13ac835e2b
jfreitasmat <jfreitas@mat.unb.br>
2018-03-22 15:58:20 -0300

    Recuperando e tratando os recomendantes indicados por um dado candidato com as informações pessoais e status da carta.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -686,7 +686,50 @@ class AdminController extends CoordenadorController
 
 	public function getListaIndicacoes()
 	{
-		return view('templates.partials.admin.tabela_indicacoes');
+		$relatorio = new ConfiguraInscricaoPos();
+
+      	$relatorio_disponivel = $relatorio->retorna_edital_vigente();
+
+
+		$finalizacoes = new FinalizaInscricao;
+		
+
+		$inscricoes_finalizadas = $finalizacoes->retorna_usuarios_relatorio_individual($relatorio_disponivel->id_inscricao_pos, $this->locale_default)->paginate(10);
+
+		$dados_para_template = [];
+
+		foreach ($inscricoes_finalizadas as $inscricao) {
+
+			$recomendante_candidato = new ContatoRecomendante();
+
+			$recomendantes_candidato = $recomendante_candidato->retorna_recomendante_candidato($inscricao->id_user,$inscricao->id_inscricao_pos);
+
+			$dados_para_template[$inscricao->id_user]['nome_candidato'] = $inscricao->nome;
+			$dados_para_template[$inscricao->id_user]['tipo_programa_pos'] = $inscricao->tipo_programa_pos_ptbr;
+			$i = 1;
+			foreach ($recomendantes_candidato as $recomendante) {
+
+
+				$dado_pessoal_recomendante = new DadoRecomendante();
+
+				$usuario_recomendante = User::find($recomendante->id_recomendante);
+
+				$dados_para_template[$inscricao->id_user]['email_recomendante_'.$i] = $usuario_recomendante->email;
+
+				$dados_para_template[$inscricao->id_user]['nome_recomendante_'.$i] = $dado_pessoal_recomendante->retorna_dados_pessoais_recomendante($recomendante->id_recomendante)->nome_recomendante;
+
+				$carta_recomendacao = new CartaRecomendacao();
+				
+				$carta_aluno = $carta_recomendacao->retorna_carta_recomendacao($recomendante->id_recomendante,$inscricao->id_user,$inscricao->id_inscricao_pos);
+
+				$dados_para_template[$inscricao->id_user]['status_carta_'.$i] = $carta_aluno->completada;
+
+				$i++;
+			}
+		}
+		
+		// dd($dados_para_template);
+		return view('templates.partials.admin.tabela_indicacoes')->with(compact('dados_para_template', 'inscricoes_finalizadas'));
 	}
 
 	public function postListaIndicacoes(Request $request)
