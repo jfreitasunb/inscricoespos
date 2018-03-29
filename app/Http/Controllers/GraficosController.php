f1ebfaab3deb5562132a51f8ee3cbf0274b20b99
José Antônio Oliveira de Freitas <jfreitas@mat.unb.br>
2018-03-29 16:08:24 -0300

    Começando a classe para gerar os gráficos.

--- app/Http/Controllers/GraficosController.php
+++ app/Http/Controllers/GraficosController.php
@@ -41,33 +41,12 @@ use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class GraficosController extends BaseController
-{
+{   
+    private $locale_default = 'pt-br';
+
 	public function index()
     {
-        $chart = Charts::multi('bar', 'material')
-            // Setup the chart settings
-            ->title("My Cool Chart")
-            // A dimension of 0 means it will take 100% of the space
-            ->dimensions(0, 400) // Width x Height
-            // This defines a preset of colors already done:)
-            ->template("material")
-            // You could always set them manually
-            // ->colors(['#2196F3', '#F44336', '#FFC107'])
-            // Setup the diferent datasets (this is a multi chart)
-            ->dataset('Element 1', [5,20,100])
-            ->dataset('Element 2', [15,30,80])
-            ->dataset('Element 3', [25,10,40])
-            // Setup what the values mean
-            ->labels(['One', 'Two', 'Three']);
 
-
-            $chart2 = Charts::create('pie', 'highcharts')
-                ->title('My nice chart')
-                ->labels(['First', 'Second', 'Third'])
-                ->values([5,10,20])
-                ->dimensions(1000,500)
-                ->responsive(true);
-
-        return view('templates.partials.graficos.test', ['chart' => $chart, 'chart2' => $chart2]);
+        
     }
 }
