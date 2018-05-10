23d14fa7c5eb37d2999a970f952c6ad58989f852
jfreitasmat <jfreitas@mat.unb.br>
2018-05-10 14:28:38 -0300

    Reestilização da página inicial.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,29 @@
       </div>
     </div>
   </div>
-  <div class="row">
+  <div class="row" style="margin-top:50px">
+    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+    <form role="form">
+      <fieldset>
+        <div class="row">
+          <div class="col-xs-6 col-sm-6 col-md-6">
+            <a href="{{ route('auth.login') }}" class="btn btn-lg btn-success btn-block">{{trans('tela_inicial.menu_login')}}</a>
+          </div>
+          <div class="col-xs-6 col-sm-6 col-md-6">
+            <a href="{{ route('auth.registrar') }}" class="btn btn-lg btn-primary btn-block">{{trans('tela_inicial.menu_registrar')}}</a>
+          </div>
+          <div class="text-center" style="margin-top:70px">
+            <span class="button-checkbox">
+          <a href="{{ route('password.request') }}" class="btn btn-link">{{trans('tela_login.menu_esqueceu_senha')}}</a>
+        </span>  
+          </div>
+          
+        </div>
+      </fieldset>
+    </form>
+  </div>
+</div>
+  {{-- <div class="row">
     <div class="col-md-6 col-md-offset-3">
       <div class="panel panel-login">
         <div class="panel-heading">
@@ -36,7 +58,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> --}}
   <p class="bottom-three">
    
   </p>
