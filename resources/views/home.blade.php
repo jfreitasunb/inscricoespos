78ded7f098d269c33e7f06e26e7a6ef3ef106824
jfreitasmat <jfreitas@mat.unb.br>
2018-05-10 09:34:20 -0300

    Melhorando a aparência do site.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
   <div class="row">
     <div class="col-md-6 col-md-offset-3">
       <div class="idiomas btn-toolbar">
-        <div class="btn-group btn-group-justified">
+        <div class="btn-group-justified">
           <a href="{{ route('lang.portuguese') }}" class="btn btn-primary button">Português</a>
           <a href="{{ route('lang.english') }}" class="btn btn-primary button">English</a>
           <a href="{{ route('lang.spanish') }}" class="btn btn-primary button">Español</a>
