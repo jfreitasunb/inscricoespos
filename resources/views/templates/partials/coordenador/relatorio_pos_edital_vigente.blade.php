d0ef420d360d7a9a060ae30c3fde9ebdf209b997
jfreitasmat <jfreitas@mat.unb.br>
2018-05-10 16:17:39 -0300

    Nada de mais.

--- resources/views/templates/partials/coordenador/relatorio_pos_edital_vigente.blade.php
+++ resources/views/templates/partials/coordenador/relatorio_pos_edital_vigente.blade.php
@@ -1,13 +1,12 @@
 @extends('templates.default')
 
 @section('relatorio_pos_edital_vigente')
-
   <form action="" method="POST">
     <legend>Relatório de inscritos</legend>
-    	<strong>Total de Inscritos: </strong> {{ $total_inscritos }} <br>
-    	@foreach ($programa_para_inscricao as $programa)
-    		<strong>Total de inscritos para o {{ $programa }}:</strong> {{ $contagem[$programa] }}<br>
-    	@endforeach
+	<strong>Total de Inscritos: </strong> {{ $total_inscritos }} <br>
+	@foreach ($programa_para_inscricao as $programa)
+		<strong>Total de inscritos para o {{ $programa }}:</strong> {{ $contagem[$programa] }}<br>
+	@endforeach
     	<div class="table-responsive">
 		    <table class="table table-striped">
 			  	<thead>
@@ -20,19 +19,19 @@
 				    </tr>
 			  	</thead>
 			  	<tbody>
-				    	<tr>
-				      	<th scope="row"><a href="{!! route('gera.relatorio', ['id_inscricao_pos' => $relatorio_disponivel['id_inscricao_pos']]) !!}">{{$relatorio_disponivel['edital']}}</a></th>
-				      	<td><a href="{!! route('gera.relatorio', ['id_inscricao_pos' => $relatorio_disponivel['id_inscricao_pos']]) !!}">{{ $nome_programas }}</a></td>
-				      	<td><a href="{!! route('gera.relatorio', ['id_inscricao_pos' => $relatorio_disponivel['id_inscricao_pos']]) !!}">{{\Carbon\Carbon::parse($relatorio_disponivel['inicio_inscricao'])->format('d/m/Y')." à ".\Carbon\Carbon::parse($relatorio_disponivel['fim_inscricao'])->format('d/m/Y')}}</a></td>
-				      	<td>@if($monitoria == $relatorio_disponivel['id_inscricao_pos'] || isset($local_arquivos)) 
-				      		@foreach ($arquivos_zipados_para_view as $zip_relatorio)
-				      		 	<a target="_blank" href="{{ asset($local_arquivos['arquivo_zip'].$zip_relatorio) }}" >{{$zip_relatorio}}</a>
-				      		@endforeach
-				      		@endif</td>
-				      	<td>@if($monitoria == $relatorio_disponivel['id_inscricao_pos'] || isset($local_arquivos)) <a target="_blank" href="{{asset($local_arquivos['local_relatorios'].$relatorio_csv)}}">{{$relatorio_csv}}</a> @endif</td>
-				    	</tr>
+			    	<tr>
+			      	<th scope="row"><a href="{!! route('gera.relatorio', ['id_inscricao_pos' => $relatorio_disponivel['id_inscricao_pos']]) !!}">{{$relatorio_disponivel['edital']}}</a></th>
+			      	<td><a href="{!! route('gera.relatorio', ['id_inscricao_pos' => $relatorio_disponivel['id_inscricao_pos']]) !!}">{{ $nome_programas }}</a></td>
+			      	<td><a href="{!! route('gera.relatorio', ['id_inscricao_pos' => $relatorio_disponivel['id_inscricao_pos']]) !!}">{{\Carbon\Carbon::parse($relatorio_disponivel['inicio_inscricao'])->format('d/m/Y')." à ".\Carbon\Carbon::parse($relatorio_disponivel['fim_inscricao'])->format('d/m/Y')}}</a></td>
+			      	<td>@if($monitoria == $relatorio_disponivel['id_inscricao_pos'] || isset($local_arquivos)) 
+			      		@foreach ($arquivos_zipados_para_view as $zip_relatorio)
+			      		 	<a target="_blank" href="{{ asset($local_arquivos['arquivo_zip'].$zip_relatorio) }}" >{{$zip_relatorio}}</a>
+			      		@endforeach
+			      		@endif</td>
+			      	<td>@if($monitoria == $relatorio_disponivel['id_inscricao_pos'] || isset($local_arquivos)) <a target="_blank" href="{{asset($local_arquivos['local_relatorios'].$relatorio_csv)}}">{{$relatorio_csv}}</a> @endif</td>
+			    	</tr>
 			  	</tbody>
 			</table>
-		<div>
+		</div>
   </form>
 @stop
