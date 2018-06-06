bc8ef47ac334fc3790a71f4481a7965c576928bb
jfreitasmat <jfreitas@mat.unb.br>
2018-06-06 14:30:42 -0300

    Pesquisa por nome não precisa de fazer um join mais.

--- app/Models/DadoPessoalCandidato.php
+++ app/Models/DadoPessoalCandidato.php
@@ -25,16 +25,16 @@ class DadoPessoalCandidato extends Model
         'celular',
     ];
 
-    public function retorna_dados_pessoais($id_user)
+    public function retorna_dados_pessoais($id_candidato)
     {
         
-        return $this->find($id_user);
+        return $this->find($id_candidato);
 
     }
 
     public function retorna_user_por_nome($nome_pesquisado)
     {
-        return $this->where('nome', 'ILIKE', $nome_pesquisado.'%')->join('users', 'users.id_user', 'dados_pessoais.id_user')->select('users.id_user', 'dados_pessoais.nome', 'users.email', 'users.locale', 'users.user_type', 'users.ativo')->get();
+        return $this->where('nome', 'ILIKE', $nome_pesquisado.'%')->get();
     }
 
 }
