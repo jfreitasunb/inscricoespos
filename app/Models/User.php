7ad574a7a5e8da0a882e2a31c023b087e5995f4b
jfreitasmat <jfreitas@mat.unb.br>
2018-06-06 14:40:01 -0300

    Comentando função duplicada.

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,11 +54,11 @@ class User extends Authenticatable
         
     }
 
-    public function retorna_usuario_por_email($email)
-    {
-        return $this->where('email',$email)->join('dados_pessoais', 'dados_pessoais.id_user','users.id_user')->get()->first();
+    // public function retorna_usuario_por_email($email)
+    // {
+    //     return $this->where('email',$email)->join('dados_pessoais', 'dados_pessoais.id_user','users.id_user')->get()->first();
 
-    }
+    // }
 
     public function retorna_user_por_email($email)
     {
