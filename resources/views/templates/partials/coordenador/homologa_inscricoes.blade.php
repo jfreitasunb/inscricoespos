2dd81e887d9a73d9a0b1187df8578647a06740bf
jfreitasmat <jfreitas@mat.unb.br>
2018-07-05 15:13:14 -0300

    Removendo links.

--- resources/views/templates/partials/coordenador/homologa_inscricoes.blade.php
+++ resources/views/templates/partials/coordenador/homologa_inscricoes.blade.php
@@ -17,17 +17,15 @@
         <tr>
           <th scope="col">{{ trans('tela_cartas_pendentes.nome_candidato') }}</th>
           <th scope="col">{{ trans('tela_cartas_pendentes.tipo_programa') }}</th>
-          <th>Ficha de Inscrição</th>
-          <th>Cartas Recebidas</th>
+          <th>Homologar Inscrição?</th>
         </tr>
       </thead>
       <tbody>
         @foreach( $inscricoes_finalizadas as $finalizada)
           <tr class="">
-            <td><a href=" {{ route('ver.ficha.individual', ['id_inscricao_pos' => $finalizada['id_inscricao_pos'],'id_aluno' => $finalizada['id_candidato']]) }}">{{ $finalizada['nome'] }}</a></td>
-            <td><a href=" {{ route('ver.ficha.individual', ['id_inscricao_pos' => $finalizada['id_inscricao_pos'],'id_aluno' => $finalizada['id_candidato']]) }}">{{ $finalizada['tipo_programa_pos_ptbr'] }}</a></td>
+            <td>{{ $finalizada['nome'] }}</td>
+            <td>{{ $finalizada['tipo_programa_pos_ptbr'] }}</td>
             <td></td>
-            <td>Nada</td>
           </tr>
         @endforeach
       </tbody>
