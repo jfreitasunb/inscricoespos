1470cdfd963a12bef1026d57ec12e9674c510471
jfreitasmat <jfreitas@mat.unb.br>
2018-07-05 15:31:33 -0300

    Visualizando os dados.

--- resources/views/templates/partials/coordenador/homologa_inscricoes.blade.php
+++ resources/views/templates/partials/coordenador/homologa_inscricoes.blade.php
@@ -9,31 +9,35 @@
 @section('homologa_inscricoes')
 
 <fieldset class="scheduler-border">
-  <legend class="scheduler-border">Fichas de Inscrição Individuais</legend>
-
-  <div class="table-responsive">
-    <table class="table table-bordered table-hover">
-      <thead>
-        <tr>
-          <th scope="col">{{ trans('tela_cartas_pendentes.nome_candidato') }}</th>
-          <th scope="col">{{ trans('tela_cartas_pendentes.tipo_programa') }}</th>
-          <th>Homologar Inscrição?</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach( $inscricoes_finalizadas as $finalizada)
-          <tr class="">
-            <td>{{ $finalizada['nome'] }}</td>
-            <td>{{ $finalizada['tipo_programa_pos_ptbr'] }}</td>
-            <td></td>
+  <legend class="scheduler-border">Inscrições para homologação</legend>
+  {!! Form::open(array('route' => 'homologa.inscricoes', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!}
+    {!! Form::hidden('id_inscricao_pos', $relatorio_disponivel->id_inscricao_pos, []) !!}
+    <div class="table-responsive">
+      <table class="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th scope="col">{{ trans('tela_cartas_pendentes.nome_candidato') }}</th>
+            <th scope="col">{{ trans('tela_cartas_pendentes.tipo_programa') }}</th>
+            <th>Homologar Inscrição?</th>
           </tr>
-        @endforeach
-      </tbody>
-    </table>
-  </div>
-  <div class="text-center">
-    {{ $inscricoes_finalizadas->render() }}
-  </div>
+        </thead>
+        <tbody>
+          @foreach( $inscricoes_finalizadas as $finalizada)
+            <tr class="">
+              {!! Form::hidden('id_programa_pos', $finalizada['id_programa_pos'], []) !!}
+              <td>{{ $finalizada['nome'] }}</td>
+              <td>{{ $finalizada['tipo_programa_pos_ptbr'] }}</td>
+              <td>{!! Form::checkbox('homologar[]',1,1) !!} Sim {!! Form::checkbox('homologar[]',0,0) !!} Não</td>
+            </tr>
+          @endforeach
+        </tbody>
+        
+      </table>
+    </div>
+    <div class="col-md-10 text-center"> 
+      {!! Form::submit('Homologar', array('class' => 'register-submit btn btn-primary btn-lg', 'id' => 'register-submit', 'tabindex' => '4')) !!}
+    </div>
+  {!! Form::close() !!}
 </fieldset>
 
 @endsection
