0aa75d10933783455bbe935705fd6407bbc3bb74
jfreitasmat <jfreitas@mat.unb.br>
2018-07-30 17:49:49 -0300

    Dados dos candidatos homologados.

--- app/Models/FinalizaInscricao.php
+++ app/Models/FinalizaInscricao.php
@@ -56,7 +56,7 @@ class FinalizaInscricao extends Model
         if (sizeof($inscricoes_homologadas) == 0) {
             return $this->where('finaliza_inscricao.id_inscricao_pos', $id_inscricao_pos)->where('finaliza_inscricao.finalizada', true)->join('dados_pessoais_candidato', 'dados_pessoais_candidato.id_candidato','finaliza_inscricao.id_candidato')->join('users', 'users.id_user', 'finaliza_inscricao.id_candidato')->join('escolhas_candidato', 'escolhas_candidato.id_candidato', 'dados_pessoais_candidato.id_candidato')->where('escolhas_candidato.id_inscricao_pos', $id_inscricao_pos)->join('programa_pos_mat', 'id_programa_pos', 'escolhas_candidato.programa_pretendido')->select('finaliza_inscricao.id_candidato', 'finaliza_inscricao.id_inscricao_pos','users.nome', 'users.email', 'programa_pos_mat.id_programa_pos', 'programa_pos_mat.'.$nome_coluna)->orderBy('escolhas_candidato.programa_pretendido' , 'desc')->orderBy('users.nome','asc');
         }else{
-            dd('ninguem');
+            return $homologadas->retorna_dados_homologados($id_inscricao_pos, $locale);
         }
 
         
