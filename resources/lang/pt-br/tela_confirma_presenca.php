25a43f65ae70c8e1f787c92f232e670575479a2d
jfreitasmat <jfreitas@mat.unb.br>
2018-07-31 17:34:52 -0300

    Mensagem da tela de confirmação.

---
+++ resources/lang/pt-br/tela_confirma_presenca.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+*Tradução dos textos para Português
+ */
+
+
+return [
+    'confirma_presenca' => 'Confirmar Presença',
+    'mensagem_inicio' => 'Prezado(a) candidato(a), ',
+    'mensagem_meio' => ' você foi selecionado(a) para o programa de ',
+    'mensagem_fim' => 'no MAT/UnB. Você deve clicar em um dos botões abaixo para confirmar ou declinar sua participação no programa.',
+    'confirma' => 'Confirmar',
+    'declina' => 'Declinar',
+    'confirma_envio' => 'Tem certeza que deseja enviar sua resposta?',
+    'texto_confirma_envio' => 'Tem certeza que deseja recusar sua participação no programa?',
+    'sim_envia' => 'Sim',
+    'nao_envia' => 'Não',
+    'envio_cancelado' => 'Envio cancelado!',
+];
