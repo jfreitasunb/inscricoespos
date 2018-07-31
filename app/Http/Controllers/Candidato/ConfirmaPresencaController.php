ebf4e8b5b453e35cadbea8740bc2145f9363bb37
jfreitasmat <jfreitas@mat.unb.br>
2018-07-31 17:42:51 -0300

    Verificações de proteção.

--- app/Http/Controllers/Candidato/ConfirmaPresencaController.php
+++ app/Http/Controllers/Candidato/ConfirmaPresencaController.php
@@ -123,6 +123,90 @@ class ConfirmaPresencaController extends BaseController
 
 	public function postConfirmaPresenca(Request $request)
 	{	
-		dd(isset($request->confirma));
+		$id_candidato = $request->id_candidato;
+
+		$id_inscricao_pos = $request->id_inscricao_pos;
+
+		$id_programa_pretendido = $request->id_programa_pretendido;
+
+		if (isset($request->confirma)) {
+			$confirmou_presenca = True;
+		}else{
+			$confirmou_presenca = False;
+		}
+
+		$user = $this->SetUser();
+		
+		$id_user = $user->id_user;
+
+		$locale_candidato = Session::get('locale');
+
+		$edital_ativo = new ConfiguraInscricaoPos();
+
+		if ($id_inscricao_pos != $edital_ativo->retorna_inscricao_ativa()->id_inscricao_pos) {
+			return redirect()->back();
+		}
+		
+		if ($id_candidato != $id_user) {
+			return redirect()->back();
+		}
+
+		
+		$edital = $edital_ativo->retorna_inscricao_ativa()->edital;
+		
+		$autoriza_inscricao = $edital_ativo->autoriza_inscricao();
+
+		if (!$autoriza_inscricao) {
+			
+			$finaliza_inscricao = new FinalizaInscricao();
+
+			$status_inscricao = $finaliza_inscricao->retorna_inscricao_finalizada($id_user,$id_inscricao_pos);
+
+			if (!$status_inscricao) {
+
+				return redirect()->back();
+			}
+
+			$homologa = new HomologaInscricoes();
+
+			$candidato_homologado = $homologa->retorna_se_foi_homologado($id_user, $id_inscricao_pos);
+
+			if (!$candidato_homologado) {
+				return redirect()->back();
+			}
+
+			$selecionado = new CandidatosSelecionados();
+
+			$status_selecao = $selecionado->retorna_status_selecionado($id_inscricao_pos, $id_user);
+
+			if (!$status_selecao->selecionado) {
+				return redirect()->back();
+			}
+
+			if ($status_selecao->confirmou_presenca) {
+				notify()->flash(trans('mensagens_gerais.confirmou_presenca'),'success');
+			
+				return redirect()->route('home');
+			}
+
+			$nome = User::find($id_user)->nome;
+
+			$programa_pos = new ProgramaPos();
+
+			$nome_programa_pretendido = $programa_pos->pega_programa_pos_mat($status_selecao->programa_pretendido, $locale_candidato);
+
+			$dados_para_template['id_candidato'] = $id_user;
+
+			$dados_para_template['id_inscricao_pos'] = $id_inscricao_pos;
+
+			$dados_para_template['nome'] = $nome;
+			
+			$dados_para_template['programa_pretendido'] = $nome_programa_pretendido;
+
+			return view('templates.partials.candidato.confirma_presenca',compact('dados_para_template'));
+
+		}else{
+			return redirect()->back();
+		}
 	}
 }
