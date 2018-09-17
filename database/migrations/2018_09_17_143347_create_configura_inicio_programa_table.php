bc40e5e1359bbc50ab150dc5d026efaa13b8eae0
jfreitasmat <jfreitas@mat.unb.br>
2018-09-17 10:21:09 -0300

    Configurando início no programa.

--- database/migrations/2018_09_17_143347_create_configura_inicio_programa_table.php
+++ database/migrations/2018_09_17_143347_create_configura_inicio_programa_table.php
@@ -14,19 +14,11 @@ class CreateConfiguraInicioProgramaTable extends Migration
     public function up()
     {
         Schema::create('configura_inicio_programa', function (Blueprint $table) {
-            $table->increments('id');
-            $table->unsignedInteger('id_candidato');
-            $table->foreign('id_candidato')->references('id_user')->on('users')->onDelete('cascade');
+            $table->increments('id_inicio_programa');
             $table->unsignedInteger('id_inscricao_pos');
             $table->foreign('id_inscricao_pos')->references('id_inscricao_pos')->on('configura_inscricao_pos')->onDelete('cascade');
-            $table->unsignedInteger('programa_pretendido');
-            $table->foreign('programa_pretendido')->references('id_programa_pos')->on('programa_pos_mat')->onDelete('cascade');
-            $table->boolean('selecionado');
-            $table->boolean('confirmou_presenca')->default(False);
-            $table->unsignedInteger('inicio_no_programa');
-            $table->foreign('inicio_no_programa')->references('id_inicio_programa')->on('configura_inicio_programa')->onDelete('cascade');
-            $table->unsignedInteger('id_coordenador');
-            $table->foreign('id_coordenador')->references('id_user')->on('users')->onDelete('cascade');
+            $table->integer('mes_inicio');
+            $table->date('prazo_confirmacao');
             $table->softDeletes();
             $table->timestamps();
         });
