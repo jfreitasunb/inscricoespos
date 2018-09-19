bcb3334d39d1b758f6c25ae368e9c31e06730e4a
jfreitasmat <jfreitas@mat.unb.br>
2018-09-19 16:14:26 -0300

    Arquivo de tradução dos nomes dos meses para espanhol.

---
+++ resources/lang/es/meses.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+*Tradução dos textos para Espanhol
+ */
+
+
+return [
+    'mes_1'  => 'January',
+    'mes_2'  => 'February',
+    'mes_3'  => 'March',
+    'mes_4'  => 'April',
+    'mes_5'  => 'May',
+    'mes_6'  => 'June',
+    'mes_7'  => 'July',
+    'mes_8'  => 'August',
+    'mes_9'  => 'September',
+    'mes_10' => 'October',
+    'mes_12' => 'November',
+    'mes_12' => 'December',
+];
