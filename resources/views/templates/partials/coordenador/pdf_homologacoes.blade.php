c92732236ec64ebf3466388e90ec35935933ce3e
jfreitasmat <jfreitas@mat.unb.br>
2018-09-25 18:10:08 -0300

    Formatação.

--- resources/views/templates/partials/coordenador/pdf_homologacoes.blade.php
+++ resources/views/templates/partials/coordenador/pdf_homologacoes.blade.php
@@ -61,7 +61,7 @@
             }
         </script>
 
-        <h2>Homologação das inscrições para o Programa de Pós-graduação em Matemática - {{ $dados_homologacao['numero_semestre'] }}º/{{ $dados_homologacao['ano_inicio'] }}</h2>
+        <h2>Homologação das inscrições para o Programa de Pós-Graduação em Matemática - {{ $dados_homologacao['numero_semestre'] }}º/{{ $dados_homologacao['ano_inicio'] }}</h2>
         <div>
             <p>O Departamento de Matemática torna pública a homologação das inscrições para {{ $dados_homologacao['texto_cursos_pos'] }} em Matemática, com início no {{ $dados_homologacao['texto_semestre'] }} período letivo de {{ $dados_homologacao['ano_inicio'] }}, conforme item 7.1 do edital nº {{ $dados_homologacao['edital'] }}. </p>
         </div>
