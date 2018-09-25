f4043098b92cfe0baa0bf42885908b191521ef44
jfreitasmat <jfreitas@mat.unb.br>
2018-09-25 15:53:25 -0300

    Variáveis com o ano e semestre.

--- resources/views/templates/partials/coordenador/pdf_homologacoes.blade.php
+++ resources/views/templates/partials/coordenador/pdf_homologacoes.blade.php
@@ -37,9 +37,9 @@
             }
         </script>
 
-        <h2>Homologação das inscrições para o Programa de Pós-graduação em Matemática - SEMESTRE/ANO</h2>
+        <h2>Homologação das inscrições para o Programa de Pós-graduação em Matemática - {{ $numero_semestre }}/{{ $ano }}</h2>
         <div>
-            <p>O Departamento de Matemática torna pública a homologação das inscrições para {{ $texto_cursos_pos }} Acadêmico em Matemática, com início no primeiro período letivo de 2019, conforme item 7.1 do edital nº {{ $edital }}. </p>
+            <p>O Departamento de Matemática torna pública a homologação das inscrições para {{ $texto_cursos_pos }} Acadêmico em Matemática, com início no {{ $texto_semestre }} período letivo de {{ $ano }}, conforme item 7.1 do edital nº {{ $edital }}. </p>
         </div>
     </body>
 </html>
