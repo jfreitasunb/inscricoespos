297d4fac349f3b946acebcfb65e307a2962c0084
jfreitasmat <jfreitas@mat.unb.br>
2018-09-25 14:59:58 -0300

    Mais identação.

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -19,30 +19,29 @@ class BaseController extends Controller
 
 	public $periodo_inscricao;
 
-    public $texto_inscricao_pos;
+  public $texto_inscricao_pos;
 
-	public function __construct() {
+	public function __construct()
+  {
 
-       $inscricao_pos = new ConfiguraInscricaoPos();
+    $inscricao_pos = new ConfiguraInscricaoPos();
 
-	   $periodo_inscricao = $inscricao_pos->retorna_periodo_inscricao();
+    $periodo_inscricao = $inscricao_pos->retorna_periodo_inscricao();
 
-       $texto_inscricao_pos = $inscricao_pos->define_texto_inscricao();
+    $texto_inscricao_pos = $inscricao_pos->define_texto_inscricao();
 
-       // dd($texto_inscricao_pos);
+    View::share ( 'periodo_inscricao', $periodo_inscricao );
 
-       View::share ( 'periodo_inscricao', $periodo_inscricao );
+    View::share ( 'texto_inscricao_pos', $texto_inscricao_pos );
+  }
 
-       View::share ( 'texto_inscricao_pos', $texto_inscricao_pos );
+  public function SetUser()
+  {
+    if (session()->has('impersonate')) {
+        
+        return User::find(session()->get('impersonate'));
+    }else{
+        return Auth::user();
     }
-
-    public function SetUser()
-    {
-        if (session()->has('impersonate')) {
-            
-            return User::find(session()->get('impersonate'));
-        }else{
-            return Auth::user();
-        }
-    }
+  }
 }
