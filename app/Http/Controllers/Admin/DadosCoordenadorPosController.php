f500c3e93ffdeff546fc3151d3c240b7afa16b06
jfreitasmat <jfreitas@mat.unb.br>
2018-09-26 20:59:40 -0300

    Models e controllers desnecessários.

--- app/Http/Controllers/Admin/DadosCoordenadorPosController.php
+++ app/Http/Controllers/Admin/DadosCoordenadorPosController.php
@@ -3,20 +3,13 @@
 namespace InscricoesPos\Http\Controllers\Admin;
 
 use Auth;
-use DB;
-use Mail;
 use Session;
-use Notification;
-use Carbon\Carbon;
-use InscricoesPos\Models\{User, ConfiguraInscricaoPos, AreaPosMat, ProgramaPos, RelatorioController, FinalizaInscricao, ContatoRecomendante, DadoRecomendante, DadoPessoal, EscolhaCandidato, CartaRecomendacao, AssociaEmailsRecomendante};
+use InscricoesPos\Models\{User, DadosCoordenadorPos};
 use Illuminate\Http\Request;
-use InscricoesPos\Mail\EmailVerification;
 use InscricoesPos\Http\Controllers\Controller;
 use InscricoesPos\Http\Controllers\AuthController;
 use InscricoesPos\Http\Controllers\CoordenadorController;
 use InscricoesPos\Http\Controllers\DataTable\UserController;
-use InscricoesPos\Notifications\NotificaRecomendante;
-use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Pagination\LengthAwarePaginator;
 
