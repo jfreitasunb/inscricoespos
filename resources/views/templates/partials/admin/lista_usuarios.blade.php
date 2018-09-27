0741b47f44f487ce5f03571bbd3fb42fcc4f87c5
jfreitasmat <jfreitas@mat.unb.br>
2018-09-27 17:18:03 -0300

    Controller e view para usar com o Vue.

---
+++ resources/views/templates/partials/admin/lista_usuarios.blade.php
@@ -0,0 +1,20 @@
+@extends('templates.default')
+
+@section('stylesheets')
+  {!! Html::style( asset('css/parsley.css') ) !!}
+  {!! Html::style( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') ) !!}
+  {!! Html::style( asset('bower_components/moment/locale/fr.js') ) !!}
+@endsection
+
+@section('lista_usuarios')
+
+
+
+@endsection
+
+@section('scripts')
+  {!! Html::script( asset('bower_components/moment/min/moment.min.js') ) !!}
+  {!! Html::script( asset('bower_components/moment/locale/pt-br.js') ) !!}
+  {!! Html::script( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') ) !!}
+  {!! Html::script( asset('bower_components/moment/locale/fr.js') ) !!}
+@endsection
