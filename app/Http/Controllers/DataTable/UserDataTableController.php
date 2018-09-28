2c6d5bb8c9519a1f2a6bbc765f529a5bfa682903
jfreitasmat <jfreitas@mat.unb.br>
2018-09-27 21:01:37 -0300

    Datatable para gerenciar os usuários.

--- app/Http/Controllers/DataTable/UserDataTableController.php
+++ app/Http/Controllers/DataTable/UserDataTableController.php
@@ -5,7 +5,7 @@ namespace InscricoesPos\Http\Controllers\DataTable;
 use Illuminate\Http\Request;
 use InscricoesPos\Http\Controllers\Controller;
 
-class UserDataTableController extends Controller
+class UserDataTableController extends DataTableController
 {
     //
 }
