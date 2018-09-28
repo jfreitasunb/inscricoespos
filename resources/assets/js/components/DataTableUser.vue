ec7774e7e3fc7a4451fe2545b98cc36e7478406d
jfreitasmat <jfreitas@mat.unb.br>
2018-09-27 22:03:42 -0300

    Começando a tabela com os dados.

--- resources/assets/js/components/DataTableUser.vue
+++ resources/assets/js/components/DataTableUser.vue
@@ -3,15 +3,25 @@
         <div class="panel-heading">NAME OF TABLE</div>
 
         <div class="panel-body">
-            
+            <div class="table-responsive">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Coluna</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
+        props: ['endpoint'],
         mounted () {
-            
+            console.log(this.endpoint)
         }
     }
 </script>
+
