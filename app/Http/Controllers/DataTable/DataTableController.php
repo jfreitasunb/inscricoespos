6371e18e14096d6eb77db3219bc922f75db9cf36
jfreitasmat <jfreitas@mat.unb.br>
2018-09-27 21:16:23 -0300

    Construindo o builder.

--- app/Http/Controllers/DataTable/DataTableController.php
+++ app/Http/Controllers/DataTable/DataTableController.php
@@ -2,13 +2,27 @@
 
 namespace InscricoesPos\Http\Controllers\DataTable;
 
+use Exception;
 use Illuminate\Http\Request;
 use InscricoesPos\Http\Controllers\Controller;
 use InscricoesPos\Http\Controllers\BaseController;
+use Illuminate\Database\Eloquent\Builder;
 
 abstract class DataTableController extends BaseController
 {
+    protected $builder;
+
     abstract public function builder();
+
+    public function __construct()
+    {
+        $builder = $this->builder();
+
+        if (!$builder instanceof Builder) {
+            throw new Exception("Entity builder not instance of Builder");
+            
+        }
+    }
 
     public function index()
     {
