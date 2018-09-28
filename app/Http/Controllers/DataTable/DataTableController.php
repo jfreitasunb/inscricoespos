1a10eddf4f38856029bb5344311c53b521e1a4bd
jfreitasmat <jfreitas@mat.unb.br>
2018-09-27 21:06:29 -0300

    Incluindo o controller básico.

--- app/Http/Controllers/DataTable/DataTableController.php
+++ app/Http/Controllers/DataTable/DataTableController.php
@@ -4,6 +4,7 @@ namespace InscricoesPos\Http\Controllers\DataTable;
 
 use Illuminate\Http\Request;
 use InscricoesPos\Http\Controllers\Controller;
+use InscricoesPos\Http\Controllers\BaseController;
 
 abstract class DataTableController extends BaseController
 {
