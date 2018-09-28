da7244ae04c5e772f241522d6e3ef0a030d11d33
jfreitasmat <jfreitas@mat.unb.br>
2018-09-27 21:09:28 -0300

    Retornando uma query builder.

--- app/Http/Controllers/DataTable/UserDataTableController.php
+++ app/Http/Controllers/DataTable/UserDataTableController.php
@@ -5,7 +5,12 @@ namespace InscricoesPos\Http\Controllers\DataTable;
 use Illuminate\Http\Request;
 use InscricoesPos\Http\Controllers\Controller;
 
+use InscricoesPos\Models\User;
+
 class UserDataTableController extends DataTableController
 {
-    //
+    public function builder()
+    {
+        return User::query();
+    }
 }
