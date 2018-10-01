c400c4caf4991ac840e97ad2b29621d3b0bbc2fd
jfreitasmat <jfreitas@mat.unb.br>
2018-10-01 20:50:16 -0300

    Iniciando campo de pesquisa rápida.

--- resources/assets/js/components/DataTableUser.vue
+++ resources/assets/js/components/DataTableUser.vue
@@ -2,6 +2,15 @@
     <div class="panel panel-default">
         <div class="panel-heading">{{ response.table }}</div>
         <div class="panel-body">
+            <div class="row">
+                <div class="form-group col-md-10">
+                    <label for="filter">Pesquisa rápida</label>
+                    <input type="text" id="filter" class="form-control" v-model="quickSearchQuery">
+                </div>
+                <div class="form-group col-md-2">
+                    
+                </div>
+            </div>
             <div class="table-responsive">
                 <table class="table table-striped">
                     <thead>
@@ -40,7 +49,9 @@
                 sort: {
                 key: 'id_user',
                 order: 'asc'
-            }
+            },
+
+            quickSearchQuery: ''
             }
         },
 
