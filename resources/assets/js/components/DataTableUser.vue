de9145efb9d1c57879c2aee8f85ee84105e3815e
jfreitasmat <jfreitas@mat.unb.br>
2019-02-05 15:01:44 -0200

    Atualizando dos dados.

--- resources/assets/js/components/DataTableUser.vue
+++ resources/assets/js/components/DataTableUser.vue
@@ -160,6 +160,13 @@
 
                 axios.patch(`${this.endpoint}/${this.editing.id_user}`, this.editing.form).then(() => {
 
+                    this.getRecords().then(() => {
+
+                        this.editing.id_user = null
+                        this.editing.form = {}
+
+                    })
+
                 })
             }
         },
