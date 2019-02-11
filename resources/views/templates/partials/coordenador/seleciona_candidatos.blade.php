6b3ede662bf49f54ab8cc4da399f4130ed095fd4
jfreitasmat <jfreitas@mat.unb.br>
2019-02-11 14:57:15 -0200

    Tela de seleção.

--- resources/views/templates/partials/coordenador/seleciona_candidatos.blade.php
+++ resources/views/templates/partials/coordenador/seleciona_candidatos.blade.php
@@ -11,7 +11,7 @@
 <div id="app">
   <div class="row">
     <div class="col-md-12">
-      <seleciona-candidatos route="{{route('pdf.homologa.inscricoes')}}" endpoint="{{ route('homologa.index') }}"></seleciona-candidatos>
+      <seleciona-candidatos route="{{route('pdf.homologa.inscricoes')}}" endpoint="{{ route('seleciona.index') }}"></seleciona-candidatos>
     </div>
   </div>
 </div>
