edf4ef2e5ff37993e85d39f0b3f1d4873a155149
jfreitasmat <jfreitas@mat.unb.br>
2019-02-12 15:15:24 -0200

    Exibindo o link de download se já tiver terminado a seleção.

--- resources/assets/js/components/SelecionaCandidatos.vue
+++ resources/assets/js/components/SelecionaCandidatos.vue
@@ -16,7 +16,7 @@
                         <option value="">Todos</option>
                     </select>
                 </div>
-                <div class="col-md-offset-3 col-md-6" v-if="response.total_inscritos !== response.total_homologados">
+                <div class="col-md-offset-3 col-md-6" v-if="response.total_selecionados === response.total_homologados">
                     <a :href="this.route" style="font-size:30px;"><span class="glyphicon glyphicon-download-alt"></span> Download do PDF com as Homologações</a>
                 </div>
             </div>
