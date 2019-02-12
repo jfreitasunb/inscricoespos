e54f0fa7b06f1fd54c15391ad69db64a723ac0e8
jfreitasmat <jfreitas@mat.unb.br>
2019-02-12 21:53:12 -0200

    Ajustes estéticos.

--- resources/views/templates/partials/coordenador/pdf_homologacoes.blade.php
+++ resources/views/templates/partials/coordenador/pdf_homologacoes.blade.php
@@ -4,32 +4,28 @@
         <meta charset="utf-8">
         <style>
             h2 {text-align:center;}
+            
             label {font-weight: bold;}
+            
             label.motivacao {font-weight: normal;text-align:justify;}
+            
             p.motivacao {font-weight: normal;text-align:justify;}
+            
             .page_break { page-break-before: always;}
-            #wrap_texto {
-                margin-top: 20px;
-                
-            }
-            #wrap_tabela {
-                margin-top: 100px !important;
-                margin-bottom: 20px;
-            }
-            #header {
-                position: fixed;
-                /*border-bottom:1px solid gray;*/
-            }
+            
             .pagenum:before {
                 content: counter(page);
             }
+            
             p:last-child { page-break-after: never; }
+            
             table.customTable {
                 border-collapse: collapse;
                 border-width: 2px;
                 border-color: #080A0F;
                 border-style: solid;
                 color: #000000;
+                margin-top: 100px !important;
             }
 
             table.customTable th.tg-baqh{
@@ -47,14 +43,6 @@
                 border-width: 2px;
                 border-color: #080A0F;
                 border-style: solid;
-                padding: 5px;
-            }
-
-            table.customTable thead {
-                background-color: #F8F8F8;
-            }
-            table.customTable thead tr {
-                padding-top: 200px;
             }
         </style>
     </head>
@@ -74,36 +62,35 @@
                 $pdf->page_text(540, 750, "Página {PAGE_NUM}/{PAGE_COUNT}", $font, 7, array(0, 0, 0));
             }
         </script>
-        <div id="header"><hr></div>
-        <div id="wrap_texto">
+        <header><hr></header>
+        <div>
             <h2>Homologação das inscrições para o Programa de Pós-Graduação em Matemática - {{ $dados_homologacao['numero_semestre'] }}º/{{ $dados_homologacao['ano_inicio'] }}</h2>
             <div>
                 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;O Departamento de Matemática torna pública a homologação das inscrições para {{ $dados_homologacao['texto_cursos_pos'] }} em Matemática, com início no {{ $dados_homologacao['texto_semestre'] }} período letivo de {{ $dados_homologacao['ano_inicio'] }}, conforme item 7.1 do edital nº {{ $dados_homologacao['edital'] }}. </p>
             </div>
         </div>
         
-            @foreach ($homologacoes as $key => $candidato)
-                <div id="wrap_tabela">
-                    <h3 align="center">{{ $key }}:</h3>
-                    <table class="customTable" align="center">
-                      <tr>
-                        <th class="tg-baqh" align="center">Ordem</th>
-                        <th class="tg-0lax" align="center">Nome</th>
-                      </tr>
-                      @foreach ($candidato as $ordem => $nome)
-                        <tr>
-                            <td class="tg-baqh" align="center">{{ $ordem + 1}}</td>
-                            <td class="tg-0lax">{{ $nome }}</td>
-                        </tr>
-                      @endforeach
-                    </table>
-                </div>
-            @endforeach
+        @foreach ($homologacoes as $key => $candidato)
+            <div id="wrap_tabela">
+                <h3 align="center">{{ $key }}:</h3>
+                <table class="customTable" align="center">
+                  <tr>
+                    <th class="tg-baqh" align="center">Ordem</th>
+                    <th class="tg-0lax" align="center">Nome</th>
+                  </tr>
+                  @foreach ($candidato as $ordem => $nome)
+                    <tr>
+                        <td class="tg-baqh" align="center">{{ $ordem + 1}}</td>
+                        <td class="tg-0lax">{{ $nome }}</td>
+                    </tr>
+                  @endforeach
+                </table>
+            </div>
+        @endforeach
         <div>
             <p align="center">Brasília, {{ $dados_homologacao['dia'] }} de {{ $dados_homologacao['nome_mes'] }} de {{ $dados_homologacao['ano_homologacao'] }}</p>
             <p align="center">{{ $dados_homologacao['nome_coordenador'] }}</p>
             <p align="center">{{ $dados_homologacao['tratamento'] }} de Pós-Graduação MAT/UnB</p>
         </div>
-    
     </body>
 </html>
