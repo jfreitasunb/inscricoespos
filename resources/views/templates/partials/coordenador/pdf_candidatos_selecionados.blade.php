535464a0ebbac0ac02453cb34f99b8450b385be8
jfreitasmat <jfreitas@mat.unb.br>
2019-02-14 20:27:17 -0200

    Mais alguns ajustes.

--- resources/views/templates/partials/coordenador/pdf_candidatos_selecionados.blade.php
+++ resources/views/templates/partials/coordenador/pdf_candidatos_selecionados.blade.php
@@ -4,22 +4,44 @@
     <head>
         <meta charset="utf-8">
         <style>
-            h2 {text-align:center;}
-            label {font-weight: bold;}
-            label.motivacao {font-weight: normal;text-align:justify;}
-            p.motivacao {font-weight: normal;text-align:justify;}
-            .page_break { page-break-before: always;}
-            .footer {
-                width: 100%;
-                text-align: center;
-                position: fixed;
-                font-size: 8pt;
-                bottom: 0px;
+            body {
+                margin: 100px;
             }
+
+            #header{
+                position: fixed;
+            }
+
+            #unb{
+                font-size: 10px;
+            }
+
+            #ied{
+                font-size: 8px;
+            }
+
+            #fonemat{
+                font-size: 8px;
+                text-align: right;
+                margin-top: -5px;
+            }
+
+            h2 {text-align:center;}
+            
+            label {font-weight: bold;}
+            
+            label.motivacao {font-weight: normal;text-align:justify;}
+            
+            p.motivacao {font-weight: normal;text-align:justify;}
+            
+            .page_break { page-break-before: always;}
+            
             .pagenum:before {
                 content: counter(page);
             }
+            
             p:last-child { page-break-after: never; }
+            
             table.customTable {
                 border-collapse: collapse;
                 border-width: 2px;
@@ -56,22 +78,39 @@
     <body>
         <script type="text/php">
             if (isset($pdf)) {
+                $w = $pdf->get_width();
                 $font = $fontMetrics->getFont("Arial", "bold");
-                $pdf->page_text(35, 750, "Campus Universitário Darcy Ribeiro
-ICC-Centro - Bloco A - Sala - AT-425 Térreo
-Brasília – DF – CEP 70.910-900", $font, 7, array(0, 0, 0) );
+                $pdf->page_script('
+                    $w = $pdf->get_width();
+                    $pdf->line(35,750,$w-35,750,array(0,0,0),0.5);
+                ');
+                $pdf->page_text(40, 750, "PPG/MAT-UnB", $font, 7, array(0, 0, 0));
                 $pdf->page_text(540, 750, "Página {PAGE_NUM}/{PAGE_COUNT}", $font, 7, array(0, 0, 0));
             }
         </script>
-
+        <div id="header">
+            <hr style="height:1.5px;border:none;color:#333;background-color:#333;">
+            <div id="unb">
+                UnB-Universidade de Brasília
+            </div>
+            <div id="ied">
+                <img src="{{ public_path('imagens/logo/unb_contorno.png') }}" alt="Logo" height="25px" align="right" vspace="-10px">
+                IE-Instituto de Ciências Exatas<br>MAT-Departamento de Matemática
+                <br>
+                <hr style="height:0.75px;border:none;color:#333;background-color:#333;">
+                Campus Universitário Darcy Ribeiro 70.910-900
+                <br>
+            </div>
+                <div id="fonemat">
+                    Fone: (61) 3107-6479/6480 Fax: (61) 3107-6482
+                </div>
+        </div>
         <h2>Programa de Pós-Graduação em Matemática</h2>
         <br>
         <h3>Resultado Final da Seleção para o Programa de Pós-graduação em Matemática - {{ $dados_homologacao['numero_semestre'] }}º/{{ $dados_homologacao['ano_inicio'] }}</h3>
         <div>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;O Departamento de Matemática torna público o Resultado Final do Processo de Seleção para {{ $dados_homologacao['texto_cursos_pos'] }} em Matemática, com início no {{ $dados_homologacao['texto_semestre'] }} período letivo de {{ $dados_homologacao['ano_inicio'] }}, conforme item 7.1 do edital nº {{ $dados_homologacao['edital'] }}. </p>
         </div>
-
-        <div><br></div>
 
         <div>
             @foreach ($homologacoes as $key => $candidato)
@@ -91,11 +130,9 @@ Brasília – DF – CEP 70.910-900", $font, 7, array(0, 0, 0) );
                 </table>
             @endforeach
         </div>
-        <div><br></div>
         <div>
             <p align="right">Brasília, {{ $dados_homologacao['dia'] }} de {{ $dados_homologacao['nome_mes'] }} de {{ $dados_homologacao['ano_homologacao'] }}</p>
         </div>
-        <div><br></div>
         <div>
             <p align="center">{{ $dados_homologacao['nome_coordenador'] }}</p>
             <p align="center">{{ $dados_homologacao['tratamento'] }} de Pós-Graduação MAT/UnB</p>
--- resources/views/templates/partials/coordenador/pdf_homologacoes.blade.php
+++ resources/views/templates/partials/coordenador/pdf_homologacoes.blade.php
@@ -25,7 +25,6 @@
                 margin-top: -5px;
             }
             
-
             h2 {text-align:center;}
             
             label {font-weight: bold;}
@@ -78,13 +77,6 @@
                     $w = $pdf->get_width();
                     $pdf->line(35,750,$w-35,750,array(0,0,0),0.5);
                 ');
-                {{-- $pdf->line(35,10,$w-35,10,array(0,0,0),1.5);
-                $pdf->page_text(35, 10, "UnB-Universidade de Brasília", $font, 7, array(0, 0, 0));
-                $pdf->page_text(35, 20, "IE-Instituto de Ciências Exatas", $font, 7, array(0, 0, 0) );
-                $pdf->page_text(35, 30, "MAT-Departamento de Matemática", $font, 7, array(0, 0, 0) );
-                
-                $pdf->page_text(35, 40, "Campus Universitário Darcy Ribeiro 70.910-900", $font, 7, array(0, 0, 0) );
-                $pdf->page_text(440, 40, "Fone: (61) 3107-6479/6480 Fax: (61) 3107-6482", $font, 7, array(0, 0, 0) ); --}}
                 $pdf->page_text(40, 750, "PPG/MAT-UnB", $font, 7, array(0, 0, 0));
                 $pdf->page_text(540, 750, "Página {PAGE_NUM}/{PAGE_COUNT}", $font, 7, array(0, 0, 0));
             }
@@ -102,7 +94,10 @@
                 Campus Universitário Darcy Ribeiro 70.910-900
                 <br>
             </div>
-                <div id="fonemat">Fone: (61) 3107-6479/6480 Fax: (61) 3107-6482</div></div>
+                <div id="fonemat">
+                    Fone: (61) 3107-6479/6480 Fax: (61) 3107-6482
+                </div>
+        </div>
         <div>
             <h2>Homologação das inscrições para o Programa de Pós-Graduação em Matemática - {{ $dados_homologacao['numero_semestre'] }}º/{{ $dados_homologacao['ano_inicio'] }}</h2>
             <div>
