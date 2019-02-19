b3e43a93c6d25ebd4e7de74c83f4955a0ebe16b2
jfreitasmat <jfreitas@mat.unb.br>
2019-02-19 15:35:05 -0300

    Novo menu do coordenador.

--- resources/views/templates/partials/coordenador/menu_coordenador.blade.php
+++ resources/views/templates/partials/coordenador/menu_coordenador.blade.php
@@ -1,30 +1,142 @@
 <div class="row">
-  <nav class="navbar navbar-default-coord col-md-8 col-md-offset-2" role="navigation">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bar1">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-      </button>
+    <div class="col-sm-3 col-md-2">
+        <div class="panel-group" id="accordion">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseDois"><span class="glyphicon glyphicon-file fa-fw">
+                            </span>Inscrições</a>
+                        </h4>
+                    </div>
+                    <div id="collapseDois" class="panel-collapse collapse {{ $keep_open_accordion_inscricoes }}">
+                        <div class="panel-body">
+                            <table class="table">
+                              <tr>
+                                <td class= "{{ Route::currentRouteNamed('dados.coordenador.pos') ? 'active_link' : '' }}">
+                                    <span class="glyphicon glyphicon-cog fa-fw"></span><a href="{{ route('dados.coordenador.pos') }}">Dados do coordenador da Pós</a>
+                                </td>
+                              </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('configura.inscricao') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-wrench fa-fw"></span><a href="{{ route('configura.inscricao') }}">Configurar Inscrição</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('cadastra.area.pos') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-wrench fa-fw"></span><a href="{{ route('cadastra.area.pos') }}">Cadastrar nova área Pós</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('editar.area.pos') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-wrench fa-fw"></span><a href="{{ route('editar.area.pos') }}">Editar áreas Pós</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('editar.formacao') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-wrench fa-fw"></span><a href="{{ route('editar.formacao') }}">Editar Formação</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('configura.periodo.confirmacao') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-wrench fa-fw"></span><a href="{{ route('configura.periodo.confirmacao') }}">Configurar Período Confirmação</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('lista.recomendacoes') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-list fa-fw"></span><a href="{{ route('lista.recomendacoes') }}">Lista as indicações por candidato</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('homologa.inscricoes') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-list fa-fw"></span><a href="{{ route('homologa.inscricoes') }}">Homologa Inscrições</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('seleciona.candidatos') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-list fa-fw"></span><a href="{{ route('seleciona.candidatos') }}">Candidatos Selecionados</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('status.selecionados') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-list fa-fw"></span><a href="{{ route('status.selecionados') }}">Status das Confirmações</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTres"><span class="glyphicon glyphicon-file fa-fw">
+                            </span>Relatórios</a>
+                        </h4>
+                    </div>
+                    <div id="collapseTres" class="panel-collapse collapse {{ $keep_open_accordion_relatorios }}">
+                        <div class="panel-body">
+                            <table class="table">
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('auxilia.selecao') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-duplicate fa-fw"></span><a href="{{ route('auxilia.selecao') }}">Auxilia Seleção</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('relatorio.atual') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-duplicate fa-fw"></span><a href="{{ route('relatorio.atual') }}">Edital Vigente</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('relatorio.anteriores') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-backward fa-fw"></span><a href="{{ route('relatorio.anteriores') }}">Edital Anterior</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('gera.ficha.individual') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-file fa-fw"></span><a href="{{ route('gera.ficha.individual') }}">Por Candidato</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('link.acesso') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-file fa-fw"></span><a href="{{ route('link.acesso') }}">Link de Acesso</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class= "{{ Route::currentRouteNamed('ver.charts') ? 'active_link' : '' }}">
+                                        <span class="glyphicon glyphicon-stats fa-fw"></span><a href="{{ route('ver.charts') }}">Gráficos</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <span class="glyphicon glyphicon-log-out fa-fw"></span><a href="{{ route('auth.logout') }}">Sair</a>
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-9 col-md-10">
+            <div class="menuadmin well">
+                @yield('dados_coordenador_pos')
+                @yield('edita_area_pos')
+                @yield('admin_impersonate')
+                @yield('cadastra_area_pos')
+                @yield('edita_formacao')
+                @yield('configura_inscricao')
+                @yield('configurar_periodo_confirmacao')
+                @yield('homologa_inscricoes')
+                @yield('seleciona_candidatos')
+                @yield('status_selecionados')
+                @yield('tabela_indicacoes')
+                @yield('relatorio_pos_edital_vigente')
+                @yield('relatorio_pos_editais_anteriores')
+                @yield('link_acesso')
+                @yield('ficha_individual')
+                @yield('graficos')
+                @yield('auxilia_selecao')
+            </div>
+        </div>
     </div>
-    <div class="collapse navbar-collapse" id="bar1">
-      <ul class="nav navbar-nav">
-        <li class="{{ Route::currentRouteNamed('configura.inscricao') ? 'active' : '' }}"><a href="{{ route('configura.inscricao') }}">Configurar Inscrição</a></li>
-        <li class="{{ Route::currentRouteNamed('cadastra.area.pos') ? 'active' : '' }}"><a href="{{ route('cadastra.area.pos') }}">Cadastrar nova área Pós</a></li>
-        <li class="{{ Route::currentRouteNamed('editar.area.pos') ? 'active' : '' }}"><a href="{{ route('editar.area.pos') }}">Editar áreas Pós</a></li>
-        <li class="{{ Route::currentRouteNamed('editar.formacao') ? 'active' : '' }}"><a href="{{ route('editar.formacao') }}">Editar Formação</a></li>
-        <li class="{{ Route::currentRouteNamed('relatorio.atual') ? 'active' : '' }}"><a href="{{ route('relatorio.atual') }}">Relatórios Edital Vigente</a></li>
-        <li class="{{ Route::currentRouteNamed('gera.ficha.individual') ? 'active' : '' }}"><a href="{{ route('gera.ficha.individual') }}">Ficha Individual</a></li>
-        <li class="{{ Route::currentRouteNamed('relatorio.anteriores') ? 'active' : '' }}"><a href="{{ route('relatorio.anteriores') }}">Relatórios Editais Anteriores</a></li>
-        <li class="{{ Route::currentRouteNamed('homologa.inscricoes') ? 'active' : '' }}"><a href="{{ route('homologa.inscricoes') }}">Homologa Inscrições</a></li>
-        <li class="{{ Route::currentRouteNamed('seleciona.candidatos') ? 'active' : '' }}"><a href="{{ route('seleciona.candidatos') }}">Candidatos Selecionados</a></li>
-        <li class="{{ Route::currentRouteNamed('status.selecionados') ? 'active' : '' }}"><a href="{{ route('status.selecionados') }}">Status das Confirmações</a></li>
-        <li class="{{ Route::currentRouteNamed('lista.recomendacoes') ? 'active' : '' }}"><a href="{{ route('lista.recomendacoes') }}">Cartas recebidas</a></li>
-        <li class="{{ Route::currentRouteNamed('link.acesso') ? 'active' : '' }}"><a href="{{ route('link.acesso') }}">Link de Acesso</a></li>
-        <li class="{{ Route::currentRouteNamed('auth.logout') ? 'active' : '' }}"><a href="{{ route('auth.logout') }}">Sair</a></li>
-      </ul>
-    </div>
-  </nav>
-</div>
