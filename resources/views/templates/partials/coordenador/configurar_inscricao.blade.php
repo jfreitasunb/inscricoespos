3e481b104ebca197133436e1c85ca81b94290693
jfreitasmat <jfreitas@mat.unb.br>
2019-02-19 15:40:27 -0300

    Campo para upload do edital em inglês e espanhol, caso existam.

--- resources/views/templates/partials/coordenador/configurar_inscricao.blade.php
+++ resources/views/templates/partials/coordenador/configurar_inscricao.blade.php
@@ -91,18 +91,44 @@
           {!! Form::text('edital_numero', null, ['class' => 'form-group', 'required' => '', 'data-parsley-type' => 'integer']) !!}
         </div>
 
+        <legend>Arquivo com Edital em Português</legend>
         <span class="input-group-btn">
           <button type="button" class="btn btn-primary" style="display:none;">
-            <span class="glyphicon glyphicon-remove"></span> Clear
+            <span class="glyphicon glyphicon-remove"></span>
           </button>
           <div class="register-submit btn btn-primary">
-            <input type="file" accept="application/pdf" name="edital" required=""/> <!-- rename it -->
+            <input type="file" accept="application/pdf" name="edital_portugues" required=""/>
           </div>
         </span>
-        @if ($errors->has('edital'))
-          <span class="help-block">{{ $errors->first('edital') }}</span>
+        @if ($errors->has('edital_portugues'))
+          <span class="help-block">{{ $errors->first('edital_portugues') }}</span>
         @endif
-        
+        <legend></legend>
+        <legend>Arquivo com Edital em Inglês</legend>
+        <span class="input-group-btn">
+          <button type="button" class="btn btn-primary" style="display:none;">
+            <span class="glyphicon glyphicon-remove"></span>
+          </button>
+          <div class="register-submit btn btn-primary">
+            <input type="file" accept="application/pdf" name="edital_ingles"/>
+          </div>
+        </span>
+        @if ($errors->has('edital_ingles'))
+          <span class="help-block">{{ $errors->first('edital_ingles') }}</span>
+        @endif
+        <legend></legend>
+        <legend>Arquivo com Edital em Espanhol</legend>
+        <span class="input-group-btn">
+          <button type="button" class="btn btn-primary" style="display:none;">
+            <span class="glyphicon glyphicon-remove"></span>
+          </button>
+          <div class="register-submit btn btn-primary">
+            <input type="file" accept="application/pdf" name="edital_espanhol"/>
+          </div>
+        </span>
+        @if ($errors->has('edital_espanhol'))
+          <span class="help-block">{{ $errors->first('edital_espanhol') }}</span>
+        @endif
         <legend></legend>
         <div class="col-md-10 text-center"> 
           {!! Form::submit('Salvar', array('class' => 'register-submit btn btn-primary btn-lg', 'id' => 'register-submit', 'tabindex' => '4')) !!}
