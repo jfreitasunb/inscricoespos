1c68108b454b6dcd7989fae2cb8a881f5aef9f72
jfreitasmat <jfreitas@mat.unb.br>
2019-02-27 21:40:17 -0300

    Removendo tela de confirmação.

--- resources/views/templates/partials/candidato/finalizar_inscricao.blade.php
+++ resources/views/templates/partials/candidato/finalizar_inscricao.blade.php
@@ -25,7 +25,7 @@
       <div class="form-group">
         <div class="row">
           <div class="col-md-6 col-md-offset-3 text-center">
-            {!! Form::button(trans('tela_finalizar_inscricao.menu_envio_definitivo'), ['class' => 'btn btn-warning btn-lg register-submit', 'onclick' => 'return archiveFunction(event)', 'id' => 'envio_definitivo']) !!}
+            {!! Form::submit(trans('tela_finalizar_inscricao.menu_envio_definitivo'), ['class' => 'btn btn-danger btn-lg register-submit']) !!}
           </div>
         </div>
       </div>
@@ -33,31 +33,6 @@
   
 @endsection
 
-@section('post-script')
-<script>
-function archiveFunction(event) {
-event.preventDefault(); // prevent form submit
-var form = event.target.form; // storing the form
-        swal({
-  title: '{{ trans('tela_finalizar_inscricao.confirma_envio') }}',
-  text: '{{ trans('tela_finalizar_inscricao.texto_confirma_envio') }}',
-  type: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#DD6B55",
-  confirmButtonText: '{{ trans('tela_finalizar_inscricao.sim_envia') }}',
-  cancelButtonText: '{{ trans('tela_finalizar_inscricao.nao_envia') }}',
-}).then(function () {
-  form.submit();          // submitting the form when user press yes
-},function(dismiss){
-  if (dismiss === 'cancel') {
-    swal(
-      '{{ trans('tela_finalizar_inscricao.envio_cancelado') }}'
-    )
-  } 
-});
-}
-</script>
-@endsection
 @section('scripts')
   {!! Html::script( asset('js/parsley.min.js') ) !!}
   {!! Html::script( asset('i18n/pt-br.js') ) !!}
