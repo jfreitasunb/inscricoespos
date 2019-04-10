af689427984098f1122ff484b8ecd50c339424c4
jfreitasmat <jfreitas@mat.unb.br>
2019-04-10 14:08:53 -0300

    Somente iniciais do nome em maiúscula.

--- app/Http/Controllers/DataTable/SelecionaCandidatosDataTableController.php
+++ app/Http/Controllers/DataTable/SelecionaCandidatosDataTableController.php
@@ -115,9 +115,9 @@ class SelecionaCandidatosDataTableController extends DataTableController
                 $foi_selecionado = $seleciona->retorna_status_selecionado($id_inscricao_pos, $dados->id_candidato);
 
                 if (is_null($foi_selecionado)) {
-                    $dados_vue[] = ['id' => $i, 'id_candidato' => $dados->id_candidato, 'nome' => (User::find($dados->id_candidato))->nome, 'nome_programa_pretendido' => (ProgramaPos::find($id_programa_pretendido))->tipo_programa_pos_ptbr, 'id_inscricao_pos' => $dados->id_inscricao_pos, "id_programa_pretendido" => $id_programa_pretendido, 'selecionado' => 'nao_definido'];
+                    $dados_vue[] = ['id' => $i, 'id_candidato' => $dados->id_candidato, 'nome' => $this->titleCase((User::find($dados->id_candidato))->nome), 'nome_programa_pretendido' => (ProgramaPos::find($id_programa_pretendido))->tipo_programa_pos_ptbr, 'id_inscricao_pos' => $dados->id_inscricao_pos, "id_programa_pretendido" => $id_programa_pretendido, 'selecionado' => 'nao_definido'];
                 }else{
-                    $dados_vue[] = ['id' => $i, 'id_candidato' => $dados->id_candidato, 'nome' => (User::find($dados->id_candidato))->nome, 'nome_programa_pretendido' => (ProgramaPos::find($id_programa_pretendido))->tipo_programa_pos_ptbr, 'id_inscricao_pos' => $dados->id_inscricao_pos, "id_programa_pretendido" => $id_programa_pretendido, 'selecionado' => $foi_selecionado->selecionado, 'colocacao' => $foi_selecionado->classificacao];
+                    $dados_vue[] = ['id' => $i, 'id_candidato' => $dados->id_candidato, 'nome' => $this->titleCase((User::find($dados->id_candidato))->nome), 'nome_programa_pretendido' => (ProgramaPos::find($id_programa_pretendido))->tipo_programa_pos_ptbr, 'id_inscricao_pos' => $dados->id_inscricao_pos, "id_programa_pretendido" => $id_programa_pretendido, 'selecionado' => $foi_selecionado->selecionado, 'colocacao' => $foi_selecionado->classificacao];
                 }
 
                 $i++;
