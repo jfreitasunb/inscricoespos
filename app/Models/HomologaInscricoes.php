3726a15cf1b6a886b0802b80c4e956584cfab7c4
jfreitasmat <jfreitas@mat.unb.br>
2019-04-10 15:17:43 -0300

    Funções para contar somente que foi homologado.

--- app/Models/HomologaInscricoes.php
+++ app/Models/HomologaInscricoes.php
@@ -35,6 +35,12 @@ class HomologaInscricoes extends FuncoesModels
         return $this->where('id_inscricao_pos', $id_inscricao_pos)->count();
     }
 
+    public function retorna_total_inscricoes_homologadas_validas($id_inscricao_pos)
+    {
+        return $this->where('id_inscricao_pos', $id_inscricao_pos)->where('homologada', TRUE)->count();
+    }
+    
+
     public function retorna_dados_homologados($id_inscricao_pos, $locale)
     {
         $nome_coluna = $this->define_nome_coluna_tipo_programa_pos($locale);
