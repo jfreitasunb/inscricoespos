ad8683ccd51c57f4691567be8e64762dc88b2108
jfreitasmat <jfreitas@mat.unb.br>
2019-07-10 21:09:04 -0300

    Arquivo de traduções da tela de envio de documentos para matrícula.

---
+++ resources/lang/pt-br/tela_envia_documentos_matricula.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+*Tradução dos textos para Português
+ */
+
+
+return [
+    'documentos_matricula' => 'Documentos para Matrícula',
+    'nome_recomendante' => 'Nome',
+	'atualizar_dados' => 'Prezado(a) recomendante, antes de enviar sua recomendação, por favor atualize seus dados.',
+    'instituicao_recomendante' => 'Instituição',
+	'titulacao_recomendante' => 'Grau acadêmico mais alto obtido',
+    'area_recomendante' => 'Área',
+    'ano_titulacao' => 'Ano de obtenção deste grau',
+    'inst_obtencao_titulo' => 'Instituição de obtenção deste grau',
+    'endereco_recomendante' => 'Endereço institucional',
+    'menu_enviar' => 'Enviar',
+    'menu_editar' => 'Editar',
+];
