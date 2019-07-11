b9f2c1fdfd484f023ba26ea79d3b192a201209de
jfreitasmat <jfreitas@mat.unb.br>
2019-07-10 21:05:36 -0300

    Mais campos desnecessários.

--- resources/views/templates/partials/candidato/envia_documentos_matricula.blade.php
+++ resources/views/templates/partials/candidato/envia_documentos_matricula.blade.php
@@ -5,14 +5,8 @@
 @endsection
 
 @section('envia_documentos_matricula')
-<fieldset class="scheduler-border">
-  <legend class="scheduler-border"></legend>
-    <div class="row">
-      {{trans('tela_envia_documentos_matricula.texto_motivacao')}}
-    </div>
-</fieldset>
 
-{!! Form::open(array('route' => 'documentos.matricula', 'class' => 'form-horizontal', 'enctype' => 'multipart/form-data', 'data-parsley-validate' => '' )) !!}
+{!! Form::open(array('route' => 'envia.documentos.matricula', 'class' => 'form-horizontal', 'enctype' => 'multipart/form-data', 'data-parsley-validate' => '' )) !!}
   <fieldset class="scheduler-border">
         <legend class="scheduler-border">{{ trans('tela_envia_documentos_matricula.documentos_pessoais') }}</legend>
         <div class="form-horizontal"{{ $errors->has('documentos_pessoais') ? ' has-error' : '' }}>
@@ -78,23 +72,14 @@
         </div>
   </fieldset>
   
-  <fieldset class="scheduler-border">
-      <div class="row">
-        <p> {{ trans('tela_envia_documentos_matricula.concordancia_1') }} {{ link_to($arquivos_editais.$edital_pdf.'.pdf',trans('tela_envia_documentos_matricula.texto_link_edital'), array('target' => '_blank' )) }} {{ trans('tela_envia_documentos_matricula.concordancia_2') }}</p>
-        <label>
-          {!! Form::checkbox('concorda_termos', '1', null, ['class' => 'control-label', 'required' => '']) !!} {{ trans('tela_envia_documentos_matricula.concordancia_3') }}
-        </label>
+  <div class="form-group">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3 text-center">
+        {!! Form::submit(trans('tela_envia_documentos_matricula.menu_enviar'), ['class' => 'btn btn-primary btn-lg register-submit']) !!}
       </div>
-  </fieldset>
-
-      <div class="form-group">
-        <div class="row">
-          <div class="col-md-6 col-md-offset-3 text-center">
-            {!! Form::submit(trans('tela_envia_documentos_matricula.menu_enviar'), ['class' => 'btn btn-primary btn-lg register-submit']) !!}
-          </div>
-        </div>
-      </div>
-      {!! Form::close() !!}
+    </div>
+  </div>
+  {!! Form::close() !!}
   
 @endsection
 
