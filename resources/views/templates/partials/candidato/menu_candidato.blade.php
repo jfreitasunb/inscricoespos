39f9cef898c6016e160b47a030bc0d0932ee61f5
jfreitasmat <jfreitas@mat.unb.br>
2019-07-10 21:08:43 -0300

    Nome correto do arquivo com as traduções.

--- resources/views/templates/partials/candidato/menu_candidato.blade.php
+++ resources/views/templates/partials/candidato/menu_candidato.blade.php
@@ -20,10 +20,10 @@
           <li class="{{ Route::currentRouteNamed('status.cartas') ? 'active' : '' }}"><a href="{{ route('status.cartas') }}">{{ trans('tela_status_cartas.status_cartas') }}</a></li>
         @endstatuscarta
         @confirmacao_participacao
-            <li class="{{ Route::currentRouteNamed('confirma.presenca') ? 'active' : '' }}"><a href="{{ route('confirma.presenca') }}">{{ trans('envia_documentos_matricula.documentos_matricula') }}</a></li>
+            <li class="{{ Route::currentRouteNamed('confirma.presenca') ? 'active' : '' }}"><a href="{{ route('confirma.presenca') }}">{{ trans('tela_envia_documentos_matricula.documentos_matricula') }}</a></li>
           @endconfirmacao_participacao
           @envia_documentos_matricula
-            <li class="{{ Route::currentRouteNamed('envia.documentos.matricula') ? 'active' : '' }}"><a href="{{ route('envia.documentos.matricula') }}">{{ trans('envia_documentos_matricula.documentos_matricula') }}</a></li>
+            <li class="{{ Route::currentRouteNamed('envia.documentos.matricula') ? 'active' : '' }}"><a href="{{ route('envia.documentos.matricula') }}">{{ trans('tela_envia_documentos_matricula.documentos_matricula') }}</a></li>
           @endenvia_documentos_matricula
         <li class="{{ Route::currentRouteNamed('auth.logout') ? 'active' : '' }}"><a href="{{ route('auth.logout') }}">{{ trans('tela_sair.sair') }}</a></li>
         @impersonating_candidato
