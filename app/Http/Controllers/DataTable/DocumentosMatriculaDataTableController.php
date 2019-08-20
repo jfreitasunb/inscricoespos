8a5ef03c4e37404eac29d7e77fd7d4866cceb449
José <Freitas>
2019-08-20 16:10:46 -0300

    Começando a recuperar os dados corretos.

--- app/Http/Controllers/DataTable/DocumentosMatriculaDataTableController.php
+++ app/Http/Controllers/DataTable/DocumentosMatriculaDataTableController.php
@@ -22,7 +22,7 @@ class DocumentosMatriculaDataTableController extends DataTableController
     public function getDisplayableColumns()
     {
         return [
-            'id_candidato', 'id_inscricao_pos', 'programa_pretendido',
+            'id_candidato', 'id_inscricao_pos', 'id_programa_pretendido',
         ];
     }
 
@@ -71,12 +71,12 @@ class DocumentosMatriculaDataTableController extends DataTableController
 
     protected function getRecords(Request $request)
     {   
-        $dados_temporarios = $this->builder()->limit($request->limit)->where('desclassificado', FALSE)->orderBy('id_candidato')->get($this->getDisplayableColumns());
+        $dados_temporarios = $this->builder()->limit($request->limit)->where('arquivo_final', TRUE)->orderBy('id_candidato')->get($this->getDisplayableColumns());
 
         if (sizeof($dados_temporarios) > 0) {
             foreach ($dados_temporarios as $dados) {
 
-            $dados_vue[] = ['id_candidato' => $dados->id_candidato, 'nome' => (User::find($dados->id_candidato))->nome, 'nome_programa_pretendido' => (ProgramaPos::find($dados->programa_pretendido))->tipo_programa_pos_ptbr, 'id_inscricao_pos' => $dados->id_inscricao_pos, "id_programa_pretendido" => $dados->programa_pretendido];
+            $dados_vue[] = ['id_candidato' => $dados->id_candidato, 'nome' => (User::find($dados->id_candidato))->nome, 'nome_programa_pretendido' => (ProgramaPos::find($dados->id_programa_pretendido))->tipo_programa_pos_ptbr, 'id_inscricao_pos' => $dados->id_inscricao_pos, "id_programa_pretendido" => $dados->id_programa_pretendido];
             }
         }else{
             $dados_vue = [];
