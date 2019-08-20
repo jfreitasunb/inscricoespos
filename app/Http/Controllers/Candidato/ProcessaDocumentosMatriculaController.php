bd8d7cc82c5b90169d405835dccb78f2432decf4
jfreitasmat <jfreitas@mat.unb.br>
2019-08-19 21:58:10 -0300

    Preparação para gerar o PDF com o documento de matrícula.

--- app/Http/Controllers/Candidato/ProcessaDocumentosMatriculaController.php
+++ app/Http/Controllers/Candidato/ProcessaDocumentosMatriculaController.php
@@ -34,13 +34,24 @@ use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 use InscricoesPos\Rules\ArrayUnico;
 use InscricoesPos\Rules\NumeroUploads;
+use Symfony\Component\Process\Process;
 
 /**
 * Classe para manipulação do candidato.
 */
 class ProcessaDocumentosMatriculaController extends BaseController
 {
-
+	protected $normalizeChars = array(
+      'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
+      'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
+      'Ï'=>'I', 'Ñ'=>'N', 'Ń'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
+      'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a',
+      'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i',
+      'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ń'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
+      'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ƒ'=>'f',
+      'ă'=>'a', 'î'=>'i', 'â'=>'a', 'ș'=>'s', 'ț'=>'t', 'Ă'=>'A', 'Î'=>'I', 'Â'=>'A', 'Ș'=>'S', 'Ț'=>'T',
+    );
+    
 	public function getProcessaDocumentosMatricula(){
 
 		$user = $this->SetUser();
@@ -105,7 +116,16 @@ class ProcessaDocumentosMatriculaController extends BaseController
 
 			$documentos_matricula = new DocumentoMatricula();
 
-			dd($documentos_matricula->retorna_documentos_matricula_enviados($id_user, $id_inscricao_pos));
+			$arquivos_matricula_recebidos = $documentos_matricula->retorna_documentos_matricula_enviados($id_user, $id_inscricao_pos);
+
+			$argumento_pdftk = "";
+			foreach ($arquivos_matricula_recebidos as $key) {
+				$argumento_pdftk .= storage_path('app/').$key->nome_arquivo." ";
+			}
+
+			dd('pdftk '.$argumento_pdftk.' cat output '.storage_path('app/')."arquivos_internos/".str_replace(' ', '-', strtr($nome, $this->normalizeChars)).".pdf");
+
+			$process = new Process('pdftk '.$argumento_pdftk.' cat output '.storage_path('app/')."arquivos_internos/".str_replace(' ', '-', strtr($nome, $this->normalizeChars)).".pdf");
 
 			$dados_para_template['id_candidato'] = $id_user;
 
