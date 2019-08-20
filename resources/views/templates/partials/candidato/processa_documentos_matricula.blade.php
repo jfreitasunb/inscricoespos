23157653b4e8b612d179b3500db8cc58906b242d
José <Freitas>
2019-08-20 15:22:12 -0300

    Dados para confirmação.

--- resources/views/templates/partials/candidato/processa_documentos_matricula.blade.php
+++ resources/views/templates/partials/candidato/processa_documentos_matricula.blade.php
@@ -5,27 +5,30 @@
 @endsection
 
 @section('processa_documentos_matricula')
-{!! Form::open(array('route' => 'finalizar.inscricao', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!}
+{!! Form::open(array('route' => 'documento.final.matricula', 'class' => 'form-horizontal', 'data-parsley-validate' => '' )) !!}
       
   <fieldset class="scheduler-border">
-    <legend class="scheduler-border">{{trans('tela_finalizar_inscricao.tela_finaliza')}}</legend>
+    <legend class="scheduler-border">{{trans('tela_finalizar_documentos_matricula.tela_finaliza')}}</legend>
       <div class="row">
-        {{ trans('tela_finalizar_inscricao.texto_finaliza') }}
+        {{ trans('tela_finalizar_documentos_matricula.texto_finaliza') }}
       </div>
   </fieldset>
 
   <fieldset class="scheduler-border">
-    <legend class="scheduler-border">{{trans('tela_finalizar_inscricao.ficha_inscricao')}}</legend>
+    <legend class="scheduler-border">{{trans('tela_finalizar_documentos_matricula.ficha_inscricao')}}</legend>
       <div class="row">
-        <a href="{{ asset($ficha_inscricao) }}" target="_blank"><i class="fa fa-file-pdf-o fa-4x"></i>{{ $nome_candidato }}</a>
+        <a href="{{ asset($dados_para_template['ficha_inscricao']) }}" target="_blank"><i class="fa fa-file-pdf-o fa-4x"></i>{{ $dados_para_template['nome_candidato'] }}</a>
           {{-- {{ link_to($ficha_inscricao,''.$nome_candidato, array('target' => '_blank' ) ) }} --}}
       </div>
   </fieldset>
-  {!! Form::hidden('ficha_inscricao', $ficha_inscricao, []) !!}
+  {!! Form::hidden('id_candidato', $dados_para_template['id_candidato'], []) !!}
+  {!! Form::hidden('id_inscricao_pos', $dados_para_template['id_inscricao_pos'], []) !!}
+  {!! Form::hidden('id_programa_pretendido', $dados_para_template['id_programa_pretendido'], []) !!}
+  {!! Form::hidden('ficha_inscricao', $dados_para_template['ficha_inscricao'], []) !!}
       <div class="form-group">
         <div class="row">
           <div class="col-md-6 col-md-offset-3 text-center">
-            {!! Form::submit(trans('tela_finalizar_inscricao.menu_envio_definitivo'), ['class' => 'btn btn-danger btn-lg register-submit']) !!}
+            {!! Form::submit(trans('tela_finalizar_documentos_matricula.menu_envio_definitivo'), ['class' => 'btn btn-danger btn-lg register-submit']) !!}
           </div>
         </div>
       </div>
