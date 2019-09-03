7d1690bb3e78922aec7616c67415550e3f980d30
jfreitasmat <jfreitas@mat.unb.br>
2019-09-02 21:18:35 -0300

    Template inicial.

---
+++ resources/views/templates/partials/coordenador/configurar_periodo_envio_documentos_matricula.blade.php
@@ -0,0 +1,74 @@
+@extends('templates.default')
+
+@section('stylesheets')
+  {!! Html::style( asset('css/parsley.css') ) !!}
+  {!! Html::style( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') ) !!}
+  {!! Html::style( asset('bower_components/moment/locale/fr.js') ) !!}
+@endsection
+
+@section('configurar_periodo_envio_documentos_matricula')
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      {!! Form::open(array('route' => 'configura.periodo.confirmacao','data-parsley-validate' => '' ,'enctype' => 'multipart/form-data')) !!}
+        <legend>Configurar o(s) período(s) de confirmação para o edital n<sup>o</sup> <strong>{{ $edital }}</strong></legend>
+        <div class="col-xs-6">
+          <div class="form-group form-inline">
+            {!! Form::label('mes_inicio_1', 'Mês de início no programa:') !!}
+            <div class='input-group' id='mes_inicio_1'>
+              {!! Form::text('mes_inicio_1', null, ['class' => 'form-control', 'required' => '']) !!}
+              <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-6">
+          <div class="form-group form-inline">
+            {!! Form::label('prazo_confirmacao_mes_1', 'Prazo para confirmação de interesse:') !!}
+            <div class='input-group' id='prazo_confirmacao_mes_1'>
+              {!! Form::text('prazo_confirmacao_mes_1', null, ['class' => 'form-control', 'required' => '']) !!}
+              <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-6">
+          <div class="form-group form-inline">
+            {!! Form::label('mes_inicio_2', 'Mês de início no programa:') !!}
+            <div class='input-group' id='mes_inicio_2'>
+              {!! Form::text('mes_inicio_2', null, ['class' => 'form-control']) !!}
+              <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-6">
+          <div class="form-group form-inline">
+            {!! Form::label('prazo_confirmacao_mes_2', 'Prazo para confirmação de interesse:') !!}
+            <div class='input-group' id='prazo_confirmacao_mes_2'>
+              {!! Form::text('prazo_confirmacao_mes_2', null, ['class' => 'form-control']) !!}
+              <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-10 text-center">
+          {!! Form::hidden('id_inscricao_pos', $edital_vigente->id_inscricao_pos, []) !!}
+          {!! Form::submit('Salvar', array('class' => 'register-submit btn btn-primary btn-lg', 'id' => 'register-submit', 'tabindex' => '4')) !!}
+        </div>
+    </div>
+    {!! Form::close() !!}
+  </div>
+@endsection
+
+@section('scripts')
+  {!! Html::script( asset('bower_components/moment/min/moment.min.js') ) !!}
+  {!! Html::script( asset('bower_components/moment/locale/pt-br.js') ) !!}
+  {!! Html::script( asset('bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') ) !!}
+  {!! Html::script( asset('bower_components/moment/locale/fr.js') ) !!}
+  {!! Html::script( asset('js/datepicker.js') ) !!}
+  {!! Html::script( asset('js/parsley.min.js') ) !!}
+@endsection
