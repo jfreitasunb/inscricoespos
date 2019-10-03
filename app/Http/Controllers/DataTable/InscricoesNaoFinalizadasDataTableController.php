a0237978efbe39d038e8e984914c5737771b3128
José <Freitas>
2019-10-03 18:28:30 -0300

    Dependências e novas rotinas para recuperar os dados existentes.

--- app/Http/Controllers/DataTable/InscricoesNaoFinalizadasDataTableController.php
+++ app/Http/Controllers/DataTable/InscricoesNaoFinalizadasDataTableController.php
@@ -10,8 +10,10 @@ use InscricoesPos\Models\User;
 use InscricoesPos\Models\ConfiguraInscricaoPos;
 use InscricoesPos\Models\EscolhaCandidato;
 use InscricoesPos\Models\ProgramaPos;
+use InscricoesPos\Models\ContatoRecomendante;
+use InscricoesPos\Models\Documento;
 
-
+use Storage;
 use DB;
 
 class InscricoesNaoFinalizadasDataTableController extends DataTableController
@@ -43,7 +45,7 @@ class InscricoesNaoFinalizadasDataTableController extends DataTableController
             'email' => 'Email',
             'programa_pretendido' => 'Programa Pretendido',
             'created_at' => 'Criação',
-            'updated_at' => 'Última atualização'
+            'updated_at' => 'Última atualização',
         ];
     }
 
@@ -78,16 +80,52 @@ class InscricoesNaoFinalizadasDataTableController extends DataTableController
 
         foreach ($dados_temporarios as $dados) {
             
+            $id_candidato = $dados->id_candidato;
+
             $escolha = new EscolhaCandidato();
-            if (!is_object($escolha->retorna_escolha_candidato($dados->id_candidato, $id_inscricao_pos))) {
+            
+            if (!is_object($escolha->retorna_escolha_candidato($id_candidato, $id_inscricao_pos))) {
                 $programa_pretendido = null;
             }else{
-                $id_programa_pretendido = $escolha->retorna_escolha_candidato($dados->id_candidato, $id_inscricao_pos)->programa_pretendido;
+                $id_programa_pretendido = $escolha->retorna_escolha_candidato($id_candidato, $id_inscricao_pos)->programa_pretendido;
                 $programa_pretendido = (ProgramaPos::find($id_programa_pretendido))->tipo_programa_pos_ptbr;
+            }
+
+            $contatos = new ContatoRecomendante();
+            
+            $situacao_recomendante = $contatos->retorna_recomendante_candidato($id_candidato,$id_inscricao_pos);
+            if (!is_null($situacao_recomendante)) {
+                $i=1;
+                foreach ($situacao_recomendante as $situacao) {
+                    $recomendante[$i] = $situacao->email_enviado;
+                    $i++;
+                }
+            }else{
+                $recomendante[1] = null;
+                $recomendante[2] = null;
+                $recomendante[3] = null;
+            }
+
+            $documentos_enviados = new Documento();
+
+            if ($documentos_enviados->retorna_existencia_documentos($id_candidato, $id_inscricao_pos)){
+                
+                $documentos = url('/').Storage::url($documentos_enviados->retorna_documento($id_candidato,$id_inscricao_pos)['nome_arquivo']);
+
+                $comprovante = url('/').Storage::url($documentos_enviados->retorna_comprovante_proficiencia($id_candidato,$id_inscricao_pos)['nome_arquivo']);
+
+                $historico = url('/').Storage::url($documentos_enviados->retorna_historico($id_candidato,$id_inscricao_pos)['nome_arquivo']);
+
+                $projeto = url('/').Storage::url($documentos_enviados->retorna_projeto($id_candidato,$id_inscricao_pos)['nome_arquivo']);
+            }else{
+                $documentos = null;
+                $comprovante = null;
+                $historico = null;
+                $projeto = null;
             }
             
 
-            $dados_vue[] = ['id_candidato' => $dados->id_candidato, 'nome' => (User::find($dados->id_candidato))->nome, 'email' => (User::find($dados->id_candidato))->email, 'programa_pretendido' => $programa_pretendido, 'created_at' => $dados->created_at->format('d/m/Y'). " ".$dados->created_at->format('H:m'), 'updated_at' => $dados->updated_at->format('d/m/Y'). " ".$dados->updated_at->format('H:m')];
+            $dados_vue[] = ['id_candidato' => $id_candidato, 'nome' => (User::find($dados->id_candidato))->nome, 'email' => (User::find($dados->id_candidato))->email, 'programa_pretendido' => $programa_pretendido, 'created_at' => $dados->created_at->format('d/m/Y'). " ".$dados->created_at->format('H:m'), 'updated_at' => $dados->updated_at->format('d/m/Y'). " ".$dados->updated_at->format('H:m'), 'recomendante1' => $recomendante[1], 'recomendante2' => $recomendante[2], 'recomendante3' => $recomendante[3], 'documentos' => $documentos, 'comprovante' => $comprovante, 'historico' => $historico, 'projeto' => $projeto];
         }
         
         return $dados_vue;
