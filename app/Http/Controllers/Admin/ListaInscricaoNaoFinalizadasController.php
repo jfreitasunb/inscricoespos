147b1e032df0e3d1a5658046fdb1c490428ba0f5
José Antônio <jfreitas@mat.unb.br>
2019-10-02 22:09:47 -0300

    Código não utilizado mais.

--- app/Http/Controllers/Admin/ListaInscricaoNaoFinalizadasController.php
+++ app/Http/Controllers/Admin/ListaInscricaoNaoFinalizadasController.php
@@ -28,14 +28,14 @@ class ListaInscricaoNaoFinalizadasController extends AdminController
 
 	public function getInscricoesNaoFinalizadas()
 	{
-		$relatorio = new ConfiguraInscricaoPos();
+		// $relatorio = new ConfiguraInscricaoPos();
 
-      	$relatorio_disponivel = $relatorio->retorna_edital_vigente();
+  //     	$relatorio_disponivel = $relatorio->retorna_edital_vigente();
 
-		$tipo_programa_pos = new FinalizaInscricao;
+		// $tipo_programa_pos = new FinalizaInscricao;
 
-		$inscricoes_nao_finalizadas = $tipo_programa_pos->usuarios_nao_finalizados($relatorio_disponivel->id_inscricao_pos)->paginate(10);
+		// $inscricoes_nao_finalizadas = $tipo_programa_pos->usuarios_nao_finalizados($relatorio_disponivel->id_inscricao_pos)->paginate(10);
 
-		return view('templates.partials.admin.nao_finalizadas')->with(compact('inscricoes_nao_finalizadas'));
+		return view('templates.partials.admin.nao_finalizadas');
 	}
 }
