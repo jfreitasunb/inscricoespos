8a9dd04715119e6d454b4755bd774e9a347dfa13
José Antônio <jfreitas@mat.unb.br>
2019-10-02 22:10:18 -0300

    Registrando novo componente.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -30,8 +30,9 @@ const HomologaInscricoes = require('./components/HomologaInscricoes.vue');
 const SelecionaCandidatos = require('./components/SelecionaCandidatos.vue');
 const MudaRecomendante = require('./components/MudaRecomendante.vue');
 const ContaCartasRecomendantes = require('./components/ContaCartasRecomendantes.vue');
+const InscricoesNaoFinalizadas = require('./components/InscricoesNaoFinalizadas.vue');
 
 const app = new Vue({
     el: '#app',
-    components: { CoordenadorDocumentosMatricula, DataTableUser, AuxiliaSelecao, HomologaInscricoes, SelecionaCandidatos, MudaRecomendante, ContaCartasRecomendantes }
+    components: { CoordenadorDocumentosMatricula, DataTableUser, AuxiliaSelecao, HomologaInscricoes, SelecionaCandidatos, MudaRecomendante, ContaCartasRecomendantes, InscricoesNaoFinalizadas }
 });
