675048c2b1b4fec6cfefa247cc6236edc96bedc0
José Antônio <jfreitas@mat.unb.br>
2019-10-02 21:42:34 -0300

    Passando dados para view.

--- app/Http/Controllers/Admin/ListaTodasIndicacoesController.php
+++ app/Http/Controllers/Admin/ListaTodasIndicacoesController.php
@@ -32,12 +32,14 @@ class ListaTodasIndicacoesController extends AdminController
 
       	$relatorio_disponivel = $relatorio->retorna_edital_vigente();
 
+      	$id_inscricao_pos = $relatorio_disponivel->id_inscricao_pos;
+
       	$edital_vigente = explode("-",$relatorio_disponivel->edital)[1]."/".explode("-",$relatorio_disponivel->edital)[0];
 
 
 		$finalizacoes = new FinalizaInscricao;
 
-		$inscricoes_finalizadas = $finalizacoes->retorna_usuarios_lista_indicacoes($relatorio_disponivel->id_inscricao_pos, $this->locale_default)->paginate(10);
+		$inscricoes_finalizadas = $finalizacoes->retorna_usuarios_lista_indicacoes($id_inscricao_pos, $this->locale_default)->paginate(10);
 
 		$dados_para_template = [];
 
@@ -45,7 +47,7 @@ class ListaTodasIndicacoesController extends AdminController
 
 			$recomendante_candidato = new ContatoRecomendante();
 
-			$recomendantes_candidato = $recomendante_candidato->retorna_recomendante_candidato($inscricao->id_candidato,$inscricao->id_inscricao_pos);
+			$recomendantes_candidato = $recomendante_candidato->retorna_recomendante_candidato($inscricao->id_candidato,$id_inscricao_pos);
 
 			$dados_para_template[$inscricao->id_candidato]['nome_candidato'] = $inscricao->nome;
 			$dados_para_template[$inscricao->id_candidato]['email'] = $inscricao->email;
@@ -61,7 +63,7 @@ class ListaTodasIndicacoesController extends AdminController
 
 				$carta_recomendacao = new CartaRecomendacao();
 				
-				$carta_aluno = $carta_recomendacao->retorna_carta_recomendacao($recomendante->id_recomendante,$inscricao->id_candidato,$inscricao->id_inscricao_pos);
+				$carta_aluno = $carta_recomendacao->retorna_carta_recomendacao($recomendante->id_recomendante,$inscricao->id_candidato,$id_inscricao_pos);
 
 				$dados_para_template[$inscricao->id_candidato]['status_carta_'.$i] = $carta_aluno->completada;
 
@@ -70,10 +72,10 @@ class ListaTodasIndicacoesController extends AdminController
 		}
 
 		$totaliza = new CartaRecomendacao();
+		
+		$total_cartas_solicitas = $finalizacoes->retorna_total_inscricoes_finalizadas($id_inscricao_pos)*3;
 
-		$total_cartas_recebidas = $totaliza->conta_total_cartas_por_edital_situacao($relatorio_disponivel->id_inscricao_pos, true);
-
-		$total_cartas_solicitas = $totaliza->conta_total_cartas_por_edital_situacao($relatorio_disponivel->id_inscricao_pos, false);
+		$total_cartas_recebidas = $totaliza->conta_total_cartas_por_edital_situacao($id_inscricao_pos, true);
 		
 		return view('templates.partials.admin.tabela_indicacoes')->with(compact('dados_para_template', 'inscricoes_finalizadas', 'edital_vigente', 'total_cartas_solicitas', 'total_cartas_recebidas'));
 	}
