55057a55c21a7080b77f1026f2304d012c784b18
José Antônio <jfreitas@mat.unb.br>
2019-11-19 10:34:18 -0300

    Remoção de entradas não utilizadas.

--- resources/lang/pt-br/tela_finalizar_documentos_matricula.php
+++ resources/lang/pt-br/tela_finalizar_documentos_matricula.php
@@ -10,6 +10,4 @@ return [
     'texto_finaliza' => 'No link abaixo você encontrará um arquivo PDF contendo todos os documentos necessários para efetivação da sua matrícula. Confira todos os dados e caso encontre algum erro retorne à tela correspondente e efetue a correção.',
     'ficha_inscricao' => 'Documentos para Matrícula',
     'menu_envio_definitivo' => 'Envio Definitivo',
-    'menu_registro_sucesso_parte_inicial' => 'Conta criada com sucesso. Foi enviado para o e-mail: ',
-    'menu_registro_sucesso_parte_final' => 'um link de ativação da sua conta. Somente após ativação você conseguirá fazer login no sistema.',
 ];
