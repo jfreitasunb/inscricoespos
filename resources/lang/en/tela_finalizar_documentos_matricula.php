a88e6b9892c4b7b205b5c33a161b526bbad4400d
José Antônio <jfreitas@mat.unb.br>
2019-11-28 15:05:31 -0300

    Tradução para o inglês da tela de finalização de envio dos documentos necessários para a matrícula.

--- resources/lang/en/tela_finalizar_documentos_matricula.php
+++ resources/lang/en/tela_finalizar_documentos_matricula.php
@@ -6,8 +6,8 @@
 
 
 return [
-	'tela_finaliza' => 'Terminar de enviar los Documentos de Registro',
-    'texto_finaliza' => 'En el link de abajo usted encontrará un archivo PDF con los datos de su inscripción. Compruebe que todos los documentos estén correctos y en el caso de encontrar algún error regrese a la página correspondiente y haga la corrección.',
-    'ficha_inscricao' => 'Documentos de Registro',
-    'menu_envio_definitivo' => 'Envío Final',
+	'tela_finaliza' => 'Finalize Submission of Registration Documents',
+    'texto_finaliza' => 'On the link bellow you will find a PDF file with your registration data. Check all the data and if you find any error, return and make the correction.',
+    'ficha_inscricao' => 'Registration Documents',
+    'menu_envio_definitivo' => 'Definitive Sending',
 ];
