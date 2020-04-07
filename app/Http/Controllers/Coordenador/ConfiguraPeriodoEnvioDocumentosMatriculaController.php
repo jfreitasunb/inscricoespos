e01c86920b20141b3a0b7e580e389cf7bbf9a193
José Antônio <jfreitas@mat.unb.br>
2020-04-07 15:46:30 -0300

    Identação.

--- app/Http/Controllers/Coordenador/ConfiguraPeriodoEnvioDocumentosMatriculaController.php
+++ app/Http/Controllers/Coordenador/ConfiguraPeriodoEnvioDocumentosMatriculaController.php
@@ -39,46 +39,46 @@ class ConfiguraPeriodoEnvioDocumentosMatriculaController extends CoordenadorCont
 
 		$edital = new ConfiguraInscricaoPos();
 
-        $edital_vigente = $edital->retorna_edital_vigente();
+                $edital_vigente = $edital->retorna_edital_vigente();
 
-        $edital = str_pad(explode('-', $edital_vigente->edital)[1], 2, '0', STR_PAD_LEFT)."/".explode('-', $edital_vigente->edital)[0];
+                $edital = str_pad(explode('-', $edital_vigente->edital)[1], 2, '0', STR_PAD_LEFT)."/".explode('-', $edital_vigente->edital)[0];
 
 		return view('templates.partials.coordenador.configurar_periodo_envio_documentos_matricula')->with(compact('edital', 'edital_vigente'));
 	}
 
 	public function postConfiguraPeriodoMatricula(Request $request)
 	{
-        $user = Auth::user();
+                $user = Auth::user();
 
-        $id_coordenador = $user->id_user;
+                $id_coordenador = $user->id_user;
 
-		$this->validate($request, [
-			'inicio_entrega' => 'required',
-			'fim_entrega' => 'required|date_format:"d/m/Y"|after:today',
-		]);
+        	$this->validate($request, [
+        		'inicio_entrega' => 'required',
+        		'fim_entrega' => 'required|date_format:"d/m/Y"|after:today',
+        	]);
 
-        $id_inscricao_pos = (int)$request->id_inscricao_pos;
+                $id_inscricao_pos = (int)$request->id_inscricao_pos;
 
-        $inicio_entrega = Carbon::createFromFormat('d/m/Y', $request->inicio_entrega);
+                $inicio_entrega = Carbon::createFromFormat('d/m/Y', $request->inicio_entrega);
 
-        $fim_entrega = Carbon::createFromFormat('d/m/Y', $request->fim_entrega);
+                $fim_entrega = Carbon::createFromFormat('d/m/Y', $request->fim_entrega);
 
-        $edital_vigente = ConfiguraInscricaoPos::find($id_inscricao_pos);
+                $edital_vigente = ConfiguraInscricaoPos::find($id_inscricao_pos);
 
-        $configura_envio_documentos = new ConfiguraEnvioDocumentosMatricula();
+                $configura_envio_documentos = new ConfiguraEnvioDocumentosMatricula();
 
-        $configura_envio_documentos->id_inscricao_pos = $id_inscricao_pos;
+                $configura_envio_documentos->id_inscricao_pos = $id_inscricao_pos;
 
-        $configura_envio_documentos->inicio_envio_documentos = $inicio_entrega;
+                $configura_envio_documentos->inicio_envio_documentos = $inicio_entrega;
 
-        $configura_envio_documentos->fim_envio_documentos = $fim_entrega;
+                $configura_envio_documentos->fim_envio_documentos = $fim_entrega;
 
-        $configura_envio_documentos->id_coordenador = $id_coordenador;
+                $configura_envio_documentos->id_coordenador = $id_coordenador;
 
-        $configura_envio_documentos->save();
+                $configura_envio_documentos->save();
 
-        notify()->flash('Período de confirmação configurado com sucesso!','success', ['timer' => 3000,]);
+                notify()->flash('Período de confirmação configurado com sucesso!','success', ['timer' => 3000,]);
 
-        return redirect()->route('home');
+                return redirect()->route('home');
 	}
 }
