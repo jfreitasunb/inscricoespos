31b54cbfb990f21e41150848d77dcf9ab06beb4d
José Antônio Freitas <jfreitas@unb.br>
2021-03-19 17:10:38 -0300

    Definindo cores personalizadas.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        colors: {
+          'bg-azul-MAT':'#009FE5',
+        }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
