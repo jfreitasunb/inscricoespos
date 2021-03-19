2756d3448eea148d5b41a5fef7fa1915da214c6d
José Antônio Freitas <jfreitas@unb.br>
2021-03-19 19:31:11 -0300

    Ajustando a página inicial.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,16 +12,20 @@
     <body>
         
         {{-- header --}}
-        <div class="bg-azul-MAT p-4">
+        <div class="bg-azul-MAT p-4 flex mb-4 lg:justify-center md:justify-between">
             {{-- left side --}}
-            <div class="flex justify-between items-center">
-                <img src="{{ asset('imagens/logo/logo_unb.png') }}" width="120" alt="Logo" class="mr-2">
-                <p class="text-indigo-50">Departamento de Matemática</p>
-                <p>Inscrições para o Mestrado e Doutorado</p>
-                <p>01/01/2021 à 31/12/2021</p>
+            <div class="lg:w-1/3 md:w-1/2">
+                <img src="{{ asset('imagens/logo/logo_unb.png') }}" width="140" alt="Logo">
             </div>
 
             {{-- right side --}}
+
+            <div class="lg:w-2/3 lg:self-center md:w-1/2 ml-2 lg:mr-96">
+                <h1 class="lg:text-6xl md:text-4xl text-white md:text-center lg:mb-4">Departamento de Matemática</h1>
+                <h2 class="lg:text-4xl md:text-2xl text-white md:text-center lg:mb-4">Inscrições para o Mestrado e Doutorado</h2>
+                <h3 class="lg:text-2xl md:text-2xl text-white md:text-center lg:mb-4">01/01/2021 à 31/12/2021</h3>
+            </div>
+            <div></div>
 
         </div>
         {{-- main area --}}
