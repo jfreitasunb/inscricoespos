513880ebef4c7fa43a86262d41c38d228af3ada8
José Antônio Freitas <jfreitas@unb.br>
2021-03-22 13:28:03 -0300

    Rota para a página principal.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('layouts.app');
+});
+
 Route::get('/candidato', function () {
     return view('candidato.index');
 });
