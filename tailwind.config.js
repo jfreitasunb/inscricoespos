55c067dc2222a662f8b68ab55dc241305f797f24
José Antônio Freitas <jfreitas@unb.br>
2021-03-22 18:31:20 -0300

    Coisas.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  purge: [],
+  purge: [
+     './resources/**/*.blade.php',
+     './resources/**/*.js',
+     './resources/**/*.vue',
+     './public/css/*.css',
+   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-        colors: {
+      colors: {
           'bg-azul-MAT':'#009FE5',
           'bg-verde-MAT':'#449D44',
         }
