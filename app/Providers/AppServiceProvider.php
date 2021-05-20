e865c1e0219a12f45f48f978e91360005501090a
José Antônio Freitas <jfreitas@unb.br>
2021-05-20 17:40:37 -0300

    Passando dados para a view.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\View;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $periodo_inscricao = "26/05/2021 à 13/06/2021";
+
+        $texto_inscricao_pos = "dois_programas";
+
+        View::share ( 'periodo_inscricao', $periodo_inscricao );
+
+        View::share ( 'texto_inscricao_pos', $texto_inscricao_pos );
     }
 }
