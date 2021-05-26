7ebb38da4c0a0cb4e7ddb4947b4fde55bf75e025
José Antônio Freitas <jfreitas@unb.br>
2021-05-26 14:34:58 -0300

    Rotas para confirmar o e-mail.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegistrarController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\HomeController;
+
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,22 +20,43 @@ use App\Http\Controllers\Auth\LoginController;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.app');
-});
+
+Route::get('/ptbr', [HomeController::class, 'getLangPortugues'])->middleware('idioma.site')->name('lang.portugues');
+
+Route::get('/en', [HomeController::class, 'getLangIngles'])->middleware('idioma.site')->name('lang.ingles');
+
+Route::get('/es', [HomeController::class, 'getLangEspanhol'])->middleware('idioma.site')->name('lang.espanhol');
+
+Route::get('/', [HomeController::class, 'index'])->middleware('idioma.site')->name('home');
 
 Route::get('/candidato', function () {
     return view('candidato.index');
-});
+})->middleware('verified');
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
 
 
-Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+                ->middleware(['auth', 'throttle:6,1'])
+                ->name('verification.send');
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
 
 Route::post('/login', [LoginController::class, 'logar']);
 
-Route::get('/register', [RegistrarController::class, 'index'])->name('auth.registrar');
+Route::get('/register', [RegistrarController::class, 'index'])->name('registrar');
 
-Route::post('/register', [RegistrarController::class, 'logar']);
+Route::post('/register', [RegistrarController::class, 'registrar']);
 
 Route::get('/forgot-password', [RecuperaSenhaController::class, 'create'])->middleware('guest')->name('password.request');
 
