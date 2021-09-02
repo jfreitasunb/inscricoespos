fa3060a66a09bd8816298ec07abb3125acaacca6
José Antônio <jfreitas@unb.br>
2021-09-02 15:37:02 -0300

    Layouts não utilizados mais.

--- resources/views/layouts/idiomas.blade.php
+++
@@ -1,5 +0,0 @@
-<div id="main" class="flex items-stretch justify-around ml-2 lg:justify-center lg:space-x-14 w-3/3">
-    <a href="{{ route('lang.portugues') }}" class="inline-block px-2 py-2 text-center text-white rounded-full bg-azul-MAT w-28 hover:bg-blue-700">Português</a>
-    <a href="{{ route('lang.ingles') }}" class="inline-block px-6 py-2 text-center text-white rounded-full bg-azul-MAT w-28 hover:bg-blue-700">English</a>
-    <a href="{{ route('lang.espanhol') }}" class="inline-block px-6 py-2 text-center text-white rounded-full bg-azul-MAT w-28 hover:bg-blue-700">Español</a>
-</div>
