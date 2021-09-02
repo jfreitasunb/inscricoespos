962ae51b1427b5b387e9b7d5cc533b131762c9e8
José Antônio <jfreitas@unb.br>
2021-09-02 15:31:03 -0300

    Traduzindo os botões.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,14 +11,14 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="email" :value="__('tela_login.menu_email')" />
 
                 <x-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" :value="__('tela_login.menu_senha')" />
 
                 <x-input id="password" class="block w-full mt-1"
                                 type="password"
@@ -30,14 +30,14 @@
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('tela_login.menu_rememberme') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="text-sm text-gray-600 underline hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('tela_login.menu_esqueceu_senha') }}
                     </a>
                 @endif
 
