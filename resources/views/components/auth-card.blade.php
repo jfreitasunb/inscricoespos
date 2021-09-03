67f98817aa132ba928a77930fe4ba872685bf952
José Antônio <jfreitas@unb.br>
2021-09-03 18:47:14 -0300

    Coisas.

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col items-center pt-2 mb-72 bg-gray-300 mt-28 sm:justify-center sm:pt-0">
+<div class="flex flex-col items-center pt-2 bg-gray-300 mb-72 mt-28 sm:justify-center sm:pt-0">
     <div class="w-full px-6 py-2 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
         {{ $slot }}
     </div>
