02d5a6621aab6371b26d887dbe27f18ad446ab6d
José Antônio <jfreitas@unb.br>
2021-09-03 18:43:10 -0300

    Alguns ajustes no layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,17 @@
     </head>
 
     <body>
-        <div class="flex flex-col justify-between h-screen bg-gray-300">
+        <div class="flex flex-col justify-between h-auto bg-gray-300">
             <x-header :message="$periodo_inscricao" />
-            <x-idiomas :idioma="$idioma" />
+            @guest
+                <div class="h-30">
+                <x-idiomas :idioma="$idioma" />
+                </div>
+            @endguest
             @if (Route::currentRouteName() == "home")
+                <div class="mt-40 mb-72">
                 <x-tela-login-registrar />
+                </div>
             @endif
             @if (Route::currentRouteName() !== "home")
                 <div class="font-sans antialiased text-gray-900">
@@ -24,5 +30,21 @@
 
             <x-footer />
         </div>
+     <script>
+      /* Optional Javascript to close the radio button version by clicking it again */
+      var myRadios = document.getElementsByName('tabs2');
+      var setCheck;
+      var x = 0;
+      for(x = 0; x < myRadios.length; x++){
+          myRadios[x].onclick = function(){
+              if(setCheck != this){
+                   setCheck = this;
+              }else{
+                  this.checked = false;
+                  setCheck = null;
+          }
+          };
+      }
+   </script>
     </body>
 </html>
