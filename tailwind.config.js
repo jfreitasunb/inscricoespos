cc452d1d3da8a4d5879dd9b58e6da3cfae83a909
José Antônio <jfreitas@unb.br>
2021-09-03 14:53:18 -0300

    Dependências para o sistema de autenticação.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: [
-     './resources/**/*.blade.php',
-     './resources/**/*.js',
-     './resources/**/*.vue',
-     './public/css/*.css',
-   ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-          'bg-azul-MAT':'#009FE5',
-          'bg-verde-MAT':'#449D44',
-        }
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
