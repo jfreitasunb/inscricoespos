a4af4b495b05cbb250f461392214f8ee01b35a36
José Antônio <jfreitas@unb.br>
2021-09-08 10:31:46 -0300

    Movido para a página principal

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,23 +1,4 @@
 <x-app-layout>
 <x-menu-sanfona />
 <x-area-conteudo-admin-coordenador />
-@once
-    @push('scripts')
-        <script>
-            var myRadios = document.getElementsByName('tabs2');
-      var setCheck;
-      var x = 0;
-      for(x = 0; x < myRadios.length; x++){
-          myRadios[x].onclick = function(){
-              if(setCheck != this){
-                   setCheck = this;
-              }else{
-                  this.checked = false;
-                  setCheck = null;
-          }
-          };
-      }
-        </script>
-    @endpush
-@endonce
 </x-app-layout>
