0634d9dda970dda4ceca4b2795727feea73a1a91
José Antônio <jfreitas@unb.br>
2021-09-08 09:04:04 -0300

    Nome dos principais menus.

--- resources/views/components/menu-admin.blade.php
+++ resources/views/components/menu-admin.blade.php
@@ -5,4 +5,9 @@
     <x-input-sanfona id="tab-single-one" type="radio" name="tabs2"/>
     <x-label-sanfona for="tab-single-one" icone="description" value="Dados da Pós Graduação" />
     <x-label-sanfona for="tab-single-one" icone="settings_applications" value="Configurar Edital" />
+    <x-label-sanfona for="tab-single-one" icone="settings_applications" value="Acompanhar Inscrições" />
+    <x-label-sanfona for="tab-single-one" icone="settings_applications" value="Gerar Relatório de Inscritos" />
+    <x-label-sanfona for="tab-single-one" icone="settings_applications" value="Processo de Seleção" />
+    <x-label-sanfona for="tab-single-one" icone="settings_applications" value="Acompanha Selecionados" />
+    <x-label-sanfona for="tab-single-one" icone="settings_applications" value="Estatísticas" />
 </x-entrada-sanfona>
