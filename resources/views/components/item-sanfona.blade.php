4c3c2e322d850a06f1b502d0cccf06dde91f1547
José Antônio <jfreitas@unb.br>
2021-09-08 09:02:03 -0300

    Transformando em componente.

---
+++ resources/views/components/item-sanfona.blade.php
@@ -0,0 +1,5 @@
+<div class="overflow-hidden leading-normal bg-gray-100 border-l-2 border-indigo-500 tab-content">
+    <p class="p-5">
+        <span class="material-icons">{{ $icone }}</span><a href="{{ $href }}">{{ $value }}</a>
+    </p>
+</div>
--- resources/views/components/menu-admin.blade.php
+++ resources/views/components/menu-admin.blade.php
@@ -1,16 +1,8 @@
 <x-entrada-sanfona>
     <x-input-sanfona id="tab-single-one" type="radio" name="tabs2"/>
-    <x-label-sanfona for="tab-single-one" :value="__('tela_registro.menu_nome')" />
-    <div class="overflow-hidden leading-normal bg-gray-100 border-l-2 border-indigo-500 tab-content">
-        <p class="p-5">
-            <span class="glyphicon glyphicon-cog fa-fw"></span><a href="#">Lista/Edita Usuários</a>
-        </p>
-    </div>
+    <x-label-sanfona for="tab-single-one" icone="settings_suggest" value="Sistema" />
+    <x-item-sanfona icone="" href="#" value="teste"/>
     <x-input-sanfona id="tab-single-one" type="radio" name="tabs2"/>
-    <x-label-sanfona for="tab-single-one" :value="__('tela_registro.menu_nome')" />
-    <div class="overflow-hidden leading-normal bg-gray-100 border-l-2 border-indigo-500 tab-content">
-        <p class="p-5">
-            <span class="glyphicon glyphicon-cog fa-fw"></span><a href="#">Lista/Edita Usuários</a>
-        </p>
-    </div>
+    <x-label-sanfona for="tab-single-one" icone="description" value="Dados da Pós Graduação" />
+    <x-label-sanfona for="tab-single-one" icone="settings_applications" value="Configurar Edital" />
 </x-entrada-sanfona>
