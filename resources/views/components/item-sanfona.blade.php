7470ba5c0faad4c04bad1e894df2dd84320cc849
José Antônio <jfreitas@unb.br>
2021-09-08 14:16:59 -0300

    Nada importante.

--- resources/views/components/item-sanfona.blade.php
+++ resources/views/components/item-sanfona.blade.php
@@ -1,4 +1,4 @@
-<div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+<div class="overflow-hidden leading-normal bg-gray-100 border-l-2 border-indigo-500 tab-content">
     <p class="p-5">
         <span class="material-icons">{{ $icone }}</span><a href="{{ $href }}">{{ $value }}</a>
     </p>
