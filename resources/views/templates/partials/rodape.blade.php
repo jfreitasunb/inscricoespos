6a1a150893b05654eeb87f9cd614b8a9543ee651
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-02-03 16:14:48 -0300

    Não sei.

--- resources/views/templates/partials/rodape.blade.php
+++
@@ -1,6 +0,0 @@
-<footer id="footer" class="sticky top-[80vh] lg:mt-52">
-	<hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700">
-	<div class="relative flex justify-center">
-		<p class="pl-4 text-center">Pós-Graduação MAT/UnB - {{ date("Y") }}  - <a class="text-blue-500 hover:underline" href="mailto:posgrad@mat.unb.br">{{ trans('mensagens_gerais.duvidas_pos') }}</a> - <a class="text-blue-500 hover:underline" href="mailto:informatica@mat.unb.br">{{ trans('mensagens_gerais.problemas') }}</a></p>
-	</div>
-</footer>
