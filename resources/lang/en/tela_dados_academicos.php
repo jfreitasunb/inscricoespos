7147ebb756db55807bd14e45b409542f04f052f7
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-03-20 17:17:47 -0300

    Textos em português, inglês e espanhol.

--- resources/lang/en/tela_dados_academicos.php
+++ resources/lang/en/tela_dados_academicos.php
@@ -14,5 +14,13 @@ return [
     'ano_conclusao' => 'Year of conclusion (or forecast)',
     'curso_pos' => 'Postgraduate Studies',
     'nivel' => 'Level',
+    'disciplinas_destaque' => 'Advanced mathematics subjects',
+    'texto_disciplinas_destaque' => 'Advanced courses',
+    'nome_disciplina' => 'Course',
+    'mencao' => 'Mention',
+    'remove_destaque' => 'Delete course?',
+    'acao' => 'Add more?',
+    'adicionar_mais' => 'Add',
+    'placeholder_nome_disciplina' => 'Course',
     'menu_enviar' => 'Submit',
 ];
--- resources/lang/es/tela_dados_academicos.php
+++ resources/lang/es/tela_dados_academicos.php
@@ -14,5 +14,13 @@ return [
     'ano_conclusao' => 'Año de conclusión (o previsión)',
     'curso_pos' => 'Posgraduación',
     'nivel' => 'Nivel',
+    'disciplinas_destaque' => 'Asignaturas de matemáticas avanzadas',
+    'texto_disciplinas_destaque' => 'Disciplinas avanzadas',
+    'nome_disciplina' => 'Curso',
+    'mencao' => 'Mención',
+    'remove_destaque' => '¿Eliminar disciplina?',
+    'acao' => '¿Añadir más?',
+    'adicionar_mais' => 'Añadir',
+    'placeholder_nome_disciplina' => 'Curso',
     'menu_enviar' => 'Enviar',
 ];
