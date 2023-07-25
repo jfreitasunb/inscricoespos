1ee3499eaa48a088562e70ffc275cb7acc73035b
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-07-25 18:01:39 -0300

    Textos traduzidos em português, inglês e espanhol.

---
+++ resources/lang/es/meses.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+*Tradução dos textos para Espanhol
+ */
+
+
+return [
+    'mes_1'  => 'Enero',
+    'mes_2'  => 'Febrero',
+    'mes_3'  => 'Marzo',
+    'mes_4'  => 'Abril',
+    'mes_5'  => 'Mayo',
+    'mes_6'  => 'Junio',
+    'mes_7'  => 'Julio',
+    'mes_8'  => 'Agosto',
+    'mes_9'  => 'Septiembre',
+    'mes_10' => 'Octubre',
+    'mes_11' => 'Noviembre',
+    'mes_12' => 'Diciembre',
+];
