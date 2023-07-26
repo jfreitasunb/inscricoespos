7fa50c39079bb3cebd40eb50fa1dc7f6f7083fd4
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-07-26 19:02:23 -0300

    Ajustando a estética da página. Começando no cabeçalho.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,22 +15,27 @@
 
         @livewireStyles
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen">
-            <header>
-                <div class="bg-azul-MAT lg:flex flex-grow lg:items-center p-8 sm:p-12">
-                    <div class="flex-grow">
-                        <a href="{{URL::to('/')}}"> <img src="{{ asset('imagens/logo/logo_unb.png') }}" alt="Logo do MAT-UnB" class="sm:flex-shrink-0 mx-auto sm:mx-0 h-24" style="height:120px" /></a>
-                        <div class="xm:ml-4 sm:text-left text-center">
-                            <h1 class="text-white text-center text-2xl sm:text-5xl mb-2">Departamento de Matemática</h1>
-                            <h2 class="text-white text-center text-2xl sm:text-5xl mb-2">Inscrições para o Mestrado e Doutorado</h2>
-                            <h3 class="text-white text-center text-2xl sm:text-5xl mb-2">22/11/2022 à 22/01/2023</h3>
-                        </div>
-                    </div>
-                </div>
-            </header>
-
-            <div class="flex justify-center items-center mt-12">
+    <body class="min-h-screen flex flex-col items-center justify-center">
+        <!-- Cabeçalho -->
+     <div class="h-32 md:h-48 w-screen bg-azul-MAT md:flex-col">
+        <!-- Header com flex -->
+        <header class="flex">
+            <!-- Div com tamanho w-1/5 e imagem -->
+            <div class="p-4 flex items-start justify-left md:justify-center md:flex-col md:items-center md:text-center">
+                <a href="{{URL::to('/')}}"> <img src="{{ asset('imagens/logo/logo_unb.png') }}" alt="Logo do MAT-UnB" class="w-16 md:w-full" style="height:120px" /></a>
+            </div>
+            <!-- Div com tamanho w-5/6 e texto em três parágrafos -->
+            <div class="p-4 flex flex-col md:ml-72 md:text-center">
+                <!-- Conteúdo da segunda div -->
+                <h1 class="text-white md:text-5xl">Departamento de Matemática</h1>
+                <h2 class="text-white md:text-5xl">Inscrições para o Mestrado e Doutorado</h2>
+                <h3 class="text-white md:text-5xl">22/11/2022 à 22/01/2023</h3>
+            </div>
+        </header>
+    </div>
+    <!-- Corpo -->
+    <main class="container mx-auto px-4 flex-grow">
+        <div class="flex justify-center items-center mt-12">
                 <div class="grid grid-cols-3 gap-4">
                     <div class="mt-4">
                         <button type="button" class="bg-azul-MAT text-white hover:text-white border border-bg-azul-MAT hover:bg-sky-900 font-semibold rounded-full text-xs lg:text-xl px-6 lg:px-32 py-1 focus-outline-none">
@@ -71,12 +76,13 @@
             </div>
         </div>
 
-        <footer id="footer" class="sticky top-[80vh] lg:mt-52">
-            <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700">
-            <div class="relative flex justify-center">
-                <p class="pl-4 text-center">Pós-Graduação MAT/UnB - {{ date("Y") }}  - <a class="text-blue-500 hover:underline" href="mailto:posgrad@mat.unb.br">{{ trans('mensagens_gerais.duvidas_pos') }}</a> - <a class="text-blue-500 hover:underline" href="mailto:informatica@mat.unb.br">{{ trans('mensagens_gerais.problemas') }}</a></p>
-            </div>
-        </footer>
+    <!-- Rodapé -->
+    <footer class="py-4 text-center fixed bottom-0 w-full">
+        <hr class="my-8">
+        <p class="pl-4 text-center">Pós-Graduação MAT/UnB - {{ date("Y") }}</p>
+        <p class="pl-4 text-center"><a class="text-blue-500 hover:underline" href="mailto:posgrad@mat.unb.br">{{ trans('mensagens_gerais.duvidas_pos') }}</a></p>
+        <p class="pl-4 text-center"><a class="text-blue-500 hover:underline" href="mailto:informatica@mat.unb.br">{{ trans('mensagens_gerais.problemas') }}</a></p>
+    </footer>
         @livewireScripts
     </body>
 </html>
