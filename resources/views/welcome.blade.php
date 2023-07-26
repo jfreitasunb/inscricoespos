88bceeea221edb124f828dd4fdf747199352a190
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-07-26 16:05:10 -0300

    Cores corretas.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,8 @@
         <title>Inscrições Pós-Graduação do MAT/UnB</title>
 
         <!-- Fonts -->
-        {{-- <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" /> --}}
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -33,18 +33,17 @@
             <div class="flex justify-center items-center mt-12">
                 <div class="grid grid-cols-3 gap-4">
                     <div class="mt-4">
-
-                        <button type="button" class="bg-azul-MAT text-white hover:text-white border border-azul-MAT bg-violet-500 hover:bg-violet-600 active:bg-violet-700 font-semibold rounded-full text-xs lg:text-xl px-6 lg:px-32 py-1 focus-outline-none">
+                        <button type="button" class="bg-azul-MAT text-white hover:text-white border border-bg-azul-MAT hover:bg-sky-900 font-semibold rounded-full text-xs lg:text-xl px-6 lg:px-32 py-1 focus-outline-none">
                             Português
                         </button>
                     </div>
                     <div class="mt-4 pl-2">
-                        <button type="button" class="bg-azul-MAT text-white hover:text-white border border-azul-MAT hover:bg-sky-900 font-semibold rounded-full text-xs lg:text-xl px-6 lg:px-32 py-1 focus-outline-none">
+                        <button type="button" class="bg-azul-MAT text-white hover:text-white border border-bg-azul-MAT hover:bg-sky-900 font-semibold rounded-full text-xs lg:text-xl px-6 lg:px-32 py-1 focus-outline-none">
                             English
                         </button>
                     </div>
                     <div class="mt-4 pl-2">
-                        <button type="button" class="bg-azul-MAT text-white hover:text-white border border-azul-MAT hover:bg-sky-900 font-semibold rounded-full text-xs lg:text-xl px-6 lg:px-32 py-1 focus-outline-none">
+                        <button type="button" class="bg-azul-MAT text-white hover:text-white border border-bg-azul-MAT hover:bg-sky-900 font-semibold rounded-full text-xs lg:text-xl px-6 lg:px-32 py-1 focus-outline-none">
                             Español
                         </button>
                     </div>
@@ -53,12 +52,12 @@
             <div class="flex justify-center items-center mt-12 lg:mt-44">
                 <div class="grid grid-cols-2 gap-4">
                     <div class="mt-4 sm:pl-4">
-                        <button type="button" class="w-full bg-verde-MAT text-white border border-azul-MAT hover:bg-green-700 font-semibold rounded-md lg:text-4xl px-12 lg:px-40 py-1 focus-outline-none">
+                        <button type="button" class="w-full bg-verde-MAT text-white border border-bg-azul-MAT hover:bg-green-700 font-semibold rounded-md lg:text-4xl px-12 lg:px-40 py-1 focus-outline-none">
                             Login
                         </button>
                     </div>
                     <div class="mt-4 sm:pl-4">
-                        <button type="button" class="bg-azul-MAT text-white border border-azul-MAT hover:bg-sky-900 font-semibold rounded-md lg:text-4xl px-12 lg:px-40 py-1 focus-outline-none">
+                        <button type="button" class="bg-azul-MAT text-white border border-bg-azul-MAT hover:bg-sky-900 font-semibold rounded-md lg:text-4xl px-12 lg:px-40 py-1 focus-outline-none">
                             Registrar
                         </button>
                     </div>
@@ -73,7 +72,7 @@
         </div>
 
         <footer id="footer" class="sticky top-[80vh] lg:mt-52">
-            <hr class="my-8 h-px">
+            <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700">
             <div class="relative flex justify-center">
                 <p class="pl-4 text-center">Pós-Graduação MAT/UnB - {{ date("Y") }}  - <a class="text-blue-500 hover:underline" href="mailto:posgrad@mat.unb.br">{{ trans('mensagens_gerais.duvidas_pos') }}</a> - <a class="text-blue-500 hover:underline" href="mailto:informatica@mat.unb.br">{{ trans('mensagens_gerais.problemas') }}</a></p>
             </div>
