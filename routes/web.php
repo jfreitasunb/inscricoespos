0aa6a0d6a4c6d1708d50e1eb1cab4cb439ab1836
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-08-01 21:36:23 -0300

    Rotas para autenticação.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,8 +24,22 @@ Route::get('/en', [HomeController::class, 'getLangEnglish'])->name('lang.english
 
 Route::get('/es', [HomeController::class, 'getLangSpanish'])->name('lang.spanish')->middleware('define.locale');
 
-// Route::get('lang/{locale}', [UserController::class, 'lang']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
