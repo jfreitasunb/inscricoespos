3a1e35b102bea2bd5bd61543a9d4d9a3de2b2c8f
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-05-01 20:24:38 -0300

    Seed correto.

--- database/seeders/ProgramaPosMatTableSeeder.php
+++ database/seeders/ProgramaPosMatTableSeeder.php
@@ -1,39 +1,38 @@
 <?php
 
+namespace Database\Seeders;
+use DB;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProgramaPosMatTableSeeder extends Seeder
 {
-
     /**
-     * Auto generated seed file
-     *
-     * @return void
+     * Run the database seeds.
      */
-    public function run()
+    public function run(): void
     {
-        
-
         \DB::table('programa_pos_mat')->delete();
         
         \DB::table('programa_pos_mat')->insert(array (
             0 => 
             array (
-                'id' => 1,
+                'id_programa_pos' => 1,
                 'tipo_programa_pos_ptbr' => 'Mestrado',
                 'tipo_programa_pos_en' => 'Master\'s degree',
                 'tipo_programa_pos_es' => 'Maestría',
-                'created_at' => '2017-11-21 15:27:00',
-                'updated_at' => '2017-11-21 15:27:00',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
             ),
             1 => 
             array (
-                'id' => 2,
+                'id_programa_pos' => 2,
                 'tipo_programa_pos_ptbr' => 'Doutorado',
                 'tipo_programa_pos_en' => 'PhD degree',
                 'tipo_programa_pos_es' => 'Doctorado',
-                'created_at' => '2017-11-21 15:27:00',
-                'updated_at' => '2017-11-21 15:27:00',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
             ),
             // 2 => 
             // array (
@@ -50,6 +49,5 @@ class ProgramaPosMatTableSeeder extends Seeder
         $nextId = DB::table($tableToCheck)->select(DB::raw('nextval(\''.$tableToCheck.'_id_programa_pos_seq\')'))->first();
 
         DB::select('SELECT setval(\''.$tableToCheck.'_id_programa_pos_seq\', '.$highestId->max.')');
-        
     }
 }
