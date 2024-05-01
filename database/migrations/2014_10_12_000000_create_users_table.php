c5d97fca507076c7c77ab0b6c7b2c5aa50107135
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-05-01 17:00:12 -0300

    Mudando o nome do campo do id de usuário.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id_user');
             $table->string('nome');
             $table->string('email')->unique();
             $table->string('locale', 5)->default('pt_BR');
