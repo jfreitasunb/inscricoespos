b9844f454d53564cfc2eebc2ce2630b28ea6a707
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-05-01 16:41:29 -0300

    Model para os programas da pós.

---
+++ app/Models/ProgramaPos.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+class ProgramaPos extends FuncoesModels
+{
+    
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    
+    protected $primaryKey = 'id_programa_pos';
+
+    protected $table = 'programa_pos_mat';
+
+    protected $fillable = [
+        'tipo_programa_pos_ptbr', 
+        'tipo_programa_pos_en', 
+        'tipo_programa_pos_es', 
+    ];
+
+    public function pega_programa_pos_mat($programa, $locale){
+
+        $nome_coluna = $this->define_nome_coluna_programa_pos_mat($locale);
+
+        return $this->select($nome_coluna)
+            ->where('id_programa_pos', $programa)
+            ->value($nome_coluna);
+    }
+}
