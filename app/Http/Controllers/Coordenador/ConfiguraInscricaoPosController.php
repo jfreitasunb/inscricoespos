3e85c521dcb98f742e97e673bfa59646ce3d0e17
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-05-02 11:32:17 -0300

    Dependências e formatação de datas.

--- app/Http/Controllers/Coordenador/ConfiguraInscricaoPosController.php
+++ app/Http/Controllers/Coordenador/ConfiguraInscricaoPosController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers\Coordenador;
 
 use App\Http\Controllers\Controller;
 use App\Models\ProgramaPos;
-use InscricoesPos\Models\ConfiguraInscricaoPos;
+use App\Models\ConfiguraInscricaoPos;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 use App;
 
 use Auth;
 
 use Session;
+
+use File;
 
 class ConfiguraInscricaoPosController extends CoordenadorController
 {
@@ -37,5 +40,34 @@ class ConfiguraInscricaoPosController extends CoordenadorController
             'edital_numero' => 'required',
             'escolhas_coordenador' => 'required',
         ]);
+
+        $configura_nova_inscricao_pos = new ConfiguraInscricaoPos();
+
+        $user = Auth::user();
+
+        $local_documentos = storage_path('app/');
+
+        $arquivos_editais = storage_path("app/public/editais/");
+
+        File::isDirectory($arquivos_editais) or File::makeDirectory($arquivos_editais,0775,true);
+        
+        // dd($request->inicio_inscricao);
+        $inicio = Carbon::createFromFormat('Y-m-d', $request->inicio_inscricao);
+        $fim = Carbon::createFromFormat('Y-m-d', $request->fim_inscricao);
+        $prazo = Carbon::createFromFormat('Y-m-d', $request->prazo_carta);
+        $homologacao = Carbon::createFromFormat('Y-m-d', $request->data_homologacao);
+        $divulgacao_resultado = Carbon::createFromFormat('Y-m-d', $request->data_divulgacao_resultado);
+
+        $necessita_recomendante = $request->necessita_recomendante;
+
+        $data_inicio = $inicio->format('Y-m-d');
+        dd($data_inicio);
+        $data_fim = $fim->format('Y-m-d');
+        $semestre_inicio = $request->semestre_inicio;
+        $prazo_carta = $prazo->format('Y-m-d');
+        $data_homologacao = $homologacao->format('Y-m-d');
+        $data_divulgacao_resultado = $divulgacao_resultado->format('Y-m-d');
+
+        dd("Aqui");
     }
 }
