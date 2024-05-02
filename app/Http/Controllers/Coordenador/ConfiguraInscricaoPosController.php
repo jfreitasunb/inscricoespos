e308d89b9137018d1d42f2da25d66090423df84a
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-05-02 11:13:24 -0300

    Mais algumas validações.

--- app/Http/Controllers/Coordenador/ConfiguraInscricaoPosController.php
+++ app/Http/Controllers/Coordenador/ConfiguraInscricaoPosController.php
@@ -29,7 +29,7 @@ class ConfiguraInscricaoPosController extends CoordenadorController
         $this->validate($request, [
             'inicio_inscricao' => 'required|date_format:"Y-m-d"|before:fim_inscricao|after:today',
             'fim_inscricao' => 'required|date_format:"Y-m-d"|after:inicio_inscricao|after:today',
-            'prazo_carta' => 'required|date_format:"Y-m-d"|after:inicio_inscricao|after:today',
+            'prazo_carta' => 'required|date_format:"Y-m-d"|after:inicio_inscricao|after:fim_inscricao|after:today',
             'data_homologacao' => 'required|date_format:"Y-m-d"|after:fim_inscricao|after:today',
             'data_divulgacao_resultado' => 'required|date_format:"Y-m-d"|after:data_homologacao|after:today',
             'necessita_recomendante' => 'required',
