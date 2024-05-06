69cce30affa94ba5b0a1946abae9eb70a8707df5
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-05-06 16:02:11 -0300

    Processando os novos dados e salvando.

--- app/Http/Controllers/Admin/EditarInscricaoPosController.php
+++ app/Http/Controllers/Admin/EditarInscricaoPosController.php
@@ -27,4 +27,57 @@ class EditarInscricaoPosController extends AdminController
 
         return view('layouts.admin.editar_inscricao')->with(compact('edital_vigente'));
     }
+
+    public function postEditarInscricao(Request $request)
+    {
+
+        $this->validate($request, [
+            'inicio_inscricao' => 'required|date_format:"Y-m-d"|before:fim_inscricao',
+            'fim_inscricao' => 'required|date_format:"Y-m-d"|after:inicio_inscricao',
+            'prazo_carta' => 'required|date_format:"Y-m-d"|after:inicio_inscricao',
+            'data_homologacao' => 'required|date_format:"Y-m-d"|after:fim_inscricao',
+            'data_divulgacao_resultado' => 'required|date_format:"Y-m-d"|after:data_homologacao',
+            'edital' => 'required',
+            'programa' => 'required',
+            'necessita_recomendante' => 'required',
+        ]);
+
+        $edital_vigente = ConfiguraInscricaoPos::find((int)$request->id_inscricao_pos);
+
+        $novos_dados_edital['inicio_inscricao'] = $request->inicio_inscricao;
+        $novos_dados_edital['fim_inscricao'] = $request->fim_inscricao;
+        $novos_dados_edital['prazo_carta'] = $request->prazo_carta;
+        $novos_dados_edital['programa'] = $request->programa;
+        $novos_dados_edital['edital'] = $request->edital;
+        $novos_dados_edital['data_homologacao'] = $request->data_homologacao;
+        $novos_dados_edital['data_divulgacao_resultado'] = $request->data_divulgacao_resultado;
+
+        $temp = strtolower($request->necessita_recomendante);
+
+        switch ($temp) {
+            case 'nao':
+                $necessita_recomendante = false;
+                break;
+            case 'não':
+                $necessita_recomendante = false;
+                break;
+            
+            case 'n':
+                $necessita_recomendante = false;
+                break;
+            case '0':
+                $necessita_recomendante = false;
+                break;
+                
+            default:
+                $necessita_recomendante = true;
+                break;
+        }
+
+        $novos_dados_edital['necessita_recomendante'] = $necessita_recomendante;
+
+        $edital_vigente->update($novos_dados_edital);
+
+        return redirect()->route('editar.inscricao')->with('success', 'Inscrição configurada com sucesso.');
+    }
 }
