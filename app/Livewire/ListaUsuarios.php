986608bed4cf6bda88f0b535011129ba56b1a045
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-05-07 11:15:41 -0300

    Listando usuários no banco de dados.

--- app/Livewire/ListaUsuarios.php
+++ app/Livewire/ListaUsuarios.php
@@ -4,12 +4,34 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+use Livewire\WithPagination;
+
+use App\Models\User;
+use App\Models\ConfiguraInscricaoPos;
+use View;
+
 class ListaUsuarios extends Component
 {
+    use WithPagination;
+
+    public $perPage = 5;
+
     public function render()
     {
-        return view('livewire.lista-usuarios')
-        ->extends('layouts.app')
+        $inscricao_pos = new ConfiguraInscricaoPos();
+
+    $periodo_inscricao = $inscricao_pos->retorna_periodo_inscricao();
+
+    $texto_inscricao_pos = $inscricao_pos->define_texto_inscricao();
+
+    View::share('periodo_inscricao', $periodo_inscricao);
+
+    View::share('texto_inscricao_pos', $texto_inscricao_pos);
+
+        return view('livewire.lista-usuarios',
+            [
+                'users' => User::paginate($this->perPage)
+            ])->extends('layouts.app')
         ->section('lista_usuarios');
     }
 }
