f6fa68f28991169c2a8d411a9e443ff902792507
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-05-07 11:16:47 -0300

    Não é utilizando mais.

--- resources/views/layouts/alerta.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- @if (session()->has('succes')) -->
-<script type="text/javascript">
-        document.addEventListener('DOMContentLoaded', () => {
-            Swal.fire('Pronto', "{{ session('status_erro') }}", 'success')
-        })
-    </script>    
-<!-- @endif -->
