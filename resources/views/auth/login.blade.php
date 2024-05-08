36d8a26ad0547e882e22b6a2f043d7a279750971
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2024-05-07 21:19:40 -0300

    Ajustes na estética de algumas páginas.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,55 @@
 @extends('layouts.app')
 
 @section('inicio')
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<div class="mt-24 flex-col justify-center font-[sans-serif] text-[#333] sm:h-screen p-4">
+    <div class="max-w-md w-full mx-auto border border-gray-300 rounded-md p-6">
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('tela_login.menu_email')" />
-            <x-text-input id="email" class="block mt-1 w-1/2" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <div class="space-y-6">
+                <!-- Email Address -->
+                <div>
+                    <x-input-label for="email" :value="__('tela_login.menu_email')" />
+                    <x-text-input id="email" class="block mt-1 w-1/2" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('tela_login.menu_senha')" />
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label for="password" :value="__('tela_login.menu_senha')" />
 
-            <x-text-input id="password" class="block mt-1 w-1/2"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                    <x-text-input id="password" class="block mt-1 w-1/2"
+                                    type="password"
+                                    name="password"
+                                    required autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+            </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('tela_login.remember_me') }}</span>
-            </label>
-        </div>
+            <!-- Remember Me -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('tela_login.remember_me') }}</span>
+                </label>
+            </div>
 
-        <div class="flex items-center justify-left mt-4">
+            <div class="!mt-10">
+                <x-primary-button>
+                    {{ __('tela_login.menu_entrar') }}
+                </x-primary-button>
+            </div>
+
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('tela_login.menu_esqueceu_senha') }}
-                </a>
+                <p class="text-sm mt-6 text-center"><a class="text-blue-600 font-semibold hover:underline ml-1" href="{{ route('password.request') }}">
+                        {{ __('tela_login.menu_esqueceu_senha') }}
+                    </a>
             @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('tela_login.menu_entrar') }}
-            </x-primary-button>
-        </div>
-    </form>
+        </form>
+    </div>
+</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,65 +1,71 @@
 @extends('layouts.app')
 
 @section('inicio')
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<div class="mt-24 flex-col justify-center font-[sans-serif] text-[#333] sm:h-screen p-4">
+    <div class="max-w-md w-full mx-auto border border-gray-300 rounded-md p-6">
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <div class="space-y-6">
+                <!-- Name -->
+                <div>
+                    <x-input-label for="nome" :value="__('tela_registro.menu_nome')" />
+                    <x-text-input id="nome" class="block mt-1 w-1/2" type="text" name="nome" :value="old('nome')" required autofocus autocomplete="nome" />
+                    <x-input-error :messages="$errors->get('nome')" class="mt-2" />
+                </div>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="nome" :value="__('tela_registro.menu_nome')" />
-            <x-text-input id="nome" class="block mt-1 w-1/2" type="text" name="nome" :value="old('nome')" required autofocus autocomplete="nome" />
-            <x-input-error :messages="$errors->get('nome')" class="mt-2" />
-        </div>
+                <!-- Email Address -->
+                <div class="mt-4">
+                    <x-input-label for="email" :value="__('tela_registro.menu_email')" />
+                    <x-text-input id="email" class="block mt-1 w-1/2" type="email" name="email" :value="old('email')" required autocomplete="email" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('tela_registro.menu_email')" />
-            <x-text-input id="email" class="block mt-1 w-1/2" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+                <!-- Confirm Email -->
+                <div class="mt-4">
+                    <x-input-label for="email_confirmation" :value="__('tela_registro.menu_confirma_email')" />
 
-        <!-- Confirm Email -->
-        <div class="mt-4">
-            <x-input-label for="email_confirmation" :value="__('tela_registro.menu_confirma_email')" />
+                    <x-text-input id="email_confirmation" class="block mt-1 w-1/2"
+                                    type="email"
+                                    name="email_confirmation" required autocomplete="email" />
 
-            <x-text-input id="email_confirmation" class="block mt-1 w-1/2"
-                            type="email"
-                            name="email_confirmation" required autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email_confirmation')" class="mt-2" />
+                </div>
 
-            <x-input-error :messages="$errors->get('email_confirmation')" class="mt-2" />
-        </div>
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label for="password" :value="__('tela_registro.menu_senha')" />
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('tela_registro.menu_senha')" />
+                    <x-text-input id="password" class="block mt-1 w-1/2"
+                                    type="password"
+                                    name="password"
+                                    required autocomplete="new-password" />
 
-            <x-text-input id="password" class="block mt-1 w-1/2"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                <!-- Confirm Password -->
+                <div class="mt-4">
+                    <x-input-label for="password_confirmation" :value="__('tela_registro.menu_confirma_senha')" />
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('tela_registro.menu_confirma_senha')" />
+                    <x-text-input id="password_confirmation" class="block mt-1 w-1/2"
+                                    type="password"
+                                    name="password_confirmation" required autocomplete="new-password" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-1/2"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                </div>
+            </div>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+            <div class="!mt-10">
+                <x-primary-button>
+                    {{ __('tela_registro.menu_registro') }}
+                </x-primary-button>
+            </div>
 
-        <div class="flex items-center justify-left mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('tela_registro.ja_possui_conta') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('tela_registro.menu_registro') }}
-            </x-primary-button>
-        </div>
-    </form>
+            <div class="!mt-10">
+                <p class="text-sm mt-6 text-center"><a href="{{ route('login') }}" class="text-blue-600 font-semibold hover:underline ml-1">{{ __('tela_registro.ja_possui_conta') }}</a></p>
+            </div>
+        </form>
+    </div>
+</div>
+</div>
 @endsection
