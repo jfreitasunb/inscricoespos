003d98d2a058db8d23fb07c16c1061fa297bf05a
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-02-11 13:47:17 -0300

    Menu de navegação do candidato.

--- resources/views/layouts/candidato/navigation.blade.php
+++ resources/views/layouts/candidato/navigation.blade.php
@@ -1,74 +1,48 @@
 <div>
-    <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('menu.candidato')" :active="request()->routeIs('menu.candidato')">
-                        Olá {{Auth::user()->nome}}!
-                    </x-nav-link>
-                </div>
-            </div>
-
-            <!-- Logout -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-nav-link>
-                </form>
-            </div>
-
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+    <ol class="flex items-center justify-center space-x-4 mt-4 w-full">
+        <li class="flex items-center space-x-2">
+            <button class="p-4 text-green-700 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:border-green-800 dark:text-green-400">
+                <span class="font-medium">Dados Pessoais</span>
+            </button>
+        </li>
+        <div class="w-10 h-0.5 bg-green-300 dark:bg-green-800"></div>
+        <div class="flex items-center space-x-2">
+            <button class="p-4 text-green-700 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:border-green-800 dark:text-green-400">
+                <span class="font-medium">Dados Acadêmicos</span>
+            </button>
+        </div>
+        <div class="w-10 h-0.5 bg-green-300 dark:bg-green-800"></div>
+        <div class="flex items-center space-x-2">
+            <button class="p-4 text-green-700 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:border-green-800 dark:text-green-400">
+                <span class="font-medium">Escolha</span>
+            </button>
+        </div>
+        <div class="w-10 h-0.5 bg-gray-300 dark:bg-gray-700"></div>
+        <div class="flex items-center space-x-2">
+            <button class="p-4 text-green-700 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:border-green-800 dark:text-green-400">
+                <span class="font-medium">Motivação/Documentos</span>
+            </button>
+        </div>
+        <div class="w-10 h-0.5 bg-gray-300 dark:bg-gray-700"></div>
+        <div class="flex items-center space-x-2">
+            <button class="p-4 text-green-700 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:border-green-800 dark:text-green-400">
+                <span class="font-medium">Status das Cartas</span>
+            </button>
+        </div>
+        <div class="w-10 h-0.5 bg-gray-300 dark:bg-gray-700"></div>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <div class="flex items-center space-x-2">
+                <button class="p-4 text-red-700 border border-red-300 rounded-lg bg-red-200 dark:bg-red-800 dark:border-red-800 dark:text-red-400">
+                    <a href="{{ route('logout') }}"  onclick="event.preventDefault();this.closest('form').submit();">
+                        <span class="font-medium">Sair</span>
+                    </a>
                 </button>
             </div>
-        </div>
-    </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('menu.candidato')" :active="request()->routeIs('menu.candidato')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->nome }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
-        </div>
-    </div>
-</nav>
+        </form>
+    </ol>
 </div>
+@yield('dados_pessoais')
 
+
+@include('layouts.rodape')
