37b429798fe5a4ab867f15b8d45bc4c3f63fa1f7
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2025-02-11 19:50:00 -0300

    Informações sobre país, estado e cidade.

---
+++ database/migrations/2025_02_11_193426_create_estados_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estados', function (Blueprint $table){
+            $table->increments('id_state');
+            $table->string('name');
+            $table->integer('country_id')->unsigned()->default(0);
+            $table->foreign('country_id')->references('id')->on('paises');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estados');
+    }
+};
